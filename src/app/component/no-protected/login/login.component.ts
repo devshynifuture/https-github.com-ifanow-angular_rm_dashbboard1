fec2aed48f7284c4ded92142ec890be69bf39849
Gaurav <gaurav@futurewise.co.in>
2019-10-23 18:55:23 +0530

    Upper slider issue resolved and formatting added
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/no-protected/login/login.component.ts
+++ src/app/component/no-protected/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {  FormGroup,  FormControl, Validators, FormBuilder } from '@angular/forms';
-import { Router} from '@angular/router';
-import { AuthService } from 'src/app/auth-service/authService';
-import { EventService } from 'src/app/Data-service/event.service';
-import { BackOfficeService } from '../../protect-component/AdviserComponent/backOffice/back-office.service';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/auth-service/authService';
+import {EventService} from 'src/app/Data-service/event.service';
+import {BackOfficeService} from '../../protect-component/AdviserComponent/backOffice/back-office.service';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +13,11 @@ import { BackOfficeService } from '../../protect-component/AdviserComponent/back
 export class LoginComponent implements OnInit {
 
   constructor(
-    private formBuilder: FormBuilder, private eventService:EventService,
-    public backOfficeService:BackOfficeService,
+    private formBuilder: FormBuilder, private eventService: EventService,
+    public backOfficeService: BackOfficeService,
     public router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService) {
+  }
 
   loginForm: FormGroup;
 
@@ -36,50 +37,68 @@ export class LoginComponent implements OnInit {
       }),
     });
   }
-  
+
   getError() {
-    return ' *This is required feild'
+    return ' *This is required field';
   }
+
   cancel(): void {
     this.loginForm.reset();
   }
 
-  enterEvent(event){
+  enterEvent(event) {
     if (event.keyCode === 13) {
-      this.onSubmit()
+      this.onSubmit();
     }
   }
 
   onSubmit() {
-    this.authService.setToken('12333nhsdhdh1233')
-    this.authService.setUserInfo('https://res.cloudinary.com/futurewise/image/upload/v1566029063/icons_fakfxf.png')
-    this.router.navigate(['/admin/subscription']);
+    // this.authService.setToken('12333nhsdhdh1233');
+    // this.authService.setUserInfo('https://res.cloudinary.com/futurewise/image/upload/v1566029063/icons_fakfxf.png');
+    // this.router.navigate(['/admin/subscription']);
     if (this.loginForm.valid) {
-       let loginData={
-        name:this.loginForm.controls['name'].value,
-        password:this.loginForm.controls['password'].value,
-        id:0,
-        role: "admin"
-       }
-       console.log(loginData)
-        this.backOfficeService.loginApi(loginData).subscribe(
-        data => this.closeDialog(data),
-        err => console.log(err)
-      )}
-     
+      const loginData = {
+        name: this.loginForm.controls.name.value,
+        password: this.loginForm.controls.password.value,
+        id: 0,
+        role: 'admin'
+      };
+      console.log(loginData);
+      this.backOfficeService.loginApi(loginData).subscribe(
+        data => {
+          if (data) {
+            console.log('data: ', data);
+            const jsonData = JSON.parse(data);
+            this.authService.setToken('12333nhsdhdh1233');
+            jsonData.advisorId = 2808;
+            jsonData.clientId = 2978;
+            jsonData.imgUrl = 'https://res.cloudinary.com/futurewise/image/upload/v1566029063/icons_fakfxf.png';
+            this.authService.setUserInfo(jsonData);
+            // this.authService.setToken(loginData.payLoad);
+            this.eventService.openSnackBar('Login SuccessFully', 'dismiss');
+            this.router.navigate(['/admin/subscription']);
+
+          }
+          // this.closeDialog(data);
+        },
+        err => {
+          console.log('error on login: ', err);
+          this.eventService.openSnackBar(err, 'dismiss');
+        }
+      );
+    }
+
   }
 
   closeDialog(data) {
-    let loginData = data;
-    console.log(data)
-    if(data.status === 200){
-        this.authService.setToken(loginData.payLoad);
-        this.eventService.openSnackBar('Login SuccessFully ','dismiss')
-        this.router.navigate(['/admin/service'])
-    }
-    
-    else{
-       this.eventService.openSnackBar(loginData.message,'dismiss')
+    const loginData = data;
+    console.log(data);
+    if (data.status === 200) {
+      this.authService.setToken(loginData.payLoad);
+      this.eventService.openSnackBar('Login SuccessFully ', 'dismiss');
+      this.router.navigate(['/admin/service']);
+    } else {
+      this.eventService.openSnackBar(loginData.message, 'dismiss');
     }
   }
 
