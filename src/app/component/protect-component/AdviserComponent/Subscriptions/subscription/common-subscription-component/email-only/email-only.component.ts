acbced24c7b181ceaf81d19ba642067a86284b4d
Gayatri Pawar <gayatri@futurewise.co.in>
2019-10-31 19:16:08 +0530

    commit froala chnNGES save in quotation

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/email-only/email-only.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/email-only/email-only.component.ts
@@ -79,12 +79,30 @@ saveData(data)
   }
   save(){
     console.log("here is saved data",this.storeData);
-    this.updateData(this.storeData);
+    if(this.emailSend=='emailQuotation'){
+      this.updateDataQuot(this.storeData);
+    }else{
+      this.updateData(this.storeData);
+    }
     this.Close('close');
+  }
+  updateDataQuot(data) {
+    const obj = {
+      id: data.id, // pass here advisor id for Invoice advisor
+      docText: data.documentText,
+      quotation:true
+    };
+    // this.subscription.updateQuotationData(obj).subscribe(
+    //   data => this.getResponseData(data)
+    // );
+
+    this.subscription.updateDocumentData(obj).subscribe(
+      data => this.getResponseData(data)
+    );
   }
   updateData(data) {
     const obj = {
-      id: data.documentRepositoryId, // pass here advisor id for Invoice advisor
+      id: data.id, // pass here advisor id for Invoice advisor
       docText: data.documentText
     };
     // this.subscription.updateQuotationData(obj).subscribe(
