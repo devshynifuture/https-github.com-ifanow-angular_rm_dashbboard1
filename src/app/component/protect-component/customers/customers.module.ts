5625d788c9db277b04b882b9ed067bf5627bd8ae
Gaurav <gaurav@futurewise.co.in>
2019-11-01 20:11:11 +0530

    Callback for subscription side bar created
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/customers/customers.module.ts
+++ src/app/component/protect-component/customers/customers.module.ts
@@ -13,14 +13,28 @@ import {ExpensesComponent} from './component/customer/accounts/expenses/expenses
 import {CustomerDocumentsComponent} from './component/customer/accounts/customer-documents/customer-documents.component';
 import {AddProfileSummaryComponent} from './component/common-component/add-profile-summary/add-profile-summary.component';
 import {SchemeLevelTransactionComponent} from './component/common-component/scheme-level-transaction/scheme-level-transaction.component';
+import {MaterialModule} from "../../../material/material";
 
 
 @NgModule({
   declarations: [/*CustomerComponent, AccountsComponent,SummaryComponent,*/ /*AssetsComponent,*/
-    /*LiabilitiesComponent, InsuranceComponent, ExpensesComponent, AddProfileSummaryComponent,  */ SchemeLevelTransactionComponent, IncomeComponent, CustomerDocumentsComponent],
+    /*LiabilitiesComponent, InsuranceComponent, ExpensesComponent, AddProfileSummaryComponent,  */ SchemeLevelTransactionComponent,
+    IncomeComponent,
+    CustomerDocumentsComponent,
+    CustomerComponent,
+    AccountsComponent,
+    SummaryComponent,
+    AssetsComponent,
+    LiabilitiesComponent,
+    InsuranceComponent,
+    ExpensesComponent,
+    AddProfileSummaryComponent,
+    IncomeComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    MaterialModule,
+
   ]
 })
 export class CustomersModule {
