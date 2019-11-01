abe1bde174d2ea6f2b56094d56d182a00f36f528
Gaurav <gaurav@futurewise.co.in>
2019-11-01 16:18:34 +0530

    Directive created for radio button selection
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/customers/component/customer/accounts/liabilities/liabilities.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/liabilities/liabilities.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import {UtilService} from '../../../../../../../services/util.service';
+import {EventService} from '../../../../../../../Data-service/event.service';
+import {SubscriptionInject} from '../../../../../AdviserComponent/Subscriptions/subscription-inject.service';
 
 @Component({
   selector: 'app-liabilities',
@@ -7,17 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LiabilitiesComponent implements OnInit {
-  displayedColumns = ['no', 'name', 'type', 'loan','ldate','today','ten','rate','emi','fin','status','icons'];
+  displayedColumns = ['no', 'name', 'type', 'loan', 'ldate', 'today', 'ten', 'rate', 'emi', 'fin', 'status', 'icons'];
   dataSource = ELEMENT_DATA;
 
-  constructor() { }
- 
- 
-  viewMode;
-  ngOnInit() {
-    this.viewMode="tab1"
+  constructor(private eventService: EventService, private subinject: SubscriptionInject) {
   }
 
+
+  viewMode;
+
+  ngOnInit() {
+    this.viewMode = 'tab1';
+  }
+
+  clickHandling() {
+    console.log('something was clicked');
+    // this.openFragment('', 'plan');
+    this.open('open', null);
+  }
+
+  openFragment(singlePlan, data) {
+    this.subinject.pushUpperData(singlePlan);
+
+    const fragmentData = {
+      Flag: data,
+      planData: singlePlan,
+      state: 'open'
+    };
+    const subscription = this.eventService.changeUpperSliderState(fragmentData).subscribe(
+      upperSliderData => {
+        subscription.unsubscribe();
+      }
+    );
+
+  }
+
+  open(state, data) {
+    let feeMode;
+    if (data && data.subscriptionPricing.feeTypeId == 1) {
+      feeMode = 'fixedModifyFees';
+    } else {
+      feeMode = 'variableModifyFees';
+    }
+    this.eventService.sidebarData(feeMode);
+    this.subinject.rightSideData(state);
+    this.subinject.addSingleProfile(data);
+
+  }
 }
 
 export interface PeriodicElement {
@@ -25,18 +64,54 @@ export interface PeriodicElement {
   name: string;
   type: string;
   loan: string;
-  ldate:string;
-  today:string;
-  ten:string;
-  rate:string;
-  emi:string;
-  fin:string;
-  status:string;
- 
+  ldate: string;
+  today: string;
+  ten: string;
+  rate: string;
+  emi: string;
+  fin: string;
+  status: string;
+
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {no: "1", name: 'Rahul Jain', type: "Home loan", loan: '60,000',ldate:"18/09/2021",today:"1,00,000",ten:"5y 9m",rate:"8.40%",emi:"60,000",fin:"ICICI FD",status:"MATURED"},
-  {no: "2", name: 'Shilpa Jain', type: "Home loan", loan: '60,000',ldate:"18/09/2021",today:"1,00,000",ten:"5y 9m",rate:"8.40%",emi:"60,000",fin:"ICICI FD",status:"MATURED"},
-  {no: "", name: 'Total', type: "", loan: '1,20,000',ldate:"",today:"1,50,000",ten:"",rate:"",emi:"",fin:"",status:""},
+  {
+    no: '1',
+    name: 'Rahul Jain',
+    type: 'Home loan',
+    loan: '60,000',
+    ldate: '18/09/2021',
+    today: '1,00,000',
+    ten: '5y 9m',
+    rate: '8.40%',
+    emi: '60,000',
+    fin: 'ICICI FD',
+    status: 'MATURED'
+  },
+  {
+    no: '2',
+    name: 'Shilpa Jain',
+    type: 'Home loan',
+    loan: '60,000',
+    ldate: '18/09/2021',
+    today: '1,00,000',
+    ten: '5y 9m',
+    rate: '8.40%',
+    emi: '60,000',
+    fin: 'ICICI FD',
+    status: 'MATURED'
+  },
+  {
+    no: '',
+    name: 'Total',
+    type: '',
+    loan: '1,20,000',
+    ldate: '',
+    today: '1,50,000',
+    ten: '',
+    rate: '',
+    emi: '',
+    fin: '',
+    status: ''
+  },
 ];
