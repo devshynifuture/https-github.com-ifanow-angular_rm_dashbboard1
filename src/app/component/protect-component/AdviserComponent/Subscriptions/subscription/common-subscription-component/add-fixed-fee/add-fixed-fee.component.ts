4a9c7c886c8261939ee96438dca0c69d437314ba
Gayatri Pawar <gayatri@futurewise.co.in>
2019-11-06 16:52:13 +0530

    add-fixed fee and add-structure maxlength line commentting

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-fixed-fee/add-fixed-fee.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/add-fixed-fee/add-fixed-fee.component.ts
@@ -56,10 +56,10 @@ export class AddFixedFeeComponent implements OnInit {
       billEvery: [, [Validators.required]],
       billingMode: [1]
     });
-    this.getFormControl().serviceName.maxLength = 40;
-    this.getFormControl().code.maxLength = 10;
-    this.getFormControl().description.maxLength = 160;
-    this.getFormControl().fees.maxLength = 10;
+    // this.getFormControl().serviceName.maxLength = 40;
+    // this.getFormControl().code.maxLength = 10;
+    // this.getFormControl().description.maxLength = 160;
+    // this.getFormControl().fees.maxLength = 10;
   }
 
   setValidation(flag) {
