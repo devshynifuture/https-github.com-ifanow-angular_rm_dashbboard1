163d3ff24d5c02a9c76627ff9331e774e9a9c7b5
Gaurav <gaurav@futurewise.co.in>
2019-11-06 11:19:51 +0530

    Date range selector issue resolved
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

---
+++ src/app/common/custom.common.module.ts
@@ -0,0 +1,19 @@
+// import DialogContainerComponent from ''
+import {NgModule} from '@angular/core';
+
+// import { LiabilityrightComponent } from './component/customer/accounts/liabilities/liabilityright/liabilityright.component';
+import {DialogContainerComponent} from './dialog-container/dialog-container.component';
+import {CustomDialogContainerComponent} from './custom-dialog-container/custom-dialog-container.component';
+import {MaterialModule} from '../material/material';
+
+
+@NgModule({
+  declarations: [DialogContainerComponent, CustomDialogContainerComponent
+
+  ],
+  imports: [
+    MaterialModule,
+  ]
+})
+export class CustomCommonModule {
+}
