340320518cf91387a84439d011996e58f3aaf03e
Akshay <akshay@futurewise.co.in>
2019-11-11 13:18:48 +0530

    add

--- src/app/component/protect-component/customers/component/customer/accounts/insurance/insurance.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/insurance/insurance.component.ts
@@ -1,26 +1,86 @@
 import { Component, OnInit } from '@angular/core';
 import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
 import { UtilService } from 'src/app/services/util.service';
+import { AuthService } from 'src/app/auth-service/authService';
+import { CustomerService } from '../../customer.service';
+import { trigger, transition, query, stagger, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-insurance',
   templateUrl: './insurance.component.html',
-  styleUrls: ['./insurance.component.scss']
+  styleUrls: ['./insurance.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [ // each time the binding value changes
+        query(':leave', [
+          stagger(0.1, [
+            animate('0.1s', style({ opacity: 0 }))
+          ])
+        ], { optional: true }),
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(100, [
+            animate('0.1s', style({ opacity: 0 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 
 export class InsuranceComponent implements OnInit {
-  displayedColumns = ['no', 'life', 'name', 'number','sum','cvalue','premium','term','pterm','desc','status','icons'];
+  displayedColumns = ['no', 'life', 'name', 'number', 'sum', 'cvalue', 'premium', 'term', 'pterm', 'desc', 'status', 'icons'];
   dataSource = ELEMENT_DATA;
-  displayedColumns1 = ['no', 'owner', 'cvalue', 'amt','mvalue','rate','mdate','type','ppf','desc','status','icons'];
+  displayedColumns1 = ['no', 'owner', 'cvalue', 'amt', 'mvalue', 'rate', 'mdate', 'type', 'ppf', 'desc', 'status', 'icons'];
   dataSource1 = ELEMENT_DATA1;
-  constructor(private subInjectService:SubscriptionInject) { }
+  advisorId: any;
+  constructor(private subInjectService: SubscriptionInject, private cusService: CustomerService) { }
   viewMode;
+  lifeInsuranceList = ["Term", "Traditional", "ULIP"]
+  generalLifeInsuranceList = [/*"Health", "Car/2 Wheeler", "Travel", "Personal accident", "Critical illness", "Cancer", "Home", "Others"*/]
   ngOnInit() {
-    this.viewMode="tab1"
+    this.viewMode = "tab1"
+    this.advisorId = AuthService.getAdvisorId();
+    // this.getInsuranceData(this.advisorId,);
   }
+  getInsuranceData(advisorId, clientId, insuranceId) {
+    let obj = {
+      'advisorId': advisorId,
+      'clientId': clientId,
+      'insuranceTypeId': insuranceId
+    }
+    this.cusService.getLifeInsuranceData(obj).subscribe(
+      data => this.getInsuranceDataResponse(data)
+    )
+  }
+  getInsuranceDataResponse(data) {
+    console.log(data)
+  }
+  
 
-  open(flagValue,data)
+  
+  toggle(value) {
+    if (value === "lifeInsurance") {
+     this.generalLifeInsuranceList=[];
+     this.lifeInsuranceList=[];
+     ["Term", "Traditional", "ULIP"].map((i)=>
+      {
+        this.lifeInsuranceList.push(i)
+      })
+   }
+    else {
+      this.lifeInsuranceList=[];
+      this.generalLifeInsuranceList=[];
+      ["Health", "Car/2 Wheeler", "Travel", "Personal accident", "Critical illness", "Cancer", "Home" ,"Others"].map((i)=>{
+        this.generalLifeInsuranceList.push(i)
+      })
+    }
+  }
+  logAnimation(event)
   {
+    console.log(event)
+  }
+  open(flagValue, data) {
     const fragmentData = {
       Flag: flagValue,
       data,
@@ -33,7 +93,7 @@ export class InsuranceComponent implements OnInit {
         if (UtilService.isDialogClose(sideBarData)) {
           console.log('this is sidebardata in subs subs 2: ', sideBarData);
           rightSideDataSub.unsubscribe();
-    
+
         }
       }
     );
@@ -47,23 +107,29 @@ export interface PeriodicElement {
   life: string;
   name: string;
   number: string;
-  sum:string;
-  cvalue:string;
-  premium:string;
-  term:string;
-  pterm:string;
-  desc:string;
-  status:string;
+  sum: string;
+  cvalue: string;
+  premium: string;
+  term: string;
+  pterm: string;
+  desc: string;
+  status: string;
 
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {no: "1", life: "Rahul Jain", name:"Cumulative", number: "358656327863",sum:"94,925",cvalue:"60,000",
-premium:"1,00,000",term:"45",pterm:"45",desc:"ICICI FD",status:"MATURED"},
-{no: "2", life: "Shilpa Jain", name:"Cumulative", number: "358656327863",sum:"94,925",cvalue:"60,000",
-premium:"1,00,000",term:"45",pterm:"45",desc:"ICICI FD",status:"MATURED"},
-{no: "", life: "", name:"", number: "",sum:"94,925",cvalue:"60,000",
-premium:"1,00,000",term:"",pterm:"",desc:"",status:""},
+  {
+    no: "1", life: "Rahul Jain", name: "Cumulative", number: "358656327863", sum: "94,925", cvalue: "60,000",
+    premium: "1,00,000", term: "45", pterm: "45", desc: "ICICI FD", status: "MATURED"
+  },
+  {
+    no: "2", life: "Shilpa Jain", name: "Cumulative", number: "358656327863", sum: "94,925", cvalue: "60,000",
+    premium: "1,00,000", term: "45", pterm: "45", desc: "ICICI FD", status: "MATURED"
+  },
+  {
+    no: "", life: "", name: "", number: "", sum: "94,925", cvalue: "60,000",
+    premium: "1,00,000", term: "", pterm: "", desc: "", status: ""
+  },
 ];
 
 export interface PeriodicElement1 {
@@ -71,21 +137,27 @@ export interface PeriodicElement1 {
   owner: string;
   cvalue: string;
   amt: string;
-  mvalue:string;
-  rate:string;
-  mdate:string;
-  type:string;
-  ppf:string;
-  desc:string;
-  status:string;
+  mvalue: string;
+  rate: string;
+  mdate: string;
+  type: string;
+  ppf: string;
+  desc: string;
+  status: string;
 
 }
 
 const ELEMENT_DATA1: PeriodicElement1[] = [
-  {no: "1", owner: "Rahul Jain", cvalue:"94,925", amt: "60,000",mvalue:"1,00,000",rate:"8.40%",
-  mdate:"18/09/2021",type:"Cumulative",ppf:"980787870909",desc:"ICICI FD",status:"MATURED"},
-  {no: "2", owner: "Shilpa Jain", cvalue:"94,925", amt: "60,000",mvalue:"1,00,000",rate:"8.40%",
-  mdate:"18/09/2021",type:"Cumulative",ppf:"980787870909",desc:"ICICI FD",status:"MATURED"},
-  {no: "", owner: "Total", cvalue:"1,28,925", amt: "1,28,925",mvalue:"1,28,925",rate:"",
-  mdate:"",type:"",ppf:"",desc:"",status:""},
+  {
+    no: "1", owner: "Rahul Jain", cvalue: "94,925", amt: "60,000", mvalue: "1,00,000", rate: "8.40%",
+    mdate: "18/09/2021", type: "Cumulative", ppf: "980787870909", desc: "ICICI FD", status: "MATURED"
+  },
+  {
+    no: "2", owner: "Shilpa Jain", cvalue: "94,925", amt: "60,000", mvalue: "1,00,000", rate: "8.40%",
+    mdate: "18/09/2021", type: "Cumulative", ppf: "980787870909", desc: "ICICI FD", status: "MATURED"
+  },
+  {
+    no: "", owner: "Total", cvalue: "1,28,925", amt: "1,28,925", mvalue: "1,28,925", rate: "",
+    mdate: "", type: "", ppf: "", desc: "", status: ""
+  },
 ];
--- src/app/component/protect-component/customers/component/customer/customer.service.ts
+++ src/app/component/protect-component/customers/component/customer/customer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpService} from 'src/app/http-service/http-service';
 import { apiConfig } from 'src/app/config/main-config';
 import { appConfig } from 'src/app/config/component-config';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,10 @@ export class CustomerService {
   }
   addFixedDeposit(data){
     return this.http.post(apiConfig.MAIN_URL + appConfig.ADD_FIXEDDEPOSIT,data)
+  }
+  getLifeInsuranceData(data)
+  {
+    let httpParams=new HttpParams().set('advisorId',data.advisorId).set('clientId',data.clientId).set('insuranceTypeId',data.insuranceTypeId)
+    return this.http.get(apiConfig.MAIN_URL + appConfig.GET_LIFE_INSURANCE,httpParams)
   }
 }
