fde3aab4230b7534d7df6831b44a367f699e604b
sunny <sunny@futurewise.co.in>
2019-11-12 13:20:21 +0530

    add

--- src/app/common/custom-dialog-container/custom-dialog-container.component.ts
+++ src/app/common/custom-dialog-container/custom-dialog-container.component.ts
@@ -21,7 +21,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       animate(300, style({opacity: 0.25}))]),
   ]), trigger('openClose', [
     state('open', style({
-      left: '35%'
+      left: '40%'
     })),
     state('openHelp', style({
       left: '65%'
