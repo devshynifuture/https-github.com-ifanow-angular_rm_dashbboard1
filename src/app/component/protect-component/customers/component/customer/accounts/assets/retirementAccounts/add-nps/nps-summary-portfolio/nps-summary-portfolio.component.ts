8e399979ba98fd45c9250f301aa8b372a6275769
devshyni <devshyni@futurewise.co.in>
2019-11-16 11:05:06 +0530

    dialog container

--- src/app/component/protect-component/customers/component/customer/accounts/assets/retirementAccounts/add-nps/nps-summary-portfolio/nps-summary-portfolio.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/retirementAccounts/add-nps/nps-summary-portfolio/nps-summary-portfolio.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { CustomerService } from '../../../../../customer.service';
 import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
 import { DatePipe } from '@angular/common';
@@ -17,12 +17,77 @@ import { MY_FORMATS2 } from 'src/app/constants/date-format.constant';
   ],
 })
 export class NpsSummaryPortfolioComponent implements OnInit {
+  ownerName: any;
+  familyMemberId: any;
+  inputData: any;
+  summaryNPS: any;
+  ownerData: any;
+  isValueAsOn = false;
+  isTotalContry = false;
+  isCurrentValue = false;
+  isFrequency = false;
+  isApproxContry = false;
+  isAccountPref = false
+  constructor(private router: Router,private fb: FormBuilder, private custumService: CustomerService, public subInjectService: SubscriptionInject, private datePipe: DatePipe) {
+    this.summaryNPS = this.fb.group({
+      published: true,
+      futureContry: this.fb.array([]),
+    });
+   }
 
-  constructor(private router: Router,private fb: FormBuilder, private custumService: CustomerService, public subInjectService: SubscriptionInject, private datePipe: DatePipe) { }
 
+  @Input()
+  set data(data) {
+    this.inputData = data;
+     this.getdataForm(data);
+  }
+
+  get data() {
+    return this.inputData;
+  }
   ngOnInit() {
+  }
+  display(value) {
+    console.log('value selected', value)
+    this.ownerName = value.userName;
+    this.familyMemberId = value.id
   }
   Close() {
     this.subInjectService.changeNewRightSliderState({ state: 'close' })
+  }
+  getdataForm(data) {
+    if (data == undefined) {
+      data = {}
+    }
+    this.summaryNPS = this.fb.group({
+      ownerName: [(data == undefined) ? '' : data.ownerName, [Validators.required]],
+      currentValue: [(data == undefined) ? '' : data.yearsCompleted, [Validators.required]],
+      valueAsOn: [(data == undefined) ? '' : data.amountReceived, [Validators.required]],
+      futureContry:[],
+      totalContry: [(data == undefined) ? '' : data.organizationName, [Validators.required]],
+      resonOfRecipt: [(data == undefined) ? '' : data.reasonOfReceipt, [Validators.required]],
+      bankAcNo: [(data == undefined) ? '' : data.bankAccountNumber, [Validators.required]],
+      frequency:[(data == undefined) ? '' : data.frequency, [Validators.required]],
+      accountPref:[(data == undefined) ? '' : data.accountPref, [Validators.required]],
+      approxContry:[(data == undefined) ? '' : data.approxContry, [Validators.required]],
+      description: [(data == undefined) ? '' : data.description, [Validators.required]],
+      id: [(data == undefined) ? '' : data.id, [Validators.required]],
+      familyMemberId: [[(data == undefined) ? '' : data.familyMemberId], [Validators.required]]
+    });
+    this.ownerData = this.summaryNPS.controls;
+    this.familyMemberId = this.summaryNPS.controls.familyMemberId.value
+    this.familyMemberId = this.familyMemberId[0]
+    
+  }
+  getFormControl(): any {
+    return this.summaryNPS.controls;
+  }
+  addFutureContry() {
+    const creds = this.summaryNPS.controls.futureContry as FormArray;
+    creds.push(this.fb.group({
+      frequency: '',
+      approxContry: '',
+      accountPref:'',
+    }));
   }
 }
