5c2343f4230d0b51d259081ab8607bc4825129cf
aman9893 <aman@futurewise.co.in>
2019-11-21 13:04:48 +0530

    ui

--- src/app/component/protect-component/customers/component/customer/plan/investments-plan/investments-plan.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/investments-plan/investments-plan.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvestmentsPlanComponent implements OnInit {
 
+  displayedColumns: string[] = ['item', 'cost'];
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+  
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
+
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
+
+
 }
+
+export interface Transaction {
+  item: string;
+  cost: number;
+}
+
