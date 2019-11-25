cd4189cc8cb6ca82e85fb948755139434dd6efb2
devshyni <devshyni@futurewise.co.in>
2019-11-25 18:19:21 +0530

    documents

--- src/app/component/protect-component/customers/component/customer/accounts/documents/documents.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/documents/documents.component.ts
@@ -8,6 +8,7 @@ import { UtilService } from 'src/app/services/util.service';
 import { DatePipe } from '@angular/common';
 import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
 import { CustomerService } from '../../customer.service';
+import * as _ from 'lodash';
 import { AuthService } from 'src/app/auth-service/authService';
 
 @Component({
@@ -24,47 +25,86 @@ export class DocumentsComponent implements OnInit {
   allFiles: any;
   AllDocs: any;
   commonFileFolders: any;
+  openFolderName: any;
+  backUpfiles: any;
+  i = 1;
+  resetData: any;
+  tabValue: any;
+  valueTab: any;
+  valueFirst: any;
 
   constructor(private _bottomSheet: MatBottomSheet, private event: EventService, private router: Router, private fb: FormBuilder, private custumService: CustomerService, public subInjectService: SubscriptionInject, private datePipe: DatePipe, public utils: UtilService) { }
   viewMode
   ngOnInit() {
+    let tabValue = 'Documents'
     this.viewMode = "tab1"
+    this.commonFileFolders = [];
+    this.backUpfiles = [];
+    this.openFolderName = [];
     this.advisorId = AuthService.getAdvisorId();
     this.clientId = AuthService.getClientId();
-    this.getAllFileList()
+    this.getAllFileList(tabValue)
   }
 
 
   openBottomSheet(): void {
     this._bottomSheet.open(BottomSheetComponent);
   }
-  getAllFileList() {
+  getAllFileList(tabValue) {
+    tabValue=(tabValue == 'Documents')?1:(tabValue == 'Recents')?2:(tabValue == 'Starred')?3:4;
+    this.valueTab = tabValue
     let obj = {
       advisorId: this.advisorId,
-      clientId:this.clientId,
-      docGetFlag:1,
-      folderParentId:0,
+      clientId: this.clientId,
+      docGetFlag: tabValue,
+      folderParentId: 0,
     }
     this.custumService.getAllFiles(obj).subscribe(
-      data => this.getAllFilesRes(data)
+      data => this.getAllFilesRes(data, 'value')
     );
   }
-  getAllFilesRes(data) {
-    this.commonFileFolders = [];
+  getAllFilesRes(data, value) {
     console.log(data)
     this.allFiles = data.files
     this.AllDocs = data.folders;
     this.commonFileFolders = data.folders;
-    this.commonFileFolders.push.apply( this.commonFileFolders, this.allFiles)
-    console.log('commonFileFolders',this.commonFileFolders)
+    this.commonFileFolders.push.apply(this.commonFileFolders, this.allFiles)
+    console.log('commonFileFolders', this.commonFileFolders)
   }
-  openFolder(value){
+  getFolders(data) {
+    this.openFolderName = _.reject(this.openFolderName, function (n) {
+      return n.openFolderId > data.openFolderId+1;
+    });
+    this.commonFileFolders = this.openFolderName[this.openFolderName.length - 1]
+    this.openFolderName = _.reject(this.openFolderName, function (n) {
+      return n.openFolderId > data.openFolderId;
+    });
+    this.valueFirst = this.openFolderName[0];
+  }
+  reset(){
+    this.commonFileFolders = this.valueFirst;
+    this.openFolderName = [];
+    this.backUpfiles = [];
+  }
+  openFolder(value) {
     let obj = {
       advisorId: this.advisorId,
-      clientId:this.clientId,
-      docGetFlag:1,
-      folderParentId:value.folderParentId,
+      clientId: this.clientId,
+      docGetFlag:  this.valueTab,
+      folderParentId: (value.id == undefined) ? 0 : value.id,
     }
+    if(this.commonFileFolders.openFolderId == undefined || this.commonFileFolders.openFolderId == null || this.openFolderName.length == 0){
+      Object.assign(this.commonFileFolders, { 'openFolderNm': value.folderName });
+      Object.assign(this.commonFileFolders, { 'openFolderId': this.i++ });
+      this.openFolderName.push(this.commonFileFolders)
+      if (this.commonFileFolders.length > 0) {
+        this.backUpfiles.push(this.commonFileFolders);
+      }
+      console.log('this.backUpfiles', this.backUpfiles)
+    }
+    this.custumService.getAllFiles(obj).subscribe(
+      data => this.getAllFilesRes(data, value)
+    );
   }
 }
 
