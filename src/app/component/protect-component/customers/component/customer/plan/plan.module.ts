38373cd03742e6a90df0d2a01fc7111306e123bc
aman9893 <aman@futurewise.co.in>
2019-11-29 13:49:26 +0530

    ui

--- src/app/component/protect-component/customers/component/customer/plan/plan.module.ts
+++ src/app/component/protect-component/customers/component/customer/plan/plan.module.ts
@@ -17,7 +17,6 @@ import {AddInvestmentPlanComponent} from './investments-plan/add-investment-plan
 import {AddProfilePlanComponent} from './profile-plan/add-profile-plan/add-profile-plan.component';
 import {HistoryRiskProfileComponent} from './profile-plan/history-risk-profile/history-risk-profile.component';
 // import {BrowserModule} from '@angular/platform-browser';
-import {AddPlaninsuranceComponent} from './insurance-plan/add-planinsurance/add-planinsurance.component';
 import {PlanRoutingModule} from "./plan-routing.module";
 import {PlanEntryModule} from "./plan.entry.module";
 
@@ -43,8 +42,6 @@ import {PlanEntryModule} from "./plan.entry.module";
     AddInvestmentPlanComponent,
     AddProfilePlanComponent,
     HistoryRiskProfileComponent,
-
-    AddPlaninsuranceComponent,
 
   ],
   imports: [
