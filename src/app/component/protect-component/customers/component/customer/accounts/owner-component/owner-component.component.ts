bea457ab50baac35ca3aae22bc5cba221c6dcbd2
devshyni <devshyni@futurewise.co.in>
2019-12-03 12:35:05 +0530

    components owner

--- src/app/component/protect-component/customers/component/customer/accounts/owner-component/owner-component.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/owner-component/owner-component.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {CustomerService} from '../../customer.service';
 import {Validators, FormBuilder} from '@angular/forms';
-import {AuthService} from 'src/app/auth-service/authService';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-owner-component',
@@ -18,6 +18,11 @@ export class OwnerComponentComponent implements OnInit {
   ownerData: any;
   clientId: any;
   sendData: any;
+  ownerName: any;
+  familyMemberId: any;
+  nomineesListFM: any;
+  dataFM: any;
+  familyList: any;
 
   constructor(private fb: FormBuilder, private custumService: CustomerService) {
   }
@@ -38,46 +43,69 @@ export class OwnerComponentComponent implements OnInit {
 
   ngOnInit() {
     console.log('OwnerComponentComponent ngOnInit ownerData', this.ownerData)
-    this.advisorId = AuthService.getAdvisorId();
-    this.clientId = AuthService.getClientId();
-    this.getListFamilyMem(this.ownerData);
-    this.getdataForm()
+   // this.advisorId = AuthService.getAdvisorId();
+   // this.clientId = AuthService.getClientId();
+    //this.getListFamilyMem(this.ownerData);
+  //  this.getdataForm()
     this.family = this.s;
   }
 
-  getListFamilyMem(data) {
-    let obj = {
-      advisorId: this.advisorId,
-      clientId: this.clientId,
-    }
-    this.custumService.getListOfFamilyByClient(obj).subscribe(
-      data => this.getListOfFamilyByClientRes(data)
-    );
-  }
-
-  getOwnerName(value) {
-    console.log('selected', value)
-    value.familyList = this.family
+  display(value) {
+    console.log('value selected', value)
+    this.ownerName = value.userName;
+    this.familyMemberId = value.id
     this.valueChange.emit(value);
   }
-
-  getListOfFamilyByClientRes(data) {
-    console.log('family Memebers', data)
-    this.sendData = data
-    this.family = data.familyMembersList
+  lisNominee(value) {
+    console.log(value)
     this.valueChange1.emit(this.sendData);
+    this.nomineesListFM = Object.assign([], value.familyMembersList);
   }
-
-  getdataForm() {
-    this.owner = this.fb.group({
-      ownerName: [(this.ownerData.ownerName.value == null) ? '' : this.ownerData.ownerName.value, [Validators.required]],
-    });
-    if (this.owner.controls.ownerName.value == '') {
-      this.getFormControl().ownerName.setValue(this.ownerData.ownerName.value);
+  nomineesList() {
+    this.dataFM = this.nomineesListFM
+    if (this.dataFM.length > 0) {
+      let name = this.ownerName
+      var evens = _.reject(this.dataFM, function (n) {
+        return n.userName == name;
+      });
+      this.familyList = evens
     }
-  }
 
-  getFormControl(): any {
-    return (this.owner.controls);
+    console.log('familyList', this.familyList)
   }
+  // getListFamilyMem(data) {
+  //   let obj = {
+  //     advisorId: this.advisorId,
+  //     clientId: this.clientId,
+  //   }
+  //   this.custumService.getListOfFamilyByClient(obj).subscribe(
+  //     data => this.getListOfFamilyByClientRes(data)
+  //   );
+  // }
+
+  // getOwnerName(value) {
+  //   console.log('selected', value)
+  //   value.familyList = this.family
+  //   this.valueChange.emit(value);
+  // }
+
+  // getListOfFamilyByClientRes(data) {
+  //   console.log('family Memebers', data)
+  //   this.sendData = data
+  //   this.family = data.familyMembersList
+  //   this.valueChange1.emit(this.sendData);
+  // }
+
+  // getdataForm() {
+  //   this.owner = this.fb.group({
+  //     ownerName: [(this.ownerData.ownerName.value == null) ? '' : this.ownerData.ownerName.value, [Validators.required]],
+  //   });
+  //   if (this.owner.controls.ownerName.value == '') {
+  //     this.getFormControl().ownerName.setValue(this.ownerData.ownerName.value);
+  //   }
+  // }
+
+  // getFormControl(): any {
+  //   return (this.owner.controls);
+  // }
 }
