765d515e38ad8deae07f138b45f88b48e71bb45f
Gayatri Pawar <gayatri@futurewise.co.in>
2019-12-12 17:12:37 +0530

    changes in real-estate

--- src/app/component/protect-component/AdviserComponent/Email/email.module.ts
+++ src/app/component/protect-component/AdviserComponent/Email/email.module.ts
@@ -1,22 +1,18 @@
-import { CommonComponentModule } from './../../common-component/common-component.module';
-import { FroalaComponent } from './../../common-component/froala/froala.component';
-import { FormsModule } from '@angular/forms';
-import { EmailListModule } from './email-component/email-list/email-list.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './../../../../material/material';
+import { CommonComponentModule } from './../../common-component/common-component.module';
+import { EmailArchiveComponent } from './email-component/email-archive/email-archive.component';
+import { EmailDraftComponent } from './email-component/email-draft/email-draft.component';
+import { EmailListModule } from './email-component/email-list/email-list.module';
+import { EmailSentComponent } from './email-component/email-sent/email-sent.component';
+import { EmailTrashComponent } from './email-component/email-trash/email-trash.component';
 import { EmailComponent } from './email-component/email.component';
 import { LeftSidebarComponent } from './email-component/left-sidebar/left-sidebar.component';
-import { EmailListComponent } from './email-component/email-list/email-list.component';
 import { EmailRoutingModule } from './email-routing.module';
-import { MaterialModule } from './../../../../material/material';
-import { EmailSentComponent } from './email-component/email-sent/email-sent.component';
-import { EmailDraftComponent } from './email-component/email-draft/email-draft.component';
-import { EmailArchiveComponent } from './email-component/email-archive/email-archive.component';
-import { EmailTrashComponent } from './email-component/email-trash/email-trash.component';
-import { EmailAddTaskComponent } from './email-component/email-list/email-add-task/email-add-task.component';
-import { ComposeEmailComponent } from './email-component/compose-email/compose-email.component';
 import { EmailEntryModule } from './email.entry.module';
+
 
 
 @NgModule({
