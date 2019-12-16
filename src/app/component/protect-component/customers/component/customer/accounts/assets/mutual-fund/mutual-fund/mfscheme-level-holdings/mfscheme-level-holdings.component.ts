9a5740fa3db8c4d02c57da1ee563dd43b40e13a1
Gayatri Pawar <gayatri@futurewise.co.in>
2019-12-16 19:12:15 +0530

    mutual fund form

---
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mfscheme-level-holdings/mfscheme-level-holdings.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-mfscheme-level-holdings',
+  templateUrl: './mfscheme-level-holdings.component.html',
+  styleUrls: ['./mfscheme-level-holdings.component.scss']
+})
+export class MFSchemeLevelHoldingsComponent implements OnInit {
+  _data: any;
+  schemeLevelHoldingForm: any;
+  ownerData: any;
+
+  constructor(public subInjectService: SubscriptionInject,private fb: FormBuilder) { }
+  @Input()
+  set data(inputData) {
+    this._data = inputData;
+    // this.getSchemeLevelHoldings(inputData);
+  }
+  get data() {
+    return this._data;
+  }
+  ngOnInit() {
+  }
+
+  getSchemeLevelHoldings(data) {
+    if (data == undefined) {
+      data = {};
+    }
+    // this.schemeLevelHoldingForm = this.fb.group({
+    //   ownerName: [data.ownerName , [Validators.required]],
+    //   dateOfReceipt: [new Date(data.dateOfReceived), [Validators.required]],
+    //   creditorName: [data.creditorName, [Validators.required]],
+    //   amtBorrowed:[data.amountBorrowed,[Validators.required]],
+    //   interest: [data.interest, [Validators.required]],
+    //   dateOfRepayment: [new Date(data.dateOfRepayment), [Validators.required]],
+    //   balance: [data.outstandingBalance, [Validators.required]],
+    //   collateral: [data.collateral],
+    // });
+
+    this.ownerData = this.schemeLevelHoldingForm.controls;
+
+  }
+  Close() {
+    this.subInjectService.changeNewRightSliderState({ state: 'close' });
+  }
+}
