cec32eabdf29932d831e3265bc67a4abf4b408c9
devshyni <devshyni@futurewise.co.in>
2019-12-17 15:02:11 +0530

    graph

--- src/app/component/protect-component/customers/component/customer/plan/profile-plan/riskProfile/risk-profile/risk-profile.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/profile-plan/riskProfile/risk-profile/risk-profile.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {PlanService} from '../../../plan.service';
-import {FormBuilder} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { PlanService } from '../../../plan.service';
+import { FormBuilder } from '@angular/forms';
 import * as Highcharts from 'highcharts';
-import {AuthService} from 'src/app/auth-service/authService';
+import { AuthService } from 'src/app/auth-service/authService';
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 
 const HighchartsMore = require('highcharts/highcharts-more.src');
 HighchartsMore(Highcharts);
@@ -33,7 +34,7 @@ export class RiskProfileComponent implements OnInit {
     alert(referenceKeyName.id);
   }
 
-  constructor(private fb: FormBuilder, public planService: PlanService,) {
+  constructor(private fb: FormBuilder, public planService: PlanService, ) {
   }
 
   ngOnInit() {
@@ -43,57 +44,184 @@ export class RiskProfileComponent implements OnInit {
     this.getdataForm('');
     this.sendRiskList = [];
   }
-
-  chartFunc(chartId) {
-    // @ts-ignore
-    Highcharts.chart(chartId, {
+  percentage(chartId) {
+    Highcharts.setOptions({
       chart: {
-        type: 'solidgauge'
+          inverted: true,
+          marginLeft: 135,
+          type: 'bullet'
       },
       title: {
-        text: 'Monthly Average Temperature'
+          text: null
       },
-      pane: {
-        center: ['50%', '50%'],
-        size: '300px',
-        startAngle: -90,
-        endAngle: 270,
-        background: {
-          backgroundColor: '#FFFE',
-          innerRadius: '100%',
-          outerRadius: '100%',
-          borderWidth: 0
-        }
+      legend: {
+          enabled: false
       },
       yAxis: {
-        min: 0,
-        max: 100,
-        labels: {
-          enabled: true
-        },
-
-        lineWidth: 0,
-        minorTickInterval: null,
-        tickPixelInterval: 400,
-        tickWidth: 0
+          gridLineWidth: 0
       },
       plotOptions: {
-        solidgauge: {
-          innerRadius: '70%'
+          // series: {
+          //     pointPadding: 0.25,
+          //     borderWidth: 0,
+          //     color: '#000',
+          //     targetOptions: {
+          //         width: '200%'
+          //     }
+          // }
+      },
+      credits: {
+          enabled: false
+      },
+      exporting: {
+          enabled: false
+      }
+  });
+  this.callFun()
+  }
+  callFun(){
+    Highcharts.chart('container2', {
+      xAxis: {
+          categories: ['<span class="hc-cat-title">Profit</span><br/>%']
+      },
+      yAxis: {
+          plotBands: [{
+              from: 0,
+              to: 20,
+              color: '#666'
+          }, {
+              from: 20,
+              to: 25,
+              color: '#999'
+          }, {
+              from: 25,
+              to: 100,
+              color: '#bbb'
+          }],
+          labels: {
+              format: '{value}%'
+          },
+          title: null
+      },
+      // series: [{
+      //     data: [{
+      //         y: 22,
+      //         target: 27
+      //     }]
+      // }],
+      tooltip: {
+          pointFormat: '<b>{point.y}</b> (with target at {point.target})'
+      }
+  });
+  }
+  guageFun(chartId) {
+    this.gaugeOptions = {
+
+      chart: {
+        type: 'gauge'
+      },
+
+      title: null,
+
+      pane: {
+        center: ['45%', '80%'],
+        size: '140%',
+       startAngle: -90,
+        endAngle: 90,
+        background: {
+          backgroundColor: '#EEE',
+          innerRadius: '70%',
+          outerRadius: '100%',
+          shape: 'solid'
         }
       },
-      series: [{
-        name: 'Speed',
-        data: [50],
-        dataLabels: {
-          enabled: false
+
+      tooltip: {
+        enabled: false
+      },
+
+      // the value axis
+      yAxis: {
+        plotBands: [{
+          from: 1,
+          to: 40,
+          color: '#02B875',
+          thickness: '30%'
+        }, {
+          from: 41,
+          to: 80,
+          color: '#5DC644',
+          thickness: '30%'
+        }, {
+          from: 81,
+          to: 120,
+          color: '#FFC100',
+          thickness: '30%'
         },
-        type: 'solidgauge'
+        {
+          from: 121,
+          to: 160,
+          color: '#FDAF40',
+          thickness: '30%'
+        }, {
+          from: 161,
+          to: 200,
+          color: '#FF7272',
+          thickness: '30%'
+        }],
+        lineWidth: 0,
+        minorTickInterval: 1,
+        tickPositions: [1, 200],
+        tickAmount: 1,
+        min: 1,
+        max: 200,
+        title: {
+          y: -70
+        },
+        labels: {
+          y: 16
+        }
+      },
 
-      }]
-    });
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: 5,
+            borderWidth: 0,
+          },
+          marker: {
+            enabled: true,
+            symbol: 'triangle',
+          }
+        },
+      }
+    };
+    this.container()
   }
+  container() {
+    var chartSpeed = Highcharts.chart('Gauge', Highcharts.merge(this.gaugeOptions, {
+      yAxis: {
+        title: {
+          text: ''
+        },
+      },
 
+      credits: {
+        enabled: true
+      },
+
+      series: [{
+        name: '',
+        data: [100],
+        dataLabels: 1,
+        tooltip: {
+          valueSuffix: ' km/h'
+        },
+      }]
+
+    }));
+  }
+  
   checkState(item, data) {
 
     console.log('$$$$$$$$$', this.sendRiskList);
@@ -152,7 +280,7 @@ export class RiskProfileComponent implements OnInit {
   submitRiskRes(data) {
     this.showRisk = true;
     setTimeout(() => {
-      this.chartFunc('devshyni');
+      this.guageFun('Gauge');
 
     }, 300);
     if (data) {
