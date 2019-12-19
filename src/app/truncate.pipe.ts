3a0ed987342419f036ef30df96f833f752101a49
Khizar <khizaruddin@futurewise.co.in>
2019-12-19 19:49:19 +0530

    email detail view

--- src/app/truncate.pipe.ts
+++ src/app/truncate.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncateStringPipe implements PipeTransform {
     transform(value: any, limit?: number): any {
         limit ? limit : 20;
-        return `${value.slice(0, limit)}...`;
+        return `${value.slice(0, limit)}${value.length < 10 ? '' : '...'}`;
     }
 }
