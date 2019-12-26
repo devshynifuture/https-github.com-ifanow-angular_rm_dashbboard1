e8cc72391f1d890e08871fd54a3aae8122056ec7
Gaurav <gaurav@futurewise.co.in>
2019-12-26 18:27:41 +0530

    Adding name of advisor instead of hardcoded ronak
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/no-protected/login/login.component.ts
+++ src/app/component/no-protected/login/login.component.ts
@@ -63,32 +63,32 @@ export class LoginComponent implements OnInit {
         roleId: 1
       };
 
-      this.hardCodeLoginForTest();
+      // this.hardCodeLoginForTest();
       // console.log(loginData);
-      /* this.backOfficeService.loginApi(loginData).subscribe(
-         data => {
-           if (data) {
-             console.log('data: ', data);
-             this.authService.setToken(data.token);
-             if (!data.advisorId) {
-               data.advisorId = data.adminAdvisorId;
-             }
-             this.authService.setUserInfo(data);
-             this.eventService.openSnackBar('Login SuccessFully', 'dismiss');
-             this.router.navigate(['admin', 'subscription', 'dashboard']);
+      this.backOfficeService.loginApi(loginData).subscribe(
+        data => {
+          if (data) {
+            console.log('data: ', data);
+            this.authService.setToken(data.token);
+            if (!data.advisorId) {
+              data.advisorId = data.adminAdvisorId;
+            }
+            this.authService.setUserInfo(data);
+            this.eventService.openSnackBar('Login SuccessFully', 'dismiss');
+            this.router.navigate(['admin', 'subscription', 'dashboard']);
 
-             // Hard coded client login for testing
-             this.authService.setClientData({
-               id: 2978, name: 'Aryendra Kumar Saxena'
-             });
-           }
-           this.closeDialog(data);
-         },
-         err => {
-           console.log('error on login: ', err);
-           this.eventService.openSnackBar(err, 'dismiss');
-         }
-       );*/
+            // Hard coded client login for testing
+            this.authService.setClientData({
+              id: 2978, name: 'Aryendra Kumar Saxena'
+            });
+          }
+          this.closeDialog(data);
+        },
+        err => {
+          console.log('error on login: ', err);
+          this.eventService.openSnackBar(err, 'dismiss');
+        }
+      );
     }
   }
 
