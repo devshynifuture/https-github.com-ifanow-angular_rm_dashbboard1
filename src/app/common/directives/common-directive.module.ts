019f6472343cebad02773b9c073a932146df3a68
Gaurav <gaurav@futurewise.co.in>
2020-01-02 16:39:01 +0530

    Invoices date selector on add invoice made consistent with other selector
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/common/directives/common-directive.module.ts
+++ src/app/common/directives/common-directive.module.ts
@@ -1,11 +1,12 @@
 // import DialogContainerComponent from ''
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 // import { LiabilityrightComponent } from './component/customer/accounts/liabilities/liabilityright/liabilityright.component';
-import { RadioGroupDirectiveDirective } from './radio-group-directive.directive';
-import { CustomRightAnimationDirective } from './custom-right-animation.directive';
-import { FormatNumberDirective } from 'src/app/format-number.directive';
-import { SkeletonLoadingDirective } from './skeleton-loading.directive';
-import { InputValidationDirective, AlphanumricDirective, TextOnlyDirective, Formatter } from './input-validation.directive';
+import {RadioGroupDirectiveDirective} from './radio-group-directive.directive';
+import {CustomRightAnimationDirective} from './custom-right-animation.directive';
+import {FormatNumberDirective} from 'src/app/format-number.directive';
+import {SkeletonLoadingDirective} from './skeleton-loading.directive';
+import {AlphaNumericDirective, Formatter, NumberOnlyDirective, TextOnlyDirective} from './number-only.directive';
+
 // import {AppModule} from "../app.module";
 
 
@@ -16,11 +17,11 @@ import { InputValidationDirective, AlphanumricDirective, TextOnlyDirective, Form
     SkeletonLoadingDirective,
     FormatNumberDirective,
     TextOnlyDirective,
-    InputValidationDirective,
-    AlphanumricDirective,
+    NumberOnlyDirective,
+    AlphaNumericDirective,
     Formatter
   ],
-  exports: [RadioGroupDirectiveDirective, SkeletonLoadingDirective, FormatNumberDirective,InputValidationDirective,AlphanumricDirective,TextOnlyDirective,Formatter],
+  exports: [RadioGroupDirectiveDirective, SkeletonLoadingDirective, FormatNumberDirective, NumberOnlyDirective, AlphaNumericDirective, TextOnlyDirective, Formatter],
   imports: [
 
     // AppModule
