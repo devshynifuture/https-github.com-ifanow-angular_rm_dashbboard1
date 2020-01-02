a0f6d77d803bdab5a83030509b3364a70981ef79
devshyni <devshyni@futurewise.co.in>
2020-01-02 12:09:49 +0530

    num formatting

--- src/app/common/directives/alphanumric.directive.ts
+++ src/app/common/directives/alphanumric.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
+import { element } from 'protractor';
 @Directive({
   selector: '[appAlphanumric]'
 })
@@ -7,6 +8,8 @@ export class AlphanumricDirective {
   constructor( private _el: ElementRef,
     private renderer: Renderer2) { }
     @HostListener('input', ['$event']) onInputChange(event) {
+      // console.log('ye toh mera wala log hai',element)
+      // console.log('ye toh mera wala log hai',this._el.nativeElement.value)
       let initialValue = this._el.nativeElement.value;
       var k = initialValue;
       return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || (k >= 48 && k <= 57));
