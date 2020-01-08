2919153ce7299d5ad9f536f9591090d7698faf9c
devshyni <devshyni@futurewise.co.in>
2020-01-08 16:51:39 +0530

    ivoice added

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/invoice/invoice.component.ts
@@ -26,15 +26,6 @@ export interface PeriodicElement {
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss'],
   providers: [
-    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
-    // application's root module. We provide it at the component level here, due to limitations of
-    // our example generation script.
-    // {
-    //   provide: DateAdapter,
-    //   useClass: MomentDateAdapter,
-    //   deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
-    // },
-    // { provide: MAT_DATE_LOCALE, useValue: 'en' },
     {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS2},
   ],
 
@@ -53,8 +44,7 @@ export class InvoiceComponent implements OnInit {
     {name: 'Registered Business - Composition', value: 1},
     {name: 'Unregistered Business', value: 2}
   ];
-  // numValidator = ValidatorType.NUMBER_ONLY;
-  // numKeyValidator = ValidatorType.NUMBER_KEY_ONLY;
+
   @ViewChild('invoiceTemplate', {static: false}) invoiceTemplate: ElementRef;
 
   constructor(public utils: UtilService, public enumService: EnumServiceService, public subInjectService: SubscriptionInject,
@@ -141,7 +131,6 @@ export class InvoiceComponent implements OnInit {
     this.getInvoiceData(data);
     this.getRecordPayment(data);
   }
-
   ngOnInit() {
 
     this.advisorId = AuthService.getAdvisorId();
@@ -173,11 +162,6 @@ export class InvoiceComponent implements OnInit {
     }
   }
 
-  // onDateInput(event){
-  //   this.paymentDate += this.utils.dateFormat(event);
-  // }
-
-
   keyPress(event: any) {
     console.log(event.target.value.length);
     const pattern = /[0-9\+\-\ ]/;
@@ -189,7 +173,6 @@ export class InvoiceComponent implements OnInit {
   }
 
   dontAllowTyping(event, maxLength: number) {
-    // console.log(this.rPayment.value());
     if (event.target.value.length > maxLength) {
       event.preventDefault();
     }
@@ -408,7 +391,7 @@ export class InvoiceComponent implements OnInit {
   }
 
   updateInvoice() {
-    if (this.taxStatus[0] == 'SGST(9%)|CGST(9%)') {
+    if (this.editPayment.value.taxStatus == 'SGST(9%)|CGST(9%)') {
       this.finAmountC = (9 / 100) * this.editPayment.controls.finalAmount.value;
       this.finAmountS = (9 / 100) * this.editPayment.controls.finalAmount.value;
       this.finAmount = this.finAmountC + this.finAmountS + parseInt(this.editPayment.controls.finalAmount.value);
