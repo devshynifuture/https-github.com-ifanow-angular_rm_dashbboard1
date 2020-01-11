96a5f5ad5d436f2a8a158f30442d3159755b254e
sarvesh <sarvesh@futurewise.co.in>
2020-01-11 16:35:13 +0530

    add

--- src/app/test/form-test/form-test.component.ts
+++ src/app/test/form-test/form-test.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ValidatorType } from '../../services/util.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { EventService } from 'src/app/Data-service/event.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-test',
@@ -18,13 +20,27 @@ export class FormTestComponent implements OnInit {
   // personNameValidator = ValidatorType.PERSON_NAME;
   validatorType = ValidatorType;
 
-  constructor(private fb: FormBuilder, private sanitizer: DomSanitizer) {
+  constructor(private eventService: EventService, private fb: FormBuilder, private sanitizer: DomSanitizer, private http: HttpClient) {
   }
   inputString;
   // numKeyValidator = ValidatorType.NUMBER_KEY_ONLY;
   ngOnInit() {
     console.log("Document preview", window.history.state)
     this.inputString = this.sanitizer.bypassSecurityTrustHtml(window.history.state.docText);
+    (this.inputString.changingThisBreaksApplicationSecurity) ? this.inputString : this.inputString = { changingThisBreaksApplicationSecurity: "Go Back To documents to view Pdf" };
+    let obj =
+    {
+      "htmlInput": this.inputString.changingThisBreaksApplicationSecurity,
+      "name": "pdfTest"
+    }
+    this.http.post('http://dev.ifanow.in:8080/futurewise/api/v1/web//subscription/html-to-pdf', obj, { responseType: 'blob' }).subscribe(
+      data => {
+        var file = new Blob([data], { type: 'application/pdf' });
+        var fileURL = URL.createObjectURL(file);
+        window.open(fileURL);
+      }
+    )
+
     console.log(this.inputString)
     this.testForm = this.fb.group({
       id: [],
