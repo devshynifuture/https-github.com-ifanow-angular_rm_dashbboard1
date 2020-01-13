4c0bbddb26c7f27d5d330b2a8855d521ec463ae3
Gaurav <gaurav@futurewise.co.in>
2020-01-13 10:23:44 +0530

    Invoice issues resolved GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/terms-agreement/terms-agreement.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/common-subscription-component/terms-agreement/terms-agreement.component.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, forwardRef, Input, OnInit, Output, Renderer2 } from '@angular/core';
-import { FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { SubscriptionInject } from '../../../subscription-inject.service';
-import { HowToUseDialogComponent } from '../how-to-use-dialog/how-to-use-dialog.component';
-import { MatDialog, TooltipPosition } from '@angular/material';
-import { SubscriptionService } from '../../../subscription.service';
-import { EventService } from 'src/app/Data-service/event.service';
-import { UtilService } from 'src/app/services/util.service';
-import { AuthService } from 'src/app/auth-service/authService';
-import { Router } from '@angular/router';
-import { escapeRegExp } from '@angular/compiler/src/util';
-import { HttpClient } from '@angular/common/http';
-import { tableHtml } from './document-preview'
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output, Renderer2} from '@angular/core';
+import {FormControl, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {SubscriptionInject} from '../../../subscription-inject.service';
+import {HowToUseDialogComponent} from '../how-to-use-dialog/how-to-use-dialog.component';
+import {MatDialog, TooltipPosition} from '@angular/material';
+import {SubscriptionService} from '../../../subscription.service';
+import {EventService} from 'src/app/Data-service/event.service';
+import {UtilService} from 'src/app/services/util.service';
+import {AuthService} from 'src/app/auth-service/authService';
+import {Router} from '@angular/router';
+import {escapeRegExp} from '@angular/compiler/src/util';
+import {HttpClient} from '@angular/common/http';
+import {tableHtml} from './document-preview';
 
 @Component({
   selector: 'app-terms-agreement',
@@ -34,14 +34,14 @@ export class TermsAgreementComponent implements OnInit {
   serviceData: any;
 
   constructor(private route: Router, public subInjectService: SubscriptionInject, public dialog: MatDialog,
-    public subService: SubscriptionService, private eventService: EventService, private render: Renderer2,
-    private http: HttpClient) {
+              public subService: SubscriptionService, private eventService: EventService, private render: Renderer2,
+              private http: HttpClient) {
     this.dataSub = this.subInjectService.singleProfileData.subscribe(
       data => this.getcommanFroalaData(data)
     );
   }
 
-  matTooltipOption: TooltipPosition[] = ['above']
+  matTooltipOption: TooltipPosition[] = ['above'];
   @Input() quotationDesignE;
   @Input() componentFlag: string;
   @Output() valueChange = new EventEmitter();
@@ -54,13 +54,13 @@ export class TermsAgreementComponent implements OnInit {
   set upperData(upperData) {
     this._upperData = upperData;
     console.log('Terms and agreemennt upperData: ', upperData);
-    this.getDataTerms(upperData)
+    this.getDataTerms(upperData);
     if (upperData && upperData.documentData) {
       // this.changeDisplay();
     }
     this.advisorId = AuthService.getAdvisorId();
     this.getPlanServiceData();
-  };
+  }
 
   get upperData() {
     return this._upperData;
@@ -91,7 +91,7 @@ export class TermsAgreementComponent implements OnInit {
   Close() {
     // this.subInjectService.rightSideData(value);
     // this.valueChange.emit(this.quotationDesignE);
-    this.eventService.changeUpperSliderState({ state: 'close' });
+    this.eventService.changeUpperSliderState({state: 'close'});
 
   }
 
@@ -104,21 +104,21 @@ export class TermsAgreementComponent implements OnInit {
     };
     this.subService.getSettingPlanServiceData(obj).subscribe(
       data => this.serviceData = data,
-      err => this.eventService.openSnackBar("Something went wrong", "dismiss")
-    )
+      err => this.eventService.openSnackBar('Something went wrong', 'dismiss')
+    );
   }
 
   copyName(data) {
-    console.log(data)
+    console.log(data);
     const text = data.currentTarget.childNodes[0].innerHTML;
-    let tag = this.render.createElement("input")
-    tag.value = text
+    const tag = this.render.createElement('input');
+    tag.value = text;
     document.body.appendChild(tag);
     tag.focus();
     tag.select();
     document.execCommand('copy');
     document.body.removeChild(tag);
-    this.eventService.openSnackBar("text copied", "dismiss")
+    this.eventService.openSnackBar('text copied', 'dismiss');
   }
 
   onSubmit() {
@@ -127,23 +127,25 @@ export class TermsAgreementComponent implements OnInit {
   }
 
   getDataTerms(data) {
-    this.dataTerms = data.documentData
+    this.dataTerms = data.documentData;
   }
 
   openDocumentPreview() {
-    const obj =
-    {
+    const obj = {
       documentRepositoryId: this._upperData.documentData.documentRepositoryId
-    }
+    };
     this.subService.getQuotationServiceData(obj).subscribe(
       data => console.log(data)
-    )
+    );
     this.serviceData.forEach(element => {
-      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp("$(service_" + element.id + ")"), 'g'), element.serviceName);
-      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp("$(client_name)"), 'g'), "Ronak Hindocha");
-      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp("$(service_fee_" + element.id + ")"), 'g'), tableHtml);
+      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp('$(service_' + element.id + ')'), 'g'),
+        element.serviceName);
+      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp('$(client_name)'), 'g'),
+        'Ronak Hindocha');
+      this.dataTerms.docText = this.dataTerms.docText.replace(new RegExp(escapeRegExp('$(service_fee_' + element.id + ')'), 'g'),
+        tableHtml);
     });
-    this.route.navigate(['test'], { state: { ...this.dataTerms } });
+    this.route.navigate(['test'], {state: {...this.dataTerms}});
 
   }
 
@@ -193,16 +195,16 @@ export class TermsAgreementComponent implements OnInit {
       name: data.name,
     };
     this.subService.updateDocumentData(obj).subscribe(
-      data => this.getResponseData(data)
+      responseData => this.getResponseData(responseData)
     );
   }
 
   getResponseData(data) {
     console.log(data);
     if (data == 1) {
-      this.eventService.openSnackBar("Document added successfully", "OK")
+      this.eventService.openSnackBar('Document added successfully', 'OK');
     }
-    this.eventService.changeUpperSliderState({ state: 'close' });
+    this.eventService.changeUpperSliderState({state: 'close'});
   }
 
   // Begin ControlValueAccesor methods.
