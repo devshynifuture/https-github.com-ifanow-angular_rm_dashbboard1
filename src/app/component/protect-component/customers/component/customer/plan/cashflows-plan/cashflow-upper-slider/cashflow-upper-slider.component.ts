04e759c4e025732e9ad6d96b715d53a54c2dc633
aman9893 <aman@futurewise.co.in>
2020-01-16 16:19:53 +0530

    add

--- src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
@@ -9,14 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashflowUpperSliderComponent implements OnInit {
 
+  displayedColumns: string[] = ['description', 'month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10', 'month11', 'month12', 'total'];
+  dataSource = ELEMENT_DATA;
+
   constructor(private subInjectService: SubscriptionInject,
     private eventService: EventService) { }
 
   ngOnInit() {
   }
 
+
+
   close() {
     this.eventService.changeUpperSliderState({ state: 'close' });
   }
 
 }
+
+
+export interface uppertablebox {
+  description: string;
+  month1: string;
+  month2: string;
+  month3: string;
+  month4: string;
+  month5: string;
+  month6: string;
+  month7: string;
+  month8: string;
+  month9: string;
+  month10: string;
+  month11: string;
+  month12: string;
+  total: string;
+}
+
+
+const ELEMENT_DATA: uppertablebox[] = [
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+
+];
