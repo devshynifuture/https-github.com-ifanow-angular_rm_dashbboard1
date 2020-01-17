9695601e3c421e01e28e82a5f2be4a40838d89fb
Khizar <khizaruddin@futurewise.co.in>
2020-01-17 16:50:12 +0530

    tables incashflow

--- src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
@@ -13,18 +13,35 @@ export class CashflowUpperSliderComponent implements OnInit {
 
   displayedColumns: string[] = ['description', 'month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10', 'month11', 'month12', 'total'];
   dataSource = ELEMENT_DATA;
+  dataSource1 = ELEMENT_DATA2;
+  dataSource2 = ELEMENT_DATA3;
 
   constructor(private subInjectService: SubscriptionInject,
     private eventService: EventService,
     public dialog: MatDialog) { }
 
   data;
+
+  isAssetsTable: boolean = false;
+  isSurplusTable: boolean = false;
   editMode: boolean = false;
   year: string = '';
 
   ngOnInit() {
     console.log(this.data);
-    this.year = this.data.year;
+
+    (this.data.tableInUse === 'assets') ? this.isAssetsTable = true : this.isAssetsTable = false;
+    if (this.data.tableInUse === 'surplus') {
+      this.isSurplusTable = true;
+      this.year = this.data.financialYear;
+    } else {
+      this.isSurplusTable = false;
+      this.year = this.data.year;
+    }
+  }
+
+  isGroup(index, item): boolean {
+    return item.groupName;
   }
 
   close() {
@@ -69,7 +86,36 @@ export interface uppertablebox {
   total: string;
 }
 
+export interface Group {
+  groupName: string
+}
+
 
 const ELEMENT_DATA: uppertablebox[] = [
   { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
 ];
+
+
+const ELEMENT_DATA2: (uppertablebox | Group)[] = [
+  { groupName: 'Goal Based' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { groupName: 'Non Goal Based' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+]
+
+export const ELEMENT_DATA3 = [
+  { groupName: 'NET SURPLUS & ROLLING BALANCE' },
+  { description: 'Ad hoc', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: 'Recurring', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: 'Cumulative/Rolling', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { groupName: 'MONTHLY ALLOCATIONS' },
+  { description: 'Amount allocated', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '# allocations', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { groupName: 'LUMPSUM ALLOCATIONS' },
+  { description: 'Amount allocated', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '# allocations', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+]
