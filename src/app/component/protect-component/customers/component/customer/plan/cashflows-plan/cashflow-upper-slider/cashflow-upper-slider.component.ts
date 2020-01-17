6fb1c872ad9debabd9b559e86a69f56478502acc
aman9893 <aman@futurewise.co.in>
2020-01-17 16:46:36 +0530

    add

--- src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/cashflows-plan/cashflow-upper-slider/cashflow-upper-slider.component.ts
@@ -11,7 +11,7 @@ import { CashflowAddComponent } from './cashflow-add/cashflow-add.component';
 })
 export class CashflowUpperSliderComponent implements OnInit {
 
-  displayedColumns: string[] = ['description', 'month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10', 'month11', 'month12', 'total'];
+  displayedColumns: string[] = ['description', 'month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10', 'month11', 'month12', 'total', 'remove'];
   dataSource = ELEMENT_DATA;
 
   constructor(private subInjectService: SubscriptionInject,
@@ -67,9 +67,10 @@ export interface uppertablebox {
   month11: string;
   month12: string;
   total: string;
+  remove: string;
 }
 
 
 const ELEMENT_DATA: uppertablebox[] = [
-  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121' },
+  { description: '2020', month1: '25', month2: '21', month3: '2,10,000', month4: '121', month5: '121', month6: '121', month7: '12', month8: '12', month9: '12', month10: '445', month11: '12', month12: '12', total: '121', remove: '' },
 ];
