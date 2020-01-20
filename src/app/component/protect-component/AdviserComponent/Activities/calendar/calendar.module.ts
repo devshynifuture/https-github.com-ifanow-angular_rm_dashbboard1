76cfe8471a493c7eb96ec99ba2877ad40c5197d2
ani <aniruddha@futurewise.co.in>
2020-01-20 15:37:47 +0530

    file attach and date formate added

--- src/app/component/protect-component/AdviserComponent/Activities/calendar/calendar.module.ts
+++ src/app/component/protect-component/AdviserComponent/Activities/calendar/calendar.module.ts
@@ -5,7 +5,9 @@ import {CommonComponentModule} from '../../../common-component/common-component.
 import {MaterialModule} from 'src/app/material/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CalendarRoutingModule} from './calendar-routing.module';
-
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
+import { MY_FORMATS2 } from 'src/app/constants/date-format.constant';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,11 @@ import {CalendarRoutingModule} from './calendar-routing.module';
     ReactiveFormsModule,
     CalendarRoutingModule,
 
+  ],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS2 },
   ],
   entryComponents: [EventDialog]
 })
