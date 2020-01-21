cb6cc99ccabfd661729a2c8fe3fdc2ec5d669a77
sarvesh <sarvesh@futurewise.co.in>
2020-01-21 12:04:43 +0530

    add

---
+++ src/app/component/protect-component/customers/component/customer/customer-activity/action-plan-activity/action-plan-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-plan-activity',
+  templateUrl: './action-plan-activity.component.html',
+  styleUrls: ['./action-plan-activity.component.scss']
+})
+export class ActionPlanActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/component/protect-component/customers/component/customer/customer-activity/notes-activity/notes-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-activity',
+  templateUrl: './notes-activity.component.html',
+  styleUrls: ['./notes-activity.component.scss']
+})
+export class NotesActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
