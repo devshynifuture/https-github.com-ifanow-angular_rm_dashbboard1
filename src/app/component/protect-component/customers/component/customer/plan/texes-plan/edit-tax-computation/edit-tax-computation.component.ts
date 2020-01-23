ff5f93e9f9586e3c650638cc0ea765c610554f74
sunny <sunny@futurewise.co.in>
2020-01-23 13:22:23 +0530

    add

---
+++ src/app/component/protect-component/customers/component/customer/plan/texes-plan/edit-tax-computation/edit-tax-computation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tax-computation',
+  templateUrl: './edit-tax-computation.component.html',
+  styleUrls: ['./edit-tax-computation.component.scss']
+})
+export class EditTaxComputationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
