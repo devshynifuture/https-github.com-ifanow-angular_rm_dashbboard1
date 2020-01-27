4a5ed97fc1b71c39db792242e466e38551daf8b0
sarvesh <sarvesh@futurewise.co.in>
2020-01-27 18:35:48 +0530

    add

--- src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/settings-subscription/preferences-settings/preferences-settings.component.ts
+++ src/app/component/protect-component/AdviserComponent/Subscriptions/subscription/settings-subscription/preferences-settings/preferences-settings.component.ts
@@ -37,7 +37,7 @@ export class PreferencesSettingsComponent implements OnInit {
 
     this.viewMode = 'tab1';
     this.advisorId = AuthService.getAdvisorId();
-    (this.utilservice.checkSubscriptionastepData(2) == false) ? this.billerProfileData = undefined : this.billerProfileData = [{ isPrimary: false }]
+    (this.utilservice.checkSubscriptionastepData(2) == false) ? this.billerProfileData = undefined : this.billerProfileData = [{}, {}, {}]
     this.getProfileBillerData();
     this.getTemplate();
   }
