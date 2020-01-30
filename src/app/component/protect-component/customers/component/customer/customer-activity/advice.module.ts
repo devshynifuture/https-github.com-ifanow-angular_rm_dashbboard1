bcf6beca24957111d391eaea444e05108c9fa4ca
sarvesh <sarvesh@futurewise.co.in>
2020-01-30 18:41:31 +0530

    add

--- src/app/component/protect-component/customers/component/customer/customer-activity/advice.module.ts
+++ src/app/component/protect-component/customers/component/customer/customer-activity/advice.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerActivityRoutingModule } from './customer-activity-routing.module';
-import { CustomerActivityComponent } from './customer-activity.component';
+// import { CustomerActivityComponent } from './customer-activity.component';
 import { MaterialModule } from 'src/app/material/material';
 
 @NgModule({
-  declarations: [CustomerActivityComponent],
+  declarations: [],
   imports: [
     CommonModule,
     CustomerActivityRoutingModule,
