0446277fdf909b13955056af1fb23e985a9dcc12
devshyni <devshyni@futurewise.co.in>
2020-02-03 16:53:23 +0530

    online transactions

--- src/app/component/protect-component/AdviserComponent/transactions/transactions/transaction.entry.module.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/transactions/transaction.entry.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TransactionAddComponent } from '../transaction-add/transaction-add.component';
+import { CustomDirectiveModule } from 'src/app/common/directives/common-directive.module';
+import { OnlineTrasactionComponent } from '../overview-transactions/doTransaction/online-trasaction/online-trasaction.component';
+import { AddArnRiaCredentialsComponent } from '../settings-transactions/settings-manage-credentials/arn-ria-credentials/add-arn-ria-credentials/add-arn-ria-credentials.component';
+import { AddSubBrokerCredentialsComponent } from '../settings-transactions/settings-manage-credentials/sub-broker-team-member/add-sub-broker-credentials/add-sub-broker-credentials.component';
+import { PurchaseTrasactionComponent } from '../overview-transactions/doTransaction/purchase-trasaction/purchase-trasaction.component';
 
 export const componentList = [
-    TransactionAddComponent
+    TransactionAddComponent,
+    OnlineTrasactionComponent,
+    AddArnRiaCredentialsComponent,
+    AddSubBrokerCredentialsComponent,
+    PurchaseTrasactionComponent,
 ]
 @NgModule({
     declarations: componentList,
@@ -13,7 +22,8 @@ export const componentList = [
         CommonModule,
         MaterialModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CustomDirectiveModule,
     ],
     exports: [
         FormsModule,
