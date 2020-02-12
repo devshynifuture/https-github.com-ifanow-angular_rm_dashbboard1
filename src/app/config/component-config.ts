35d390beb30dcc2ecfb35f048fce405ea2ba1922
sarvesh <sarvesh@futurewise.co.in>
2020-02-12 16:06:30 +0530

    add

--- src/app/config/component-config.ts
+++ src/app/config/component-config.ts
@@ -357,7 +357,8 @@ export const appConfig = {
   GET_AMCWISE_FOLIO: 'mutual-fund/folios/amc-wise/get',
   GET_SCHEMEWISE_FOLIO: 'mutual-fund/folios/scheme-wise/get',
   GET_MANDATE_DETAILS: 'bse/mandate/view',
-
+  GET_UNMAPPED_FOLIO: '/bse/user/mapped-unmapped-clients/view',
+  MAP_UNMAP_FOLIOS: '/map-iin',
   // cashflow 
   CASHFLOW_ADD_INCOME: 'cashflow/income/add',
   CASHFLOW_GET_INCOME: 'cashflow/income/get',
