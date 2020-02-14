2ffbdabd486bdad0179c036a78fe2ca7199b2c5c
Gayatri Pawar <gayatri@futurewise.co.in>
2020-02-14 13:10:11 +0530

    folio master and sip details

---
+++ src/app/component/protect-component/customers/component/common-component/folio-master-details/folio-master-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-folio-master-details',
+  templateUrl: './folio-master-details.component.html',
+  styleUrls: ['./folio-master-details.component.scss']
+})
+export class FolioMasterDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/component/protect-component/customers/component/common-component/sip-details/sip-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sip-details',
+  templateUrl: './sip-details.component.html',
+  styleUrls: ['./sip-details.component.scss']
+})
+export class SipDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
