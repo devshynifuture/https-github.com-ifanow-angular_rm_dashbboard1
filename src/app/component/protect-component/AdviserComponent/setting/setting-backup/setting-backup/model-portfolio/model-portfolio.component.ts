4deb1fa5cf8d6d8976f52e50744ae22715adbc6c
sunny <sunny@futurewise.co.in>
2020-02-19 19:36:12 +0530

    add

--- src/app/component/protect-component/AdviserComponent/setting/setting-backup/setting-backup/model-portfolio/model-portfolio.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-backup/setting-backup/model-portfolio/model-portfolio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from 'src/app/services/util.service';
+import { EventService } from 'src/app/Data-service/event.service';
+import { SubscriptionInject } from '../../../../Subscriptions/subscription-inject.service';
+import { AddModelPortfolioComponent } from '../../../setting-entry/add-model-portfolio/add-model-portfolio.component';
 
 @Component({
   selector: 'app-model-portfolio',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelPortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventService: EventService,
+    private utilService: UtilService, private subInjectService: SubscriptionInject) { }
 
   ngOnInit() {
   }
 
+  openAddModelPortfolio(data) {
+    const fragmentData = {
+      // flag: 'app-upper-setting',
+      id: 1,
+      data,
+      direction: 'top',
+      componentName: AddModelPortfolioComponent,
+      state: 'open'
+    };
+    const rightSideDataSub = this.eventService.changeUpperSliderState(fragmentData).subscribe(
+      sideBarData => {
+        console.log('this is sidebardata in subs subs : ', sideBarData);
+        if (UtilService.isDialogClose(sideBarData)) {
+          if (UtilService.isRefreshRequired(sideBarData)) {
+            console.log('this is sidebardata in subs subs 3 ani: ', sideBarData);
+          }
+          rightSideDataSub.unsubscribe();
+        }
+
+      }
+    );
+  }
 }
