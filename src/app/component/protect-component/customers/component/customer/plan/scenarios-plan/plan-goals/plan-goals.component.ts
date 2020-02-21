57b5233b9c84477a2276a483ae3a2a7747f0060e
aman9893 <aman@futurewise.co.in>
2020-02-21 19:07:39 +0530

    add

--- src/app/component/protect-component/customers/component/customer/plan/scenarios-plan/plan-goals/plan-goals.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/scenarios-plan/plan-goals/plan-goals.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class PlanGoalsComponent implements OnInit {
 
   constructor() { }
-  isLoading = true;
+  isLoading = false;
   displayedColumns: string[] = ['details', 'value', 'monthly', 'lumpsum'];
   dataSource = ELEMENT_DATA;
+
+  displayedColumns1: string[] = ['name', 'description', 'derived', 'adverse', 'moderate', 'optimum', 'radioOne', 'radioTwo'];
+  dataSource1 = ELEMENT_DATA1;
 
   ngOnInit() {
   }
@@ -28,6 +31,31 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   { details: " ", value: '48,13,000', monthly: '25,600', lumpsum: '25,88,500' },
   { details: " ", value: '48,13,000', monthly: '25,600', lumpsum: '25,88,500' },
+  { details: " ", value: '48,13,000', monthly: '25,600', lumpsum: '25,88,500' },
+  { details: " ", value: '48,13,000', monthly: '25,600', lumpsum: '25,88,500' },
+
+
+];
+
+
+
+export interface PeriodicElement1 {
+  name: string;
+  description: string;
+  derived: string;
+  adverse: string;
+  moderate: string;
+  optimum: string;
+  radioOne: string;
+  radioTwo: string;
+
+}
+
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  { name: "Basic liquidity ratio", description: 'This ratio indicates ones ability to meet mandatory monthly expenses & other emergency needs.', derived: ' ', adverse: '< 3', moderate: '3 - 6', optimum: '6 - 15', radioOne: '55.8', radioTwo: '62.3' },
+  { name: "Expanded liquidity ratio", description: ' Your ability to meet emergencies when it may not be possible to convert assets into cash.', derived: ' ', adverse: '< 3', moderate: '3 - 6', optimum: '6 - 15', radioOne: '55.8', radioTwo: '62.3' },
+
+
 
 ];
 
