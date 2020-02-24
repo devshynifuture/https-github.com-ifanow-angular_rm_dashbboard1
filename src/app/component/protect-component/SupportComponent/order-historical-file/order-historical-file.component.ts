bb3404e1e88a4603d7642859a466b62c1cd9c9a8
Khizar <khizaruddin@futurewise.co.in>
2020-02-24 19:54:44 +0530

    changes in order historical file right side slider cams validation done

--- src/app/component/protect-component/SupportComponent/order-historical-file/order-historical-file.component.ts
+++ src/app/component/protect-component/SupportComponent/order-historical-file/order-historical-file.component.ts
@@ -2,7 +2,7 @@ import { EventService } from './../../../../Data-service/event.service';
 import { UtilService } from './../../../../services/util.service';
 import { SubscriptionInject } from './../../AdviserComponent/Subscriptions/subscription-inject.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-order-historical-file',
@@ -11,39 +11,64 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class OrderHistoricalFileComponent implements OnInit {
   asOnDate: boolean = false;
+  formValidationFalseCount: number = 0;
+  mainFormValidationValue: boolean = false;
+  fromToSameError: boolean = false;
+  camsAsOnDatePastMaxDate: Date = new Date("1 January, 1993");
+  karvyOrFranklinPastMaxDate = new Date("1 January, 1990");
+  dateToday: Date = new Date();
+  dateYesterday: Date = new Date(new Date().setDate(new Date().getDate() - 1));
+  orderingFreq: {}[] = [
+    {
+      id: '1',
+      name: 'Yearly'
+    },
+    {
+      id: '2',
+      name: 'Monthly'
+    },
+    {
+      id: '3',
+      name: 'All at once'
+    }
+  ];
 
   constructor(
     private subInjectService: SubscriptionInject,
     private fb: FormBuilder,
-    private utilService: UtilService,
     private eventService: EventService
   ) { }
 
-  ngOnInit() {
-    // this.isOnlyAumSelected()
-    this.orderHistoryFileForm.get('selectFilesToOrder.cams').valueChanges.subscribe(val => {
-      console.log(val);
-      if (val['wbr22'] === true) {
-        this.asOnDate = true;
-      } else {
-        this.asOnDate = false;
-      }
-    });
-  }
+  resetDateAsOnDateAndFormCheckbox() {
+    if (this.orderHistoryFileForm.get('selectRta').value !== '1') {
+      this.asOnDate = false;
+      this.orderHistoryFileForm.get('orderingFreq').reset();
+      this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+      this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
 
-  dateRangeAndAsOnDateHandler(selector) {
-    if (selector === 'wbr22') {
-      if (this.orderHistoryFileForm.get('selectFileToOrder.cams.wbr22').value == true) {
-        if (this.asOnDate) {
-          this.asOnDate = false;
-        } else {
-          this.asOnDate = true;
+      for (const key in this.orderHistoryFileForm.get('selectFilesToOrder.cams')['controls']) {
+        if (this.orderHistoryFileForm.get('selectFilesToOrder.cams')['controls'].hasOwnProperty(key)) {
+          const element = this.orderHistoryFileForm.get('selectFilesToOrder.cams')['controls'][key];
+          element.value = false;
         }
       }
-    } else {
-      if (this.orderHistoryFileForm.get('selectFileToOrder.cams')) {
 
+      for (const key in this.orderHistoryFileForm.get('selectFilesToOrder.karvy')['controls']) {
+        if (this.orderHistoryFileForm.get('selectFilesToOrder.karvy')['controls'].hasOwnProperty(key)) {
+          const element = this.orderHistoryFileForm.get('selectFilesToOrder.karvy')['controls'][key];
+          element.value = false;
+        }
       }
+
+      for (const key in this.orderHistoryFileForm.get('selectFilesToOrder.franklin')['controls']) {
+        if (this.orderHistoryFileForm.get('selectFilesToOrder.franklin')['controls'].hasOwnProperty(key)) {
+          const element = this.orderHistoryFileForm.get('selectFilesToOrder.franklin')['controls'][key];
+          element.value = false;
+        }
+      }
+      this.camsAsOnDatePastMaxDate = this.karvyOrFranklinPastMaxDate;
+    } else {
+      this.camsAsOnDatePastMaxDate = new Date("1 January, 1993");
     }
   }
 
@@ -52,94 +77,138 @@ export class OrderHistoricalFileComponent implements OnInit {
     "selectArnRia": [, Validators.required],
     "fromDate": [, Validators.required],
     "toDate": [, Validators.required],
+    "asOnDate": [,],
     "orderingFreq": [, Validators.required],
     "selectFilesToOrder": this.fb.group({
       "cams": this.fb.group({
-        "wbr2": [,],
-        "wbr2a": [,],
-        "wbr49Active": [,],
-        "wbr49Ceased": [,],
-        "wbr9": [,],
+        "wbr2": [false,],
+        "wbr2a": [false,],
+        "wbr49Active": [false,],
+        "wbr49Ceased": [false,],
+        "wbr9": [false,],
         "wbr22": [false,]
       }),
       "karvy": this.fb.group({
-        "mfsd201": [,],
-        "mfsd243Active": [,],
-        "mfsd231Ceased": [,],
-        "mfsd211": [,],
-        "mfsd203": [,]
+        "mfsd201": [false,],
+        "mfsd243Active": [false,],
+        "mfsd231Ceased": [false,],
+        "mfsd211": [false,],
+        "mfsd203": [false,]
       }),
       "franklin": this.fb.group({
-        "myTransactionsForAPeriod": [,],
-        "activeSip": [,],
-        "ceasedSip": [,],
-        "investorFolioDetails": [,],
-        "clientsWiseAumOrWhoseBalExceedsN": [,]
+        "myTransactionsForAPeriod": [false,],
+        "activeSip": [false,],
+        "ceasedSip": [false,],
+        "investorFolioDetails": [false,],
+        "clientsWiseAumOrWhoseBalExceedsN": [false,]
       })
     }),
   });
 
-  isOnlyAumSelected() {
-    let selectedRta = false;
-    let onlyCamsWbr22Selected = false;
-    // for (const key in this.orderHistoryFileForm.controls) {
-    //   if (this.orderHistoryFileForm.controls.hasOwnProperty(key)) {
-    //     const element = this.orderHistoryFileForm.controls[key];
-    //     console.log('form key and value:::', key, element);
-    //     if (key === 'selectRta' && element.touched && element.value === '1') {
-    //       selectedRta = true;
-    //     }
-    //     if (key === 'selectFilesToOrder' && element['controls'] && selectedRta) {
-    //       for (const key in element.get('cams')['controls']) {
-    //         if (element.get('cams')['controls'].hasOwnProperty(key)) {
-    //           const el = element.get('cams')['controls'][key];
-    //           if (el.value === false) {
-    //             onlyCamsWbr22Selected = true;
-    //           } else {
-    //             onlyCamsWbr22Selected = false;
-    //           }
-    //         }
-    //       }
-    //       if (onlyCamsWbr22Selected) {
-    //         this.asOnDate = true;
-    //       } else {
-    //         this.asOnDate = false;
-    //       }
-    //     }
-    //   }
-    // }
-    // this.orderHistoryFileForm.get('selectFilesToOrder').valueChanges.subscribe(val => {
-    //   let trueCount = 0;
-    //   for (const key in val) {
-    //     if (val.hasOwnProperty(key)) {
-    //       if (key === 'cams') {
-    //         for (const k in val[key]) {
-    //           if (val[key].hasOwnProperty(k)) {
-    //             const element = val[key][k];
-    //             console.log(element);
-    //             if (val[key][k] === true) {
-    //               trueCount++;
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (trueCount > 1) {
-    //     this.asOnDate = false;
-    //   }
-    // });
+  ngOnInit() {
+    console.log("this is yesterday's date::::   ", this.dateYesterday);
+    // this.isOnlyAumSelected()
+    this.orderHistoryFileForm.valueChanges.subscribe(val => console.log(val));
+    this.orderHistoryFileForm.get('selectFilesToOrder.cams').valueChanges.subscribe(val => {
+      if (val['wbr2']) {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      } else if (val['wbr2a']) {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      } else if (val['wbr49Active']) {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      } else if (val['wbr49Ceased']) {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      } else if (val['wbr9']) {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      } else if (val['wbr22']) {
+        this.asOnDate = true;
+        this.orderHistoryFileForm.get('orderingFreq').setValue('3');
+        this.orderHistoryFileForm.get('asOnDate').setValidators(Validators.required);
+        this.orderHistoryFileForm.get('asOnDate').setErrors({ 'error': true });
+        this.orderHistoryFileForm.removeControl('fromDate');
+        this.orderHistoryFileForm.removeControl('toDate');
+      } else {
+        this.asOnDate = false;
+        this.orderHistoryFileForm.get('orderingFreq').reset();
+        this.orderHistoryFileForm.get('asOnDate').setValidators([]);
+        this.orderHistoryFileForm.addControl('fromDate', new FormControl('', Validators.required));
+        this.orderHistoryFileForm.addControl('toDate', new FormControl('', Validators.required));
+      }
+    });
+
   }
 
   dialogClose(flag) {
     this.subInjectService.changeNewRightSliderState({ state: 'close', refreshRequired: flag });
   }
 
+  validationOfSelectFilesToOrder(rtaValue) {
+    let count = 0;
+    let selectFormGroup;
+    (rtaValue === '1') ? selectFormGroup = 'cams' :
+      (rtaValue === '2') ? selectFormGroup = 'karvy' :
+        (rtaValue === '3') ? selectFormGroup = 'franklin' : null;
+
+    if (this.orderHistoryFileForm.get('selectRta').value === rtaValue) {
+      for (const key in this.orderHistoryFileForm.get(`selectFilesToOrder.${selectFormGroup}`)['controls']) {
+        if (this.orderHistoryFileForm.get(`selectFilesToOrder.${selectFormGroup}`)['controls'].hasOwnProperty(key)) {
+          const element = this.orderHistoryFileForm.get(`selectFilesToOrder.${selectFormGroup}`)['controls'][key];
+          if (element.value === false) {
+            count++;
+          }
+        }
+      }
+      if (count === (rtaValue === '1' ? 6 : 5)) {
+        this.orderHistoryFileForm.get(`selectFilesToOrder.${selectFormGroup}`).setErrors({ 'error': true });
+        this.orderHistoryFileForm.get(`selectFilesToOrder.${selectFormGroup}`).markAsTouched();
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  formValidations(whichTable) {
+    // console.log("this is formGroup::::::::::", whichTable);
+    let selectFileOrderValidation = false;
+    for (let key in whichTable.controls) {
+      if (key === 'selectFilesToOrder') {
+        selectFileOrderValidation = this.validationOfSelectFilesToOrder(this.orderHistoryFileForm.get('selectRta').value);
+      } else if (whichTable.get(key).invalid) {
+        whichTable.get(key).markAsTouched();
+        return (selectFileOrderValidation && false);
+      }
+    }
+    return (whichTable.valid) ? (selectFileOrderValidation && true) : (selectFileOrderValidation && false);
+  }
+
   orderHistoricalFileSave() {
-    if (this.utilService.formValidations(this.orderHistoryFileForm)) {
+    if (this.formValidations(this.orderHistoryFileForm)) {
       // api call
+      console.log('must be closed');
       this.dialogClose(false);
     } else {
+      this.formValidationFalseCount++;
       this.eventService.openSnackBar("Must fill required field", "DISMISS");
     }
   }
