fa3371188362f08eea77868d47f1f197abffd2fc
sunny <sunny@futurewise.co.in>
2020-02-25 15:57:03 +0530

    insurance

--- src/app/component/protect-component/customers/component/customer/plan/insurance-plan/add-new-insurance/add-new-insurance.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/insurance-plan/add-new-insurance/add-new-insurance.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-insurance.component.scss']
 })
 export class AddNewInsuranceComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'advice', 'icons'];
+  dataSource = ELEMENT_DATA;
+  displayedColumns1: string[] = ['position', 'name', 'weight', 'symbol', 'icons'];
+  dataSource1 = ELEMENT_DATA1;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+  advice: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 'Apollo Munich Optima Restore', name: '27,290/year', weight: 'Rahul Jain | 38Y',
+    symbol: '5,00,000', advice: 'Port policy'
+  },
+
+];
+
+export interface PeriodicElement1 {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+}
+
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  {
+    position: 'ICICI Lombard Health Suraksha (Recommended)',
+    name: '32,300/year', weight: 'Rahul Jain | 38Y', symbol: '20,00,000'
+  },
+  {
+    position: 'HDFC Ergo Health Super (Option 2)',
+    name: '35,100/year', weight: 'Rahul Jain | 38Y', symbol: '20,00,000'
+  },
+];
