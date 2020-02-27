66601be61ebc2bc935476cabcd972d37df5d7042
sunny <sunny@futurewise.co.in>
2020-02-27 19:26:09 +0530

    add

--- src/app/component/protect-component/AdviserComponent/setting/setting-preference/setting-preference.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-preference/setting-preference.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting-preference.component.scss']
 })
 export class SettingPreferenceComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
+  viewMode = 'tab1';
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: string;
+
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 'archit.gupta@acmefinancial.com', name: 'Verified' },
+  { position: 'welcome@acmefinancial.com', name: 'Verification in process' },
+  { position: 'info@acmefinancial.com', name: 'Verification failed' },
+
+];
