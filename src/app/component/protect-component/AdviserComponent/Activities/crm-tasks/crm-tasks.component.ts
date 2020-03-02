aa139a25ac4a6821e004050dd7055e91cce485c0
aman9893 <aman@futurewise.co.in>
2020-03-02 10:40:50 +0530

    add

--- src/app/component/protect-component/AdviserComponent/Activities/crm-tasks/crm-tasks.component.ts
+++ src/app/component/protect-component/AdviserComponent/Activities/crm-tasks/crm-tasks.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crm-tasks.component.scss']
 })
 export class CrmTasksComponent implements OnInit {
-
+  displayedColumns: string[] = ['checkbox', 'client', 'cat', 'des', 'value', 'advice', 'status', 'date', 'icons'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+export interface PeriodicElement {
+  client: string;
+  cat: string;
+  des: string;
+  value: string;
+  advice: string;
+  status: string;
+  date: string;
+  icons: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { client: 'Rahul Jain', cat: 'Mutual Funds', des: 'HDFC Equity fund - Regular plan - Growth option | 098098883', value: 'Rahul Jain', advice: 'STP 5,000/month to HDFC Dynamic bond fund regular plan monthly dividend', status: 'ACCEPTED', date: '05/09/2019', icons: '' },
+];
