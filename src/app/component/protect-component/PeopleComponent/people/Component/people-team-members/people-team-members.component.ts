e5d177962814c568051aecdc1f308eebc64e9288
sarvesh <sarvesh@futurewise.co.in>
2020-03-03 11:53:12 +0530

    add

---
+++ src/app/component/protect-component/PeopleComponent/people/Component/people-team-members/people-team-members.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-team-members',
+  templateUrl: './people-team-members.component.html',
+  styleUrls: ['./people-team-members.component.scss']
+})
+export class PeopleTeamMembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
