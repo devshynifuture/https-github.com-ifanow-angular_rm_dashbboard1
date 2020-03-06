fef448e550fa3329b6619ed3ddd05e2a75143be8
Sagar Shah <sagar.shah@futurewise.co.in>
2020-03-06 12:33:34 +0530

    revert ssl changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // APIEndpoint: 'http://localhost:8080/futurewise/api/v1/web/'
-  APIEndpoint: 'https://dev.ifanow.in:8080/futurewise/api/v1/web/',
-  GmailAPIEndpoint: 'https://dev.ifanow.in:8080/email-server/api/v1/web/',
-  TransactionPlatform: 'https://dev.ifanow.in:8080/transaction-platform/api/v1/web/',
+  APIEndpoint: 'http://dev.ifanow.in:8080/futurewise/api/v1/web/',
+  GmailAPIEndpoint: 'http://dev.ifanow.in:8080/email-server/api/v1/web/',
+  TransactionPlatform: 'http://dev.ifanow.in:8080/transaction-platform/api/v1/web/',
   PostalUrl: 'https://api.postalpincode.in/pincode/',
 
   // APIEndpoint:'http://dev.ifanow.in:8080/'
