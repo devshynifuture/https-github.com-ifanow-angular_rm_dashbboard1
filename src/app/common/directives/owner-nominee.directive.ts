a8a81e7b92bb45c139658277382c67a0861051d6
ani <aniruddha@futurewise.co.in>
2020-03-07 18:06:29 +0530

    owner nominee directive created

---
+++ src/app/common/directives/owner-nominee.directive.ts
@@ -0,0 +1,207 @@
+import { Directive, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'src/app/auth-service/authService';
+import { FormBuilder, FormArray, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/component/protect-component/customers/component/customer/customer.service';
+
+@Directive({
+  selector: '[appOwnerNominee]'
+})
+export class OwnerNomineeDirective {
+  advisorId: any;
+  ownerData: any;
+  clientId: any;
+  sendData: any;
+  @Input()set data(data) {
+    this.ownerData = data;
+    console.log('1111121212121212121212 OwnerColumnComponent data : ', data);
+    this.getListFamilyMem();
+  }
+
+  @Input()set callMethod(callMethod:any){
+    switch (callMethod.methodName) {
+      case "checkOwnerType":
+        this.checkOwnerType(callMethod.ParamValue);
+      break;
+
+      case "onChangeJointOwnership":
+        this.onChangeJointOwnership(callMethod.ParamValue);
+      break;
+
+      case "disabledMember":
+        this.disabledMember(callMethod.ParamValue);
+      break;
+
+      default:
+        break;
+    }
+  }
+
+  @Output() valueChange = new EventEmitter();
+  @Output() valueChange1 = new EventEmitter();
+  constructor(private fb: FormBuilder, private custumService: CustomerService) { }
+
+  getListFamilyMem():any {
+    this.advisorId = AuthService.getAdvisorId();
+    this.clientId = AuthService.getClientId();
+    const obj = {
+      advisorId: this.advisorId,
+      clientId: this.clientId
+    };
+    this.custumService.getListOfFamilyByClient(obj).subscribe(
+      data => this.getListOfFamilyByClientRes(data)
+    );
+  }
+
+  getListOfFamilyByClientRes(data) {
+    console.log('family Memebers', data);
+    if (data.familyMembersList && data.familyMembersList.length > 0) {
+      data.familyMembersList.forEach((singleData) => {
+        singleData['disable'] = false;
+      });
+    }
+    this.sendData = data.familyMembersList;
+    this.valueChange1.emit(this.sendData);
+  }
+
+  get getCoOwner() {
+    return this.ownerData.get('getCoOwnerName') as FormArray;
+  }
+
+  get getNominee() {
+    return this.ownerData.get('getNomineeName') as FormArray;
+  }
+
+  removeCoOwner(item) {
+    this.getCoOwner.removeAt(item);
+  }
+
+  addNewCoOwner(data) {
+    this.getCoOwner.push(this.fb.group({
+      ownerName: "", ownershipPerc: null, familyMemberId: null
+    }));
+  }
+
+  disabledMember(value){
+    this.sendData.forEach(element => {
+      for(let e in this.getCoOwner.controls){
+        const arrayCon:any = this.getCoOwner.controls[e];
+        if(arrayCon.value.ownerName != ''){
+          if(element.userName == arrayCon.value.ownerName){
+            element.disable = true;
+            return;
+          }
+          else{
+            element.disable = false;
+          }
+        }
+      }
+    });
+  }
+
+  get data() {
+    return this.ownerData;
+  }
+
+  getOwnerName(value) {
+    // console.log('selected', value);
+    // value.familyList = this.family;
+    this.valueChange.emit(value);
+  }
+
+  checkOwnerType(value){
+    if(value == '2'){
+    // this.reqError = false;
+      this.ownerData.controls['ownerName'].setValidators(Validators.required);
+      this.ownerData.get('ownerName').updateValueAndValidity();
+      for (let element in this.ownerData.controls) {
+        console.log(element)
+        if(element == 'getCoOwnerName'){
+          for(let e in this.getCoOwner.controls){
+            // this.getCoOwner.removeAt(parseInt(e));
+            const arrayCon:any = this.getCoOwner.controls[e];
+            for(let i in arrayCon.controls){
+              if(i== "ownerName"){
+              arrayCon.get(i).setValue('');
+              // arrayCon.get(i).setErrors({required:false});
+              }
+              arrayCon.get(i).setValidators([]);
+              arrayCon.get(i).updateValueAndValidity();
+            }
+          }
+        }
+      }
+    }
+    else{
+      if(this.ownerData.value.getCoOwnerName.length < 2){
+        for(let i = 0; i < 1 ; i++){
+          this.addNewCoOwner(true);
+        }
+      }
+      // this.disabledMember();
+      this.ownerData.controls['ownerName'].setValidators([]);
+      this.ownerData.get('ownerName').updateValueAndValidity();
+      for (let element in this.ownerData.controls) {
+        console.log(element)
+        if(element == 'getCoOwnerName'){
+          for(let e in this.getCoOwner.controls){
+            const arrayCon:any = this.getCoOwner.controls[e];
+            for(let i in arrayCon.controls){
+              if(i != 'familyMemberId'){
+                arrayCon.controls[i].setValidators(Validators.required);
+                arrayCon.get(i).updateValueAndValidity();
+              }
+            }
+          }
+        }
+      }
+    }
+    this.valueChange.emit(this.ownerData.controls['getCoOwnerName']);
+  }
+  showError:boolean = false;
+  nexNomineePer:any;
+  showErrorOwner:boolean =false;
+  onChangeJointOwnership(data) {
+    if (data == 'owner') {
+      this.nexNomineePer = 0;
+      
+      for(let e in this.getCoOwner.controls){
+        const arrayCon:any = this.getCoOwner.controls[e];
+        
+            this.nexNomineePer += arrayCon.value.ownershipPerc;
+          
+              if (this.nexNomineePer > 100 || this.nexNomineePer < 100) {
+                this.showErrorOwner = true;
+                if(e == "1"){
+                  arrayCon.controls['ownershipPerc'].setErrors({'incorrect': true});
+                  arrayCon.get('ownershipPerc').updateValueAndValidity();
+                }
+                console.log('show error Percent cannot be more than 100%')
+              } else {
+                this.showErrorOwner = false
+                // this.showErrorCoOwner = false;
+                arrayCon.controls['ownershipPerc'].setErrors({'incorrect': false});
+                arrayCon.get('ownershipPerc').updateValueAndValidity();
+              }
+            
+      }
+      // this.getCoOwner.value.forEach(element => {
+      //   this.nexNomineePer += (element.ownershipPerc) ? parseInt(element.ownershipPerc) : null;
+      // });
+      // this.nexNomineePer = this.fixedDeposit.controls.ownerPercent.value + this.nexNomineePer
+     
+    } else {
+      this.nexNomineePer = 0;
+
+      this.getNominee.value.forEach(element => {
+        this.nexNomineePer += (element.ownershipPer) ? parseInt(element.ownershipPer) : null;
+      });
+      if (this.nexNomineePer > 100 ) {
+        this.showError = true
+        console.log('show error Percent cannot be more than 100%')
+      } else {
+        this.showError = false
+      }
+    }
+    this.valueChange.emit(this.ownerData.controls['getCoOwnerName']);
+  }
+}
