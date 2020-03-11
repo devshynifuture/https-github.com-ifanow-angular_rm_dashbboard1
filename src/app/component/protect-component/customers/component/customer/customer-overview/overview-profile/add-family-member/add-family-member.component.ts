1fe13e7ce86ee919c1d5ed7a12063cb178c0d1b1
aman9893 <aman@futurewise.co.in>
2020-03-11 19:45:15 +0530

    add new changes

---
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/add-family-member/add-family-member.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-family-member',
+  templateUrl: './add-family-member.component.html',
+  styleUrls: ['./add-family-member.component.scss']
+})
+export class AddFamilyMemberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
