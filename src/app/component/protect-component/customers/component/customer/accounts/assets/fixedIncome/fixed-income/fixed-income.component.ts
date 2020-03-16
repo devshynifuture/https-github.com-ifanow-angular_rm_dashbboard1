a7f99fb596ec7a54eb158b3097f662aa8b3c56d4
ani <aniruddha@futurewise.co.in>
2020-03-16 20:32:53 +0530

    4 assets done

--- src/app/component/protect-component/customers/component/customer/accounts/assets/fixedIncome/fixed-income/fixed-income.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/fixedIncome/fixed-income/fixed-income.component.ts
@@ -350,7 +350,7 @@ export class FixedIncomeComponent implements OnInit {
         if (value == 'FIXED DEPOSIT') {
           this.customerService.deleteFixedDeposite(data.id).subscribe(
             data => {
-              this.eventService.openSnackBar('Fixed deposite is deleted', 'Dismiss');
+              this.eventService.openSnackBar('Fixed deposit is deleted', 'Dismiss');
               dialogRef.close();
               this.getFixedDepositList();
             },
@@ -359,7 +359,7 @@ export class FixedIncomeComponent implements OnInit {
         } else if (value == 'RECURRING DEPOSIT') {
           this.customerService.deleteRecurringDeposite(data.id).subscribe(
             data => {
-              this.eventService.openSnackBar('Recurring deposite is deleted', 'Dismiss');
+              this.eventService.openSnackBar('Recurring deposit is deleted', 'Dismiss');
               dialogRef.close();
               this.getRecurringDepositList();
             },
@@ -395,7 +395,7 @@ export class FixedIncomeComponent implements OnInit {
   }
 
   openPortfolioSummary(value, data) {
-    let popupHeaderText = !!data ? 'Edit Fixed deposits' : 'Add Fixed deposits';
+    let popupHeaderText = !!data ? 'Edit Fixed deposit' : 'Add Fixed deposit';
     const fragmentData = {
       flag: value,
       data,
@@ -453,7 +453,7 @@ export class FixedIncomeComponent implements OnInit {
   }
 
   openAddRecurringDeposit(data) {
-    let popupHeaderText = !!data ? 'Edit Recurring deposits' : 'Add Recurring deposits';
+    let popupHeaderText = !!data ? 'Edit Recurring deposit' : 'Add Recurring deposit';
     const fragmentData = {
       flag: 'addRecuringDeposit',
       data,
