1c9c29f3c3b4ada029a24e232939fb540e1656e2
ani <aniruddha@futurewise.co.in>
2020-03-20 18:40:16 +0530

    scss done

--- src/app/component/protect-component/customers/component/customer/accounts/assets/smallSavingScheme/common-component/add-scss/add-scss.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/smallSavingScheme/common-component/add-scss/add-scss.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { AuthService } from 'src/app/auth-service/authService';
 import { CustomerService } from '../../../../../customer.service';
 import { EventService } from 'src/app/Data-service/event.service';
@@ -24,7 +24,7 @@ export class AddScssComponent implements OnInit {
   familyMemberId: any;
   ownerName: any;
   scssSchemeForm: any;
-  scssOptionalSchemeForm: any;
+  callMethod: any;
   advisorId: any;
   clientId: number;
   ownerData: any;
@@ -65,15 +65,144 @@ export class AddScssComponent implements OnInit {
     return this.inputData;
   }
 
-  display(value) {
-    console.log('value selected', value);
-    this.ownerName = value;
-    this.familyMemberId = value.id;
+  
+  // ===================owner-nominee directive=====================//
+display(value) {
+  console.log('value selected', value)
+  this.ownerName = value.userName;
+  this.familyMemberId = value.id
+}
+
+lisNominee(value) {
+  this.ownerData.Fmember = value;
+  this.nomineesListFM = Object.assign([], value);
+}
+
+disabledMember(value, type) {
+  this.callMethod = {
+    methodName : "disabledMember",
+    ParamValue : value,
+    disControl : type
   }
-  lisNominee(value) {
-    console.log(value)
-    this.nomineesListFM = Object.assign([], this.utils.calculateAgeFromCurrentDate(value.familyMembersList));
+}
+
+displayControler(con) {
+  console.log('value selected', con);
+  if(con.owner != null && con.owner){
+    this.scssSchemeForm.controls.getCoOwnerName = con.owner;
   }
+  if(con.nominee != null && con.nominee){
+    this.scssSchemeForm.controls.getNomineeName = con.nominee;
+  }
+}
+
+onChangeJointOwnership(data) {
+  this.callMethod = {
+    methodName : "onChangeJointOwnership",
+    ParamValue : data
+  }
+}
+
+/***owner***/ 
+
+get getCoOwner() {
+  return this.scssSchemeForm.get('getCoOwnerName') as FormArray;
+}
+
+addNewCoOwner(data) {
+  this.getCoOwner.push(this.fb.group({
+    name: [data ? data.name : '', [Validators.required]], share: [data ? data.share : '', [Validators.required]], familyMemberId: [data ? data.familyMemberId : 0], id: [data ? data.id : 0]
+  }));
+  if (data) {
+    setTimeout(() => {
+     this.disabledMember(null,null);
+    }, 1300);
+  }
+
+  if(this.getCoOwner.value.length > 1 && !data){
+   let share = 100/this.getCoOwner.value.length;
+   for (let e in this.getCoOwner.controls) {
+    if(!Number.isInteger(share) && e == "0"){
+      this.getCoOwner.controls[e].get('share').setValue(Math.round(share) + 1);
+    }
+    else{
+      this.getCoOwner.controls[e].get('share').setValue(Math.round(share));
+    }
+   }
+  }
+  
+}
+
+removeCoOwner(item) {
+  this.getCoOwner.removeAt(item);
+  if (this.scssSchemeForm.value.getCoOwnerName.length == 1) {
+    this.getCoOwner.controls['0'].get('share').setValue('100');
+  } else {
+    let share = 100/this.getCoOwner.value.length;
+    for (let e in this.getCoOwner.controls) {
+      if(!Number.isInteger(share) && e == "0"){
+        this.getCoOwner.controls[e].get('share').setValue(Math.round(share) + 1);
+      }
+      else{
+        this.getCoOwner.controls[e].get('share').setValue(Math.round(share));
+      }
+    }
+  }
+  this.disabledMember(null, null);
+}
+/***owner***/ 
+
+/***nominee***/ 
+
+get getNominee() {
+  return this.scssSchemeForm.get('getNomineeName') as FormArray;
+}
+
+removeNewNominee(item) {
+  this.getNominee.removeAt(item);
+  if (this.scssSchemeForm.value.getNomineeName.length == 1) {
+    this.getNominee.controls['0'].get('sharePercentage').setValue('100');
+  } else {
+    let share = 100/this.getNominee.value.length;
+    for (let e in this.getNominee.controls) {
+      if(!Number.isInteger(share) && e == "0"){
+        this.getNominee.controls[e].get('sharePercentage').setValue(Math.round(share) + 1);
+      }
+      else{
+        this.getNominee.controls[e].get('sharePercentage').setValue(Math.round(share));
+      }
+    }
+  }
+}
+
+
+
+addNewNominee(data) {
+  this.getNominee.push(this.fb.group({
+    name: [data ? data.name : ''], sharePercentage: [data ? data.sharePercentage : 0], familyMemberId: [data ? data.familyMemberId : 0], id: [data ? data.id : 0]
+  }));
+  if (!data || this.getNominee.value.length < 1) {
+    for (let e in this.getNominee.controls) {
+      this.getNominee.controls[e].get('sharePercentage').setValue(0);
+    }
+  }
+
+  if(this.getNominee.value.length > 1 && !data){
+    let share = 100/this.getNominee.value.length;
+    for (let e in this.getNominee.controls) {
+      if(!Number.isInteger(share) && e == "0"){
+        this.getNominee.controls[e].get('sharePercentage').setValue(Math.round(share) + 1);
+      }
+      else{
+        this.getNominee.controls[e].get('sharePercentage').setValue(Math.round(share));
+      }
+    }
+   }
+   
+  
+}
+/***nominee***/ 
+// ===================owner-nominee directive=====================//
   getFormDataNominee(data) {
     console.log(data)
     this.nomineesList = data.controls
@@ -88,19 +217,55 @@ export class AddScssComponent implements OnInit {
     }
     this.scssData = data;
     this.scssSchemeForm = this.fb.group({
-      ownerName: [!data.ownerName ? '' : data.ownerName, [Validators.required, AssetValidationService.ageValidators(60)]],
+      getCoOwnerName: this.fb.array([this.fb.group({
+        name: ['',[Validators.required]],
+        share: ['',[Validators.required]],
+        familyMemberId: 0,
+        id:0
+      })]),
+      // ownerName: [!data.ownerName ? '' : data.ownerName, [Validators.required, AssetValidationService.ageValidators(60)]],
       amtInvested: [data.amountInvested, [Validators.required, Validators.min(1000), Validators.max(1500000)]],
       commDate: [new Date(data.commencementDate), [Validators.required]],
-      ownershipType: [(data.ownerTypeId) ? String(data.ownerTypeId) : '2', [Validators.required]]
-    });
-    this.scssOptionalSchemeForm = this.fb.group({
       poBranch: [data.postOfficeBranch],
       nominees: this.nominees,
       bankAccNumber: [data.bankAccountNumber],
-      description: [data.description]
+      description: [data.description],
+      getNomineeName: this.fb.array([this.fb.group({
+        name: [''],
+        sharePercentage: [0],
+        familyMemberId: [0],
+        id:[0]
+      })])
     });
-    this.ownerData = this.scssSchemeForm.controls;
-    this.familyMemberId = data.familyMemberId
+    
+    // ==============owner-nominee Data ========================\\
+  /***owner***/ 
+  if(this.scssSchemeForm.value.getCoOwnerName.length == 1){
+    this.getCoOwner.controls['0'].get('share').setValue('100');
+  }
+
+  if (data.ownerList) {
+    this.getCoOwner.removeAt(0);
+    data.ownerList.forEach(element => {
+      this.addNewCoOwner(element);
+    });
+  }
+  
+/***owner***/ 
+
+/***nominee***/ 
+if(data.nomineeList){
+  this.getNominee.removeAt(0);
+  data.nomineeList.forEach(element => {
+    this.addNewNominee(element);
+  });
+}
+/***nominee***/ 
+
+this.ownerData = {Fmember: this.nomineesListFM, controleData:this.scssSchemeForm}
+// ==============owner-nominee Data ========================\\ 
+    // this.ownerData = this.scssSchemeForm.controls;
+    // this.familyMemberId = data.familyMemberId
   }
 
   moreFields() {
@@ -108,19 +273,19 @@ export class AddScssComponent implements OnInit {
   }
 
   addScss() {
-    this.nominees = []
-    if (this.nomineesList) {
+    // this.nominees = []
+    // if (this.nomineesList) {
 
-      this.nomineesList.forEach(element => {
-        let obj = {
-          "name": element.controls.name.value,
-          "sharePercentage": element.controls.sharePercentage.value,
-          "id": element.id,
-          "familyMemberId": element.familyMemberId
-        }
-        this.nominees.push(obj)
-      });
-    }
+    //   this.nomineesList.forEach(element => {
+    //     let obj = {
+    //       "name": element.controls.name.value,
+    //       "sharePercentage": element.controls.sharePercentage.value,
+    //       "id": element.id,
+    //       "familyMemberId": element.familyMemberId
+    //     }
+    //     this.nominees.push(obj)
+    //   });
+    // }
     if (this.scssSchemeForm.invalid) {
       this.scssSchemeForm.markAllAsTouched();
     } else {
@@ -129,15 +294,22 @@ export class AddScssComponent implements OnInit {
         clientId: this.clientId,
         familyMemberId: this.familyMemberId,
         advisorId: this.advisorId,
-        ownerName: (this.ownerName == undefined) ? this.scssSchemeForm.controls.ownerName.value : this.ownerName.userName,
+        ownerList: this.scssSchemeForm.value.getCoOwnerName,
         amountInvested: this.scssSchemeForm.get('amtInvested').value,
         commencementDate: this.scssSchemeForm.get('commDate').value,
-        postOfficeBranch: this.scssOptionalSchemeForm.get('poBranch').value,
-        bankAccountNumber: this.scssOptionalSchemeForm.get('bankAccNumber').value,
-        ownerTypeId: this.scssSchemeForm.get('ownershipType').value,
-        nominees: this.nominees,
-        description: this.scssOptionalSchemeForm.get('description').value
+        postOfficeBranch: this.scssSchemeForm.get('poBranch').value,
+        bankAccountNumber: this.scssSchemeForm.get('bankAccNumber').value,
+        // ownerTypeId: this.scssSchemeForm.get('ownershipType').value,
+        nomineeList: this.scssSchemeForm.value.getNomineeName,
+        description: this.scssSchemeForm.get('description').value
       };
+
+      obj.nomineeList.forEach((element, index) => {
+        if(element.name == ''){
+          this.removeNewNominee(index);
+        }
+      });
+      obj.nomineeList= this.scssSchemeForm.value.getNomineeName;
       let adviceObj = {
         advice_id: this.advisorId,
         adviceStatusId: 5,
@@ -180,7 +352,7 @@ export class AddScssComponent implements OnInit {
 
   isFormValuesForAdviceValid() {
     if (this.scssSchemeForm.valid ||
-      (this.scssSchemeForm.valid && this.scssOptionalSchemeForm.valid && this.nomineesList.length !== 0)) {
+      (this.scssSchemeForm.valid && this.scssSchemeForm.valid && this.nomineesList.length !== 0)) {
       return true
     } else {
       return false;
