d7e23e6f2eb15696942bfc47682ed1b462ef106d
sarvesh <sarvesh@futurewise.co.in>
2020-03-21 16:44:33 +0530

    add

--- src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/add-family-member/add-family-member.component.ts
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/add-family-member/add-family-member.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
 
 @Component({
   selector: 'app-add-family-member',
@@ -8,11 +9,51 @@ import { Component, OnInit } from '@angular/core';
 export class AddFamilyMemberComponent implements OnInit {
   displayedColumns: string[] = ['details', 'status', 'pan', 'relation', 'gender', 'add', 'remove'];
   dataSource = ELEMENT_DATA;
-  constructor() { }
+  typeOfFamilyMemberAdd;
+  step = 1;
+  constructor(private subInjectService: SubscriptionInject) { }
 
   ngOnInit() {
   }
-
+  addFamilyType(value) {
+    this.typeOfFamilyMemberAdd = value;
+  }
+  close() {
+    this.subInjectService.changeNewRightSliderState({ state: 'close' });
+  }
+  next() {
+    this.step++;
+  }
+  save() {
+    this.close();
+  }
+  selectFamilyMembers(selectedFamilyMember) {
+    (selectedFamilyMember.selected) ? selectedFamilyMember.selected = false : selectedFamilyMember.selected = true;
+  }
+  remove(selectedMember) {
+    (selectedMember.count > 0) ? selectedMember.count-- : '';
+  }
+  add(selectedMember) {
+    selectedMember.count++;
+  }
+  selectFamilyMembersCount() {
+    this.next();
+  }
+  familyMemberList = {
+    'firstRow': [
+      { name: 'You', imgUrl: '/assets/images/svg/man-profile.svg', selected: false },
+      { name: 'Wife', imgUrl: '/assets/images/svg/wife-profile.svg', selected: false }
+    ],
+    'secondRow': [
+      { name: 'Father', imgUrl: '/assets/images/svg/father-profile.svg', selected: false },
+      { name: 'Mother', imgUrl: '/assets/images/svg/mother-profile.svg', selected: false }
+    ],
+    'thirdRow': [
+      { name: 'Son', imgUrl: '/assets/images/svg/son-profile.svg', selected: false },
+      { name: 'Daughter', imgUrl: '/assets/images/svg/daughter-profile.svg', selected: false, count: 0 },
+      { name: 'Others', imgUrl: '/assets/images/svg/man-profile.svg', selected: false, count: 0 }
+    ]
+  }
 }
 
 export interface PeriodicElement {
