7d59ff7fa7df9161df63cc08af0a5a19788ada2a
Ani <aniruddha@futurewise.co.in>
2020-03-23 13:50:12 +0530

    team member add done

--- src/app/component/protect-component/PeopleComponent/people/Component/people-team-members/people-team-members.component.ts
+++ src/app/component/protect-component/PeopleComponent/people/Component/people-team-members/people-team-members.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidatorType } from 'src/app/services/util.service';
+
 
 @Component({
   selector: 'app-people-team-members',
@@ -8,11 +11,30 @@ import { Component, OnInit } from '@angular/core';
 export class PeopleTeamMembersComponent implements OnInit {
   displayedColumns: string[] = ['name', 'mobile', 'email', 'type', 'map', 'action'];
   dataSource = ELEMENT_DATA;
-  constructor() { }
-
+  validatorType = ValidatorType;
+  constructor(private fb: FormBuilder) { }
+teamInviteForm:FormGroup;
   ngOnInit() {
+    this.teamInviteForm = this.fb.group({
+      name:['',[Validators.required]],
+      email:['',[Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
+      mobile:['',[Validators.required]],
+      role:['',[Validators.required]],
+      lead:['',[Validators.required]],
+      euin:['',[Validators.required]]
+    })
   }
 
+  sendInvitation(){
+    if(this.teamInviteForm.invalid){
+      for(let c in this.teamInviteForm.controls){
+        this.teamInviteForm.controls[c].markAsTouched();
+      }
+    }
+    else{
+      console.log(this.teamInviteForm.value,"teamInviteForm");
+    }
+  }
 }
 export interface PeriodicElement {
   name: string;
@@ -29,6 +51,4 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { name: 'Amitesh Anand', mobile: '9322574914', email: 'amit.mehta@gmail.com', type: 'Client', map: '', action: '' },
   { name: 'Hemal Karia', mobile: '9322574914', email: 'amit.mehta@gmail.com', type: 'Client', map: '', action: '' },
   { name: 'Kiran Kumar', mobile: '9322574914', email: 'amit.mehta@gmail.com', type: 'Client', map: '', action: '' },
-
-
 ];
