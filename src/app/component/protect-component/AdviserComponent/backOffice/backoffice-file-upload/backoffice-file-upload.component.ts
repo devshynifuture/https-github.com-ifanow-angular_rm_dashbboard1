8075dff6a38a5b6fe3be8f3ce224ba2e12e1bdd6
Gaurav <gaurav@futurewise.co.in>
2020-03-30 11:22:36 +0530

    ng build --prod issues resolved
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-file-upload/backoffice-file-upload.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-file-upload/backoffice-file-upload.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ReconciliationService } from '../backoffice-aum-reconciliation/reconciliation/reconciliation.service';
-import { AuthService } from 'src/app/auth-service/authService';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpService } from 'src/app/http-service/http-service';
+import {Component, OnInit} from '@angular/core';
+import {ReconciliationService} from '../backoffice-aum-reconciliation/reconciliation/reconciliation.service';
+import {AuthService} from 'src/app/auth-service/authService';
+import {HttpHeaders} from '@angular/common/http';
+import {HttpService} from 'src/app/http-service/http-service';
 
 @Component({
   selector: 'app-backoffice-file-upload',
@@ -10,30 +10,32 @@ import { HttpService } from 'src/app/http-service/http-service';
   styleUrls: ['./backoffice-file-upload.component.scss']
 })
 export class BackofficeFileUploadComponent implements OnInit {
-  filterRTA:any="";
-  filterStatus:any="";
-  selectedFileType:any="";
-  fileType:any;
+  filterRTA: any = "";
+  filterStatus: any = "";
+  selectedFileType: any = "";
+  fileType: any;
   advisorId: any;
-  constructor(private reconService: ReconciliationService, private http: HttpService) { }
+  myFiles: any;
+
+  constructor(private reconService: ReconciliationService, private http: HttpService) {
+  }
 
   ngOnInit() {
     this.advisorId = AuthService.getAdvisorId();
-    this.reconService.getBackOfficeFileUploadFileType({}).subscribe((data)=>{
+    this.reconService.getBackOfficeFileUploadFileType({}).subscribe((data) => {
       this.fileType = data;
     })
   }
 
-  myFiles:any;
-  getFile(e){
+  getFile(e) {
     console.log(e);
-    let obj={
-      fileType:this.selectedFileType,
+    let obj = {
+      fileType: this.selectedFileType,
       advisorId: this.advisorId
     }
-    this.reconService.getBackOfficeFileToUpload(obj).subscribe((data)=>{
+    this.reconService.getBackOfficeFileToUpload(obj).subscribe((data) => {
       // this.fileType = data;
-      if(data){
+      if (data) {
         this.uploadFileRes(data, e.target.files[0]);
       }
     });
@@ -48,23 +50,26 @@ export class BackofficeFileUploadComponent implements OnInit {
     // });
   }
 
-  uploadFileRes(data, file){
+  uploadFileRes(data, file) {
     const httpOptions = {
       headers: new HttpHeaders()
         .set('Content-Type', '')
     };
-    this.http.put(data.presignedUrl, file, httpOptions).subscribe((responseData) => {
+    this.http.putExternal(data.presignedUrl, file, httpOptions).subscribe((responseData) => {
       console.log('DocumentsComponent uploadFileRes responseData : ', responseData);
       // this.successFileUpload(this.selectedFileType, data.fileName)
+    }, error => {
+      console.log('DocumentsComponent uploadFileRes error : ', error);
+
     });
   }
 
-  successFileUpload(fileType, fileName){
+  successFileUpload(fileType, fileName) {
     let obj = {
       fileType: fileType,
       fileName: fileName
     }
-    this.reconService.successBackOfficeFileToUpload(obj).subscribe((data)=>{
+    this.reconService.successBackOfficeFileToUpload(obj).subscribe((data) => {
 
     })
   }
