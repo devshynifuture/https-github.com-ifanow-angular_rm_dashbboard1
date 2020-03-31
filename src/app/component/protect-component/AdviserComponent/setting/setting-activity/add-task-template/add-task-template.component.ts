c3b332de4b3f2ba06b88f782810de4573f033a54
devshyni <devshyni@futurewise.co.in>
2020-03-31 15:29:55 +0530

    setting assset allocation

--- src/app/component/protect-component/AdviserComponent/setting/setting-activity/add-task-template/add-task-template.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-activity/add-task-template/add-task-template.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { SubscriptionInject } from 'planner2.0/src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
-import { UtilService } from 'planner2.0/src/app/services/util.service';
 import { SettingsService } from '../../settings.service';
 import { EventService } from 'src/app/Data-service/event.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { OrgSettingServiceService } from '../../org-setting-service.service';
 import { AuthService } from 'src/app/auth-service/authService';
+import { SubscriptionInject } from '../../../Subscriptions/subscription-inject.service';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-add-task-template',
@@ -17,6 +17,8 @@ export class AddTaskTemplateComponent implements OnInit {
   dataSource = ELEMENT_DATA;
   taskTemplate
   advisorId: any;
+  category: any;
+  editMode: boolean = false;
   constructor(private subInjectService: SubscriptionInject,
     private utilService: UtilService,
     private orgSetting: OrgSettingServiceService,
@@ -27,8 +29,14 @@ export class AddTaskTemplateComponent implements OnInit {
   ngOnInit() {
     this.getdataForm('')
     this.advisorId = AuthService.getAdvisorId()
+    this.category = 'asset'
+    this.editMode = false;
+    this.taskTemplate.controls.category.setValue(1)
   }
-
+  getSelectedCategory(value, category) {
+    this.category = category
+    console.log('getSelectedCategory', value)
+  }
   getdataForm(data) {
     this.taskTemplate = this.fb.group({
       category: [(!data) ? '' : (data.category), [Validators.required]],
@@ -36,7 +44,23 @@ export class AddTaskTemplateComponent implements OnInit {
       taskTemplate: [(!data) ? '' : data.taskTemplate, [Validators.required]],
       defaultAssign: [(!data) ? '' : data.defaultAssign],
       turnaroundTime: [(!data) ? '' : data.turnaroundTime],
+      subTaskList: this.fb.array([this.fb.group({
+        taskNumber: [null, [Validators.required]],
+        description: [null, [Validators.required]],
+        turtAroundTime: [null, [Validators.required]],
+        ownerId: [null, [Validators.required]]
+      })]),
     });
+    if (data.subTaskList != undefined) {
+      data.subTaskList.forEach(element => {
+        this.taskTemplate.controls.subTaskList.push(this.fb.group({
+          taskNumber: [(element.taskNumber) + "", [Validators.required]],
+          description: [(element.description + ""), Validators.required],
+          turtAroundTime: [(element.turtAroundTime), Validators.required],
+          ownerId: [element.ownerId, [Validators.required]]
+        }))
+      })
+    }
   }
 
   getFormControl(): any {
@@ -45,27 +69,34 @@ export class AddTaskTemplateComponent implements OnInit {
   Close(flag: boolean) {
     this.subInjectService.changeNewRightSliderState({ state: 'close', refreshRequired: flag });
   }
-  saveTaskTemplate(){
+  changeTableTdValue(value, ele) {
+
+  }
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+    console.log('hgdsfhg ==', this.editMode)
+  }
+  saveTaskTemplate() {
     let obj = {
-      
-        advisorId:this.advisorId,
-        categoryId:1,
-        subcategoryId:2,
-        linkedTemplateId:2,
-        taskDescription:this.taskTemplate.controls.taskTemplate.value,
-        assignedTo:this.taskTemplate.controls.defaultAssign.value,
-        turnAroundTime:this.taskTemplate.turnaroundTime.value,
-        subTaskList: [{
-          taskNumber:1,
-          description: "Abcd needs to be done today!",
-          turtAroundTime:2,
-          ownerId:2727
-        },{
-          taskNumber:1,
-          description: "Abcd needs to be done today!",
-          turtAroundTime:2,
-          ownerId:2727
-        }]
+
+      advisorId: this.advisorId,
+      categoryId: 1,
+      subcategoryId: 2,
+      linkedTemplateId: 2,
+      taskDescription: this.taskTemplate.controls.taskTemplate.value,
+      assignedTo: this.taskTemplate.controls.defaultAssign.value,
+      turnAroundTime: this.taskTemplate.turnaroundTime.value,
+      subTaskList: [{
+        taskNumber: 1,
+        description: "Abcd needs to be done today!",
+        turtAroundTime: 2,
+        ownerId: 2727
+      }, {
+        taskNumber: 1,
+        description: "Abcd needs to be done today!",
+        turtAroundTime: 2,
+        ownerId: 2727
+      }]
     }
     this.orgSetting.addTaskTemplate(obj).subscribe(
       data => {
@@ -74,13 +105,14 @@ export class AddTaskTemplateComponent implements OnInit {
       err => this.event.openSnackBar(err, "Dismiss")
     );
   }
-  addTaskTemplateRes(data){
-    if(data){
+  addTaskTemplateRes(data) {
+    if (data) {
 
-    }else{
+    } else {
 
     }
   }
+
 }
 export interface PeriodicElement {
 
