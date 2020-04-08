0c7c3b36789e3a6417ba5d3476c17d450ff1b970
Gaurav <gaurav@futurewise.co.in>
2020-04-08 10:37:22 +0530

    Lint issues fixed GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
-import { EventService } from 'src/app/Data-service/event.service';
-import { UtilService } from 'src/app/services/util.service';
-import { CustomerService } from '../../../../customer.service';
-import { AuthService } from 'src/app/auth-service/authService';
+import {Component, OnInit} from '@angular/core';
+import {SubscriptionInject} from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
+import {EventService} from 'src/app/Data-service/event.service';
+import {UtilService} from 'src/app/services/util.service';
+import {CustomerService} from '../../../../customer.service';
+import {AuthService} from 'src/app/auth-service/authService';
+import {MfServiceService} from '../mf-service.service';
 
 @Component({
   selector: 'app-mutual-fund',
@@ -15,15 +16,25 @@ export class MutualFundComponent implements OnInit {
   mfData: any;
   advisorId: any;
   clientId: any;
+  subCategoryData: any[];
+  schemeWise: any[];
+  mutualFundList: any[];
+  totalObj: any;
+  customDataSource: any;
+  catObj: {};
 
-  constructor(public subInjectService: SubscriptionInject, public UtilService: UtilService, public eventService: EventService, private custumService: CustomerService) { }
+  constructor(public subInjectService: SubscriptionInject, public utilService: UtilService,
+              public eventService: EventService, private custumService: CustomerService,
+              private mfService: MfServiceService) {
+  }
 
   ngOnInit() {
-    this.viewMode = 'Overview Report';
+    this.viewMode = 'All Transactions';
     this.advisorId = AuthService.getAdvisorId();
     this.clientId = AuthService.getClientId();
     this.getMutualFund();
   }
+
   getMutualFund() {
     const obj = {
       advisorId: 2753,
@@ -35,9 +46,11 @@ export class MutualFundComponent implements OnInit {
       }
     );
   }
+
   getMutualFundResponse(data) {
     this.mfData = data;
   }
+
 
 }
 
