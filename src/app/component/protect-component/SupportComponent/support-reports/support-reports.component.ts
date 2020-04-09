4549d5422c75b2600d7ba314874be1316164265d
sarvesh shinde <sarvesh@futurewise.co.in>
2020-04-09 16:38:31 +0530

    data binding and api method call in support service

--- src/app/component/protect-component/SupportComponent/support-reports/support-reports.component.ts
+++ src/app/component/protect-component/SupportComponent/support-reports/support-reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SupportService } from '../support.service';
+import { EventService } from 'src/app/Data-service/event.service';
 
 @Component({
   selector: 'app-support-reports',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupportReportsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private supportService: SupportService, private eventService: EventService) { }
 
   ngOnInit() {
   }
+  getReports() {
+    let obj = {};
+    this.supportService.getBackofficeReports(obj).subscribe(
+      data => {
+        console.log(data);
+        if (data) {
 
+        }
+      },
+      err => this.eventService.openSnackBar(err, "Dismiss")
+    )
+  }
 }
