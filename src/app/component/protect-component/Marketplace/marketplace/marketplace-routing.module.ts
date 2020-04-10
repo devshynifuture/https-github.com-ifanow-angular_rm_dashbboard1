6595b250f8623a9464b50b388d809448a9ee32a0
sarvesh shinde <sarvesh@futurewise.co.in>
2020-04-10 12:56:43 +0530

    home page for marketplace

--- src/app/component/protect-component/Marketplace/marketplace/marketplace-routing.module.ts
+++ src/app/component/protect-component/Marketplace/marketplace/marketplace-routing.module.ts
@@ -6,16 +6,17 @@ import { MarketplaceComponent } from './marketplace.component';
 const routes: Routes = [{
   path: '', component: MarketplaceComponent,
   children: [
+    { path: 'home', loadChildren: () => import('../marketplace-home/marketplace-home.module').then(m => m.MarketplaceHomeModule) },
     { path: 'experts', loadChildren: () => import('../experts/experts.module').then(m => m.ExpertsModule) },
     { path: 'chat', loadChildren: () => import('../chat/chat.module').then(m => m.ChatModule) },
     { path: 'learn', loadChildren: () => import('../learn/learn.module').then(m => m.LearnModule) },
     {
       path: '',
-      redirectTo: 'experts',
+      redirectTo: 'home',
       pathMatch: 'full'
     }
   ]
-}
+},
 ];
 
 @NgModule({
