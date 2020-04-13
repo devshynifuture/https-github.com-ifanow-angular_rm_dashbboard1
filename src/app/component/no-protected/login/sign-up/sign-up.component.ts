d75f7bb2e2a8caae75a37aac3c2f3a5c95a81ece
sarvesh shinde <sarvesh@futurewise.co.in>
2020-04-13 14:01:14 +0530

    cofirm box in signup adn add adddressflag in overview profile

--- src/app/component/no-protected/login/sign-up/sign-up.component.ts
+++ src/app/component/no-protected/login/sign-up/sign-up.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {ValidatorType} from 'src/app/services/util.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {LoginService} from '../login.service';
-import {EventService} from 'src/app/Data-service/event.service';
-import {AuthService} from 'src/app/auth-service/authService';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ValidatorType } from 'src/app/services/util.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from '../login.service';
+import { EventService } from 'src/app/Data-service/event.service';
+import { AuthService } from 'src/app/auth-service/authService';
 
 @Component({
   selector: 'app-sign-up',
@@ -15,7 +15,7 @@ export class SignUpComponent implements OnInit {
   clientSignUp = false;
 
   constructor(private fb: FormBuilder, private authService: AuthService, public routerActive: ActivatedRoute,
-              private router: Router, private loginService: LoginService, private eventService: EventService) {
+    private router: Router, private loginService: LoginService, private eventService: EventService) {
   }
 
   signUpForm;
@@ -30,7 +30,7 @@ export class SignUpComponent implements OnInit {
     this.signUpForm = this.fb.group({
       fullName: [, [Validators.required]],
       email: [, [Validators.required,
-        Validators.pattern(this.validatorType.EMAIL)]],
+      Validators.pattern(this.validatorType.EMAIL)]],
       mobile: [, [Validators.required, Validators.pattern(this.validatorType.TEN_DIGITS)]],
       termsAgreement: [, [Validators.required]]
     });
@@ -61,6 +61,10 @@ export class SignUpComponent implements OnInit {
       this.loginService.register(obj, this.clientSignUp).subscribe(
         data => {
           console.log(data);
+          if (data == 400) {
+            confirm("Your contact details are already registered under username ABCD. How would you like to proceed?")
+            return;
+          }
           const forgotPassObjData = {
             mobileNo: this.signUpForm.get('mobile').value,
             email: this.signUpForm.get('email').value,
@@ -79,14 +83,13 @@ export class SignUpComponent implements OnInit {
             };
 
             this.authService.setToken('data');
-
             this.authService.setUserInfo(jsonData);
             this.authService.setClientData({
               id: 2978, name: 'Aryendra Kumar Saxena'
             });
             this.router.navigate(['customer', 'detail', 'overview', 'myfeed']);
           } else {
-            this.router.navigate(['/login/forgotpassword'], {state: forgotPassObjData});
+            this.router.navigate(['/login/forgotpassword'], { state: forgotPassObjData });
           }
         },
         err => this.eventService.openSnackBar(err, 'Dismiss')
