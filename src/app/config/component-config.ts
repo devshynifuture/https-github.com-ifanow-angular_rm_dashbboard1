9ea0ab7222317c96c0aebbca9aabe505e933a4b0
gayatri pawar <gayatri@futurewise.co.in>
2020-04-15 23:43:54 +0530

    transaction pdf and api binding

--- src/app/config/component-config.ts
+++ src/app/config/component-config.ts
@@ -201,6 +201,7 @@ export const appConfig = {
   MUTUAL_FUND_GET: 'mutual-fund/mutualfund/list/get',
   FOLIO_MASTER_DETAILS: 'mutual-fund/folioMasterDetails/list/get',
   SIP_DETAILS: 'mutual-fund/sipDetails/list/get',
+  UNREALIZED_TRANSACTION_GET:'mutual-fund/unrealized/transaction/list/get',
   ////////////////////////////////plan////////////////////////////
   GET_RISK_PROFILE: 'plan/risk-assessment/get',
   SUBMIT_RISK: 'plan/risk-assessment/add',
