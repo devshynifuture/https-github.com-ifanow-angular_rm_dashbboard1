779d146ea983945b1107fd27d035dfd402258afb
Sagar Shah <sagar.shah@futurewise.co.in>
2020-04-20 19:00:03 +0530

    fixes and changes

--- src/app/component/protect-component/AdviserComponent/setting/setting-user-roles/setting-users-roles/users/new-team-member/new-team-member.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-user-roles/setting-users-roles/users/new-team-member/new-team-member.component.ts
@@ -35,14 +35,7 @@ export class NewTeamMemberComponent implements OnInit {
     //   fontIcon: 'favorite'
     // }
   };
-  mobileNumberFA:FormArray;
-  classObj = {
-    topPadding: 'pt-20',
-    label: 'col-md-3 text-right pr-0',
-    code: 'col-md-3 pr-0',
-    mobile: 'col-md-4 pr-0',
-    addRemove: 'col-md-2',
-  }
+  countryCodes:Array<string> = ['+91', '+92', "+93"];
 
   constructor(
     private fb: FormBuilder,
@@ -77,15 +70,15 @@ export class NewTeamMemberComponent implements OnInit {
       fullName: [this.data.mainData.fullName || '',
         [Validators.required, Validators.maxLength(50), Validators.pattern(ValidatorType.PERSON_NAME)]],
       emailId: [this.data.mainData.email || '', [Validators.required, Validators.pattern(ValidatorType.EMAIL)]],
-      // mobileNo: [this.data.mainData.mobile || '', [Validators.required, Validators.pattern(this.validatorType.TEN_DIGITS)]],
+      mobileNo: [this.data.mainData.mobile || '', [Validators.required, Validators.pattern(this.validatorType.TEN_DIGITS)]],
+      isdCode: [this.data.mainData.isdCode || '', [Validators.required]],
       roleId: [roleId, [Validators.required]],
     });
   }
 
   save() {
-    if (this.teamMemberFG.invalid || this.mobileNumberFA.invalid) {
+    if (this.teamMemberFG.invalid) {
       this.teamMemberFG.markAllAsTouched();
-      this.mobileNumberFA.markAllAsTouched();
     } else {
       if (this.barButtonOptions.active) {
       } else {
@@ -100,10 +93,7 @@ export class NewTeamMemberComponent implements OnInit {
   }
 
   addTeamMember() {
-    const dataObj = {
-      ...this.teamMemberFG.value,
-      mobileList: this.mobileNumberFA.value,
-    };
+    const dataObj = this.teamMemberFG.value;
     this.settingsService.addTeamMember(dataObj).subscribe((res) => {
       this.close(true);
       this.eventService.openSnackBar('Invitation sent successfully');
@@ -159,9 +149,5 @@ export class NewTeamMemberComponent implements OnInit {
       teamMemberRoleId.setValue('');
       this.teamMemberFG.updateValueAndValidity();
     }
-  }
-
-  getNumberDetails(formArr:FormArray) {
-    this.mobileNumberFA = formArr;
   }
 }
