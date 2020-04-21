1db09e1196c1c4a01448a0c20de0d44855a27570
syed khizaruddin <jarvis@syeds-MacBook-Air.local>
2020-04-21 20:50:09 +0530

    marketplace sliders upper right sliders component creation

---
+++ src/app/component/protect-component/Advisor-Marketplace/advisor-marketplace/marketplace-review-rating/marketplace-review-reply/marketplace-review-reply.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-review-reply',
+  templateUrl: './marketplace-review-reply.component.html',
+  styleUrls: ['./marketplace-review-reply.component.scss']
+})
+export class MarketplaceReviewReplyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/component/protect-component/Advisor-Marketplace/advisor-marketplace/marketplace-review-rating/marketplace-review-request-bulk/marketplace-review-request-bulk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-review-request-bulk',
+  templateUrl: './marketplace-review-request-bulk.component.html',
+  styleUrls: ['./marketplace-review-request-bulk.component.scss']
+})
+export class MarketplaceReviewRequestBulkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
