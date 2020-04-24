4ae538639300409b8abbd32f23dfeeddb1c56e26
Sagar Shah <sagar.shah@futurewise.co.in>
2020-04-24 18:42:31 +0530

    bugs oh bugs please disappear

--- src/app/component/protect-component/AdviserComponent/setting/setting-activity/add-task-template/add-task-template.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-activity/add-task-template/add-task-template.component.ts
@@ -5,6 +5,7 @@ import {OrgSettingServiceService} from '../../org-setting-service.service';
 import {AuthService} from 'src/app/auth-service/authService';
 import {SubscriptionInject} from '../../../Subscriptions/subscription-inject.service';
 import {Subscription} from 'rxjs';
+import { SettingsService } from '../../settings.service';
 
 @Component({
   selector: 'app-add-task-template',
@@ -36,6 +37,7 @@ export class AddTaskTemplateComponent implements OnInit, OnDestroy {
     private orgSetting: OrgSettingServiceService,
     private event: EventService,
     private fb: FormBuilder,
+    private settingService: SettingsService,
   ) {
     this.advisorId = AuthService.getAdvisorId();
     this.user = AuthService.getUserInfo();
@@ -58,13 +60,15 @@ export class AddTaskTemplateComponent implements OnInit, OnDestroy {
     const obj = {
       advisorId: this.advisorId
     };
-    this.orgSetting.getTeamMemberList(obj).subscribe(
+    this.settingService.getTeamMembers(obj).subscribe(
       data => {
         this.teamMemberList = data || [];
-        this.teamMemberList.unshift(this.user);
         this.changeLoadCounter(-1);
       },
-      err => this.event.openSnackBar(err, 'Dismiss')
+      err => {
+        this.event.openSnackBar(err, 'Dismiss')
+        this.changeLoadCounter(-1);
+      }
     );
   }
 
