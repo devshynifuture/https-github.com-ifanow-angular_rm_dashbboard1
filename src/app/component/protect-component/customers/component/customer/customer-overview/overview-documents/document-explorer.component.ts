e788dde3b96c9437b0e69627b025439a6dbdcf43
devshyni <devshyni@futurewise.co.in>
2020-04-24 17:00:08 +0530

    document preview

--- src/app/component/protect-component/customers/component/customer/customer-overview/overview-documents/document-explorer.component.ts
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-documents/document-explorer.component.ts
@@ -521,7 +521,7 @@ export class DocumentExplorerComponent implements AfterViewInit, OnInit {
         fromEmail: "support@futurewise.co.in",
         toEmail: this.element.email,
         emailSubject: "Share link",
-        messageBody: this.element.link
+        messageBody: 'You have received this email because AdvisorName shared link with you.'+this.element.link
       }
       this.custumService.sendSharebleLink(obj).subscribe(
         data => this.sendSharebleLinkRes(data),
