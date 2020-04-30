7b7ad66cb640a68b1aa0bc203e253a5592e61ad4
sarvesh shinde <sarvesh@futurewise.co.in>
2020-04-30 18:25:13 +0530

    bugs resolved

--- src/app/services/excel-gen.service.ts
+++ src/app/services/excel-gen.service.ts
@@ -56,7 +56,7 @@ export class ExcelGenService {
 
     // Create workbook and worksheet
     const workbook = new Workbook();
-    const worksheet = workbook.addWorksheet(this.advisor.name+'_'+title +'_'+'Data');
+    const worksheet = workbook.addWorksheet(this.advisor.name + '_' + title + '_' + 'Data');
 
 
     // Add Row and formatting
@@ -66,8 +66,8 @@ export class ExcelGenService {
     worksheet.addRow([]);
     // let subTitleRow = worksheet.addRow(['Date :', this.datePipe.transform(new Date(), 'medium')])
 
-   worksheet.addRow(['Advisor', this.advisor.name]);
-   worksheet.addRow(['Client', this.client.name]);
+    worksheet.addRow(['Advisor', this.advisor.name]);
+    worksheet.addRow(['Client', this.client ? this.client.name : '']);
 
     // Add Image
     // let logo = workbook.addImage({
