4ee5008d595c7e295276dbeedefc4f08d14f2438
Gayatri Pawar <gayatri@futurewise.co.in>
2020-05-04 21:10:18 +0530

    bank account directive changes

--- src/app/common/directives/bank-account.directive.ts
+++ src/app/common/directives/bank-account.directive.ts
@@ -41,7 +41,6 @@ export class BankAccountDirective {
 
   @HostListener('click', ['$event'])
   public onClick(event: MouseEvent) {
-    this.clientData.clientId = this.clientData.clientId;
     this.openBankForm(this.clientData);
   }
 
