3a72de50e82c0f7b8a05bb032779b24fb48a6dfb
Gaurav <gaurav@futurewise.co.in>
2020-05-06 00:17:28 +0530

    prod issues
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/left-sidebar/leftsidebar/leftsidebar.module.ts
+++ src/app/component/left-sidebar/leftsidebar/leftsidebar.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
-import { LeftsidebarRoutingModule } from './leftsidebar-routing.module';
-import { MaterialModule } from 'src/app/material/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SubscriptionEntry } from '../../protect-component/AdviserComponent/Subscriptions/subscription.entry.module';
-import { SubscriptionUpperEntry } from '../../protect-component/AdviserComponent/Subscriptions/subscription-upper-entry-module';
-import { LeftsidebarComponent } from './leftsidebar.component';
-import { DynamicComponentService } from "../../../services/dynamic-component.service";
-import { TransactionsModule } from '../../protect-component/AdviserComponent/transactions/transactions.module';
-import { TransactionEntryModule } from '../../protect-component/AdviserComponent/transactions/transaction.entry.module';
-import { SupportEntryModule } from '../../protect-component/SupportComponent/support.entry.module';
-import { SettingEntryModule } from '../../protect-component/AdviserComponent/setting/setting-entry/setting-entry.module';
-import { ScrollDispatchModule } from '@angular/cdk/scrolling';
-import { PeopleEntryModule } from '../../protect-component/PeopleComponent/people/people-entry-module';
-import { ActivitesEntryModule } from '../../protect-component/AdviserComponent/Activities/activites-entry.module';
-import { BackofficeAumReconciliationEntryModule } from '../../protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/backoffice-aum-reconciliation.entry.module'
-import { MarketPlaceEntryModule } from '../../protect-component/Advisor-Marketplace/advisor-marketplace.entry.module';
+import {LeftsidebarRoutingModule} from './leftsidebar-routing.module';
+import {MaterialModule} from 'src/app/material/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SubscriptionEntry} from '../../protect-component/AdviserComponent/Subscriptions/subscription.entry.module';
+import {SubscriptionUpperEntry} from '../../protect-component/AdviserComponent/Subscriptions/subscription-upper-entry-module';
+import {LeftsidebarComponent} from './leftsidebar.component';
+import {DynamicComponentService} from '../../../services/dynamic-component.service';
+import {TransactionsModule} from '../../protect-component/AdviserComponent/transactions/transactions.module';
+import {TransactionEntryModule} from '../../protect-component/AdviserComponent/transactions/transaction.entry.module';
+import {SupportEntryModule} from '../../protect-component/SupportComponent/support.entry.module';
+import {SettingEntryModule} from '../../protect-component/AdviserComponent/setting/setting-entry/setting-entry.module';
+// import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import {PeopleEntryModule} from '../../protect-component/PeopleComponent/people/people-entry-module';
+import {ActivitesEntryModule} from '../../protect-component/AdviserComponent/Activities/activites-entry.module';
+import {BackofficeAumReconciliationEntryModule} from '../../protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/backoffice-aum-reconciliation.entry.module';
+import {MarketPlaceEntryModule} from '../../protect-component/Advisor-Marketplace/advisor-marketplace.entry.module';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {MY_FORMATS2} from '../../../constants/date-format.constant';
+
 @NgModule({
   declarations: [
     LeftsidebarComponent
@@ -29,7 +33,7 @@ import { MarketPlaceEntryModule } from '../../protect-component/Advisor-Marketpl
     SubscriptionEntry,
     SubscriptionUpperEntry,
     LeftsidebarRoutingModule,
-    ScrollDispatchModule,
+    // ScrollDispatchModule,
     TransactionsModule,
     TransactionEntryModule,
     SupportEntryModule,
@@ -39,9 +43,12 @@ import { MarketPlaceEntryModule } from '../../protect-component/Advisor-Marketpl
     BackofficeAumReconciliationEntryModule,
     MarketPlaceEntryModule
   ],
-  entryComponents: [SubscriptionEntry.getComponentList(), SupportEntryModule.getComponentList(), SettingEntryModule.getComponentList(), PeopleEntryModule.getComponentList(), MarketPlaceEntryModule.getComponentList()],
-  providers: [DynamicComponentService]
-  // providers: [DynamicComponentService]
+  entryComponents: [SubscriptionEntry.getComponentList(),
+    SupportEntryModule.getComponentList(), SettingEntryModule.getComponentList(),
+    PeopleEntryModule.getComponentList(), MarketPlaceEntryModule.getComponentList()],
+  providers: [DynamicComponentService,
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS2}]
 })
 export class LeftsidebarModule {
 }
