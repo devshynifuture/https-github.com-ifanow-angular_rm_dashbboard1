98a31b00723c38a8f5839dcf1733ef6807eb339f
devshyni <devshyni@futurewise.co.in>
2020-05-07 21:29:42 +0530

    bugs

--- src/app/component/protect-component/AdviserComponent/transactions/transactions-list/transactions-list.component.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/transactions-list/transactions-list.component.ts
@@ -29,11 +29,8 @@ export class TransactionsListComponent implements OnInit {
   @ViewChild(MatSort, { static: false }) sort: MatSort;
   noData: string;
   maxDate = new Date();
-<<<<<<< HEAD
   dontHide: boolean;
-=======
   credentialData: any;
->>>>>>> b681c4078e07280da4143a0b02766e01932f084a
   constructor(private onlineTransact: OnlineTransactionService,
     private eventService: EventService, private utilService: UtilService,
     private subInjectService: SubscriptionInject,
