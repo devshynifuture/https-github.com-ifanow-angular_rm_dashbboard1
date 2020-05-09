ce883e902c7db4c12247a1a84075a76c9bcd70f8
sarvesh shinde <sarvesh@futurewise.co.in>
2020-05-09 14:18:41 +0530

    risk profile in overview section changes

--- src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/overview-profile.module.ts
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/overview-profile.module.ts
@@ -4,17 +4,16 @@ import { CommonModule } from '@angular/common';
 import { OverviewProfileRoutingModule } from './overview-profile-routing.module';
 import { OverviewProfileComponent } from './overview-profile.component';
 import { MaterialModule } from 'src/app/material/material';
+import { OverviewRiskProfileComponent } from './overview-risk-profile/overview-risk-profile.component';
 
 
 
 @NgModule({
-  declarations: [OverviewProfileComponent],
+  declarations: [OverviewProfileComponent, OverviewRiskProfileComponent],
   imports: [
     CommonModule,
     OverviewProfileRoutingModule,
     MaterialModule
-
-
   ]
 })
 export class OverviewProfileModule { }
