8b4fa373acd5ce5638cd244b184e2404cc483440
sarvesh shinde <sarvesh@futurewise.co.in>
2020-05-09 18:45:12 +0530

    loder changes

--- src/app/component/protect-component/customers/component/customer/accounts/summary/market-summary/market-summary.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/summary/market-summary/market-summary.component.ts
@@ -16,6 +16,8 @@ export class MarketSummaryComponent implements OnInit {
   silverData: any;
   nifty500Data: any;
   letsideBarLoader: boolean;
+  deptDataFlag: boolean;
+  nifty500DataFlag: boolean;
 
   constructor(private cusService: CustomerService) { }
 
@@ -36,10 +38,12 @@ export class MarketSummaryComponent implements OnInit {
     )
   }
   getDeptData() {
+    this.deptDataFlag = true
     this.cusService.getDeptData().subscribe(
       data => {
         console.log(data);
         if (data) {
+          this.deptDataFlag = false;
           this.deptData = data;
           this.deptData.change_in_percentage = parseFloat(this.deptData.change_in_percentage)
           data['colourFlag'] = this.checkNumberPositiveAndNegative(data.change_in_percentage)
@@ -77,10 +81,12 @@ export class MarketSummaryComponent implements OnInit {
     this.silverData = silver;
   }
   getNifty500Data() {
+    this.nifty500DataFlag = true;
     this.cusService.getNiftyData().subscribe(
       data => {
         console.log(data);
         if (data) {
+          this.nifty500DataFlag = false
           data['colourFlag'] = this.checkNumberPositiveAndNegative(data.change_in_percentage.replace('%', ''))
           this.nifty500Data = data;
         }
