c4b621a436804cefca4ced54936e8822232d84af
Gaurav <gaurav.dwivedi1990@gmail.com>
2020-05-12 09:42:25 +0530

    Transaction Issues resolved GD commit

--- src/app/component/protect-component/AdviserComponent/transactions/overview-transactions/IIN/UCC-Creation/personal-details-inn/personal-details-inn.component.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/overview-transactions/IIN/UCC-Creation/personal-details-inn/personal-details-inn.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Input, ViewChildren, QueryList } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
-import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
-import { DatePipe } from '@angular/common';
-import { UtilService, ValidatorType } from 'src/app/services/util.service';
-import { EventService } from 'src/app/Data-service/event.service';
-import { ProcessTransactionService } from '../../../doTransaction/process-transaction.service';
-import { OnlineTransactionService } from '../../../../online-transaction.service';
-import { AuthService } from 'src/app/auth-service/authService';
-import { MatInput } from '@angular/material';
-import { CustomerService } from 'src/app/component/protect-component/customers/component/customer/customer.service';
-import { PeopleService } from 'src/app/component/protect-component/PeopleComponent/people.service';
+import {Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {SubscriptionInject} from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
+import {DatePipe} from '@angular/common';
+import {UtilService, ValidatorType} from 'src/app/services/util.service';
+import {EventService} from 'src/app/Data-service/event.service';
+import {ProcessTransactionService} from '../../../doTransaction/process-transaction.service';
+import {OnlineTransactionService} from '../../../../online-transaction.service';
+import {AuthService} from 'src/app/auth-service/authService';
+import {MatInput} from '@angular/material';
+import {CustomerService} from 'src/app/component/protect-component/customers/component/customer/customer.service';
+import {PeopleService} from 'src/app/component/protect-component/PeopleComponent/people.service';
 
 @Component({
   selector: 'app-personal-details-inn',
@@ -29,9 +29,9 @@ export class PersonalDetailsInnComponent implements OnInit {
   holder = {
     type: 'first',
     data: ''
-  }
+  };
   replaceObj: { panNumber: any; clientName: any; maidenName: any; fatherName: any; motherName: any; dateOfBirth: any; gender: any; martialStatus: any; };
-  validatorType = ValidatorType
+  validatorType = ValidatorType;
   changedValue: string;
   doneData: any;
   advisorId: any;
@@ -44,52 +44,55 @@ export class PersonalDetailsInnComponent implements OnInit {
 
 
   constructor(public subInjectService: SubscriptionInject, private fb: FormBuilder,
-    private processTransaction: ProcessTransactionService,
-    private onlineTransact: OnlineTransactionService, private datePipe: DatePipe,
-    private peopleService: PeopleService, private custumService: CustomerService,
-    public utils: UtilService,
-    public eventService: EventService) {
-    this.clientId = AuthService.getClientId()
+              private processTransaction: ProcessTransactionService,
+              private onlineTransact: OnlineTransactionService, private datePipe: DatePipe,
+              private peopleService: PeopleService, private custumService: CustomerService,
+              public utils: UtilService,
+              public eventService: EventService) {
+    this.clientId = AuthService.getClientId();
   }
+
   @Input()
   set data(data) {
     this.inputData = data;
-    this.clientData = data.clientData
-    console.log('all data in per', this.inputData)
+    this.clientData = data.clientData;
+    console.log('all data in per', this.inputData);
     if (data && data.holderList) {
-      this.getdataForm(data.holderList[0])
-      this.firstHolder = data.holderList[0]
-      this.secondHolder = data.holderList[1]
-      this.thirdHolder = data.holderList[2]
-      console.log('return data', data)
+      this.getdataForm(data.holderList[0]);
+      this.firstHolder = data.holderList[0];
+      this.secondHolder = data.holderList[1];
+      this.thirdHolder = data.holderList[2];
+      console.log('return data', data);
     } else if (data && data.firstHolder) {
-      this.getdataForm(data.firstHolder)
-      this.firstHolder = data.firstHolder
-      this.secondHolder = data.secondHolder
-      this.thirdHolder = data.thirdHolder
+      this.getdataForm(data.firstHolder);
+      this.firstHolder = data.firstHolder;
+      this.secondHolder = data.secondHolder;
+      this.thirdHolder = data.thirdHolder;
     }
-    this.generalDetails = data
+    this.generalDetails = data;
   }
 
   get data() {
     return this.inputData;
   }
+
   ngOnInit() {
-    this.advisorId = AuthService.getAdvisorId()
-    this.doneData = false
+    this.advisorId = AuthService.getAdvisorId();
+    this.doneData = false;
     if (this.firstHolder) {
-      this.getdataForm(this.firstHolder)
+      this.getdataForm(this.firstHolder);
     } else {
-      this.getdataForm('')
+      this.getdataForm('');
       if (this.clientData) {
-        this.getAddressList(this.clientData)
+        this.getAddressList(this.clientData);
       }
     }
-    this.holdingList = []
-    this.obj1 = []
+    this.holdingList = [];
+    this.obj1 = [];
   }
+
   close() {
-    this.changedValue = 'close'
+    this.changedValue = 'close';
     const fragmentData = {
       direction: 'top',
       state: 'close'
@@ -97,6 +100,7 @@ export class PersonalDetailsInnComponent implements OnInit {
 
     this.eventService.changeUpperSliderState(fragmentData);
   }
+
   getAddressList(data) {
     if (data.userType == 2) {
       this.sendObj = {
@@ -106,14 +110,14 @@ export class PersonalDetailsInnComponent implements OnInit {
         data => {
           console.log(data);
           this.addressList = data;
-          console.log('adrress', this.addressList)
+          console.log('adrress', this.addressList);
           if (this.addressList.emailList.length > 0) {
-            this.addressList.email = this.addressList.emailList[0].email
+            this.addressList.email = this.addressList.emailList[0].email;
           }
           if (this.addressList.mobileList.length > 0) {
-            this.addressList.mobileNo = this.addressList.mobileList[0].mobileNo
+            this.addressList.mobileNo = this.addressList.mobileList[0].mobileNo;
           }
-          this.getdataForm(this.addressList)
+          this.getdataForm(this.addressList);
         },
         err => {
           this.addressList = {};
@@ -122,31 +126,32 @@ export class PersonalDetailsInnComponent implements OnInit {
     } else {
       this.sendObj = {
         familyMemberId: data.familyMemberId,
-      }
+      };
       this.custumService.getFamilyMembers(this.sendObj).subscribe(
         data => {
           this.addressList = data[0];
-          console.log('adrress', this.addressList)
+          console.log('adrress', this.addressList);
           if (this.addressList.emailList.length > 0) {
-            this.addressList.email = this.addressList.emailList[0].email
+            this.addressList.email = this.addressList.emailList[0].email;
           } else if (this.addressList.mobileList.length > 0) {
-            this.addressList.mobileNo = this.addressList.mobileList[0].mobileNo
+            this.addressList.mobileNo = this.addressList.mobileList[0].mobileNo;
           }
-          this.getdataForm(this.addressList)
+          this.getdataForm(this.addressList);
         },
         err => {
-          console.error(err)
+          console.error(err);
         }
       );
     }
   }
+
   getdataForm(data) {
     if (!data) {
       data = {
         address: {}
-      }
+      };
     } else if (!data.address) {
-      data.address = null
+      data.address = null;
     }
 
     this.personalDetails = this.fb.group({
@@ -170,11 +175,13 @@ export class PersonalDetailsInnComponent implements OnInit {
       country: [!data.address ? data.country : data.address.country],
     });
   }
+
   getFormControl(): any {
     return this.personalDetails.controls;
   }
+
   openContactDetails(data) {
-    this.doneData = true
+    this.doneData = true;
     const subscription = this.processTransaction.openContact(data).subscribe(
       upperSliderData => {
         if (UtilService.isDialogClose(upperSliderData)) {
@@ -183,12 +190,14 @@ export class PersonalDetailsInnComponent implements OnInit {
       }
     );
   }
+
   reset() {
     this.personalDetails.reset();
   }
+
   SendToForm(value, flag) {
     if (flag == true) {
-      this.doneData = true
+      this.doneData = true;
     }
     if (value == 'first') {
       this.savePersonalDetails(value);
@@ -198,8 +207,7 @@ export class PersonalDetailsInnComponent implements OnInit {
       } else {
         return;
       }
-    }
-    else if (value == 'second') {
+    } else if (value == 'second') {
       this.savePersonalDetails(value);
       if (this.secondHolder && this.secondHolder.panNumber) {
         this.holder.type = value;
@@ -207,60 +215,61 @@ export class PersonalDetailsInnComponent implements OnInit {
       } else {
         this.reset();
       }
-    }
-    else if (value == 'third') {
+    } else if (value == 'third') {
       this.savePersonalDetails(value);
       if (this.thirdHolder && this.thirdHolder.panNumber) {
         this.holder.type = value;
         this.personalDetails.setValue(this.thirdHolder);
       } else {
         this.reset();
-      };
+      }
+      ;
     } else {
       this.savePersonalDetails(value);
     }
 
-    this.obj1.firstHolder = this.firstHolder
+    this.obj1.firstHolder = this.firstHolder;
     this.obj1.firstHolder.dob = new Date(this.firstHolder.dob).getTime();
     this.obj1.secondHolder = this.secondHolder;
     this.obj1.thirdHolder = this.thirdHolder;
     this.obj1.generalDetails = this.generalDetails;
-    this.obj1.holderList = this.inputData.holderList
-    this.obj1.bankDetailList = this.inputData.bankDetailList
-    this.obj1.nomineeList = this.inputData.nomineeList
+    this.obj1.holderList = this.inputData.holderList;
+    this.obj1.bankDetailList = this.inputData.bankDetailList;
+    this.obj1.nomineeList = this.inputData.nomineeList;
     this.obj1.fatcaDetail = this.inputData.fatcaDetail;
-    this.obj1.clientData = this.clientData
+    this.obj1.clientData = this.clientData;
     if (flag == true) {
       this.openContactDetails(this.obj1);
     }
   }
+
   savePersonalDetails(value) {
     if (this.personalDetails.invalid) {
       for (let element in this.personalDetails.controls) {
-        console.log(element)
+        console.log(element);
         if (this.personalDetails.get(element).invalid) {
           this.inputs.find(input => !input.ngControl.valid).focus();
           this.personalDetails.controls[element].markAsTouched();
         }
       }
     } else {
-      this.setEditHolder(this.holder.type, value)
+      this.setEditHolder(this.holder.type, value);
     }
   }
 
   setEditHolder(type, value) {
     switch (type) {
-      case "first":
+      case 'first':
         this.firstHolder = this.personalDetails.value;
         this.holder.type = value;
         break;
 
-      case "second":
+      case 'second':
         this.secondHolder = this.personalDetails.value;
         this.holder.type = value;
         break;
 
-      case "third":
+      case 'third':
         this.thirdHolder = this.personalDetails.value;
         this.holder.type = value;
         break;
