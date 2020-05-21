e4eea30251d806c085c3f57af126e680b89c7cbc
Sagar Shah <sagar.shah@futurewise.co.in>
2020-05-21 12:49:53 +0530

    handle resolver in correct way

--- src/app/component/protect-component/customers/resolvers/advisor-and-organization-info.service.ts
+++ src/app/component/protect-component/customers/resolvers/advisor-and-organization-info.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, of, forkJoin } from 'rxjs';
 import { SettingsService } from '../../AdviserComponent/setting/settings.service';
 import { AuthService } from 'src/app/auth-service/authService';
 import { map, catchError } from 'rxjs/operators';
@@ -7,7 +7,7 @@ import { Resolve } from '@angular/router';
 import { EventService } from 'src/app/Data-service/event.service';
 
 @Injectable()
-export class AdvisorAndOrganizationInfoService implements Resolve<boolean> {
+export class AdvisorAndOrganizationInfoService implements Resolve<any> {
 
   private clientData:any;
   private advisorId:any;
@@ -18,20 +18,28 @@ export class AdvisorAndOrganizationInfoService implements Resolve<boolean> {
     this.clientData = AuthService.getClientData();
     this.advisorId = this.clientData.advisorId;
   }
-  resolve(): Observable<boolean> {
+  resolve(): Observable<any> {
     let orgDetail = this.settingsService.getOrgProfile({advisorId: this.advisorId})
   
     // advisor details
     let advisorDetail = this.settingsService.getPersonalProfile({id: this.advisorId})
 
     // NEED TO HANDLE 
-    return combineLatest(advisorDetail.pipe(
-      map(data => AuthService.setAdvisorDetails(data))
+    return forkJoin(advisorDetail.pipe(
+      map(data =>{
+         AuthService.setAdvisorDetails(data)
+         return data
+      }),
+      catchError(() => of(false))
     ), orgDetail.pipe(
-      map(data => AuthService.setOrgDetails(data))
+      map(data => {
+        AuthService.setOrgDetails(data)
+        return data
+      }),
+      catchError(() => of(false))
     ))
-    .map(([advisorDetail, orgDetail])=>{
-      if(advisorDetail === null && orgDetail === null) return false
+    .map(([advisorDetailRes, orgDetailRes])=>{
+      if(advisorDetailRes === null && orgDetailRes === null) return false
       return true
     })
   }
