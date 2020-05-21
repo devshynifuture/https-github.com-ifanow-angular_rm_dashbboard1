dc5284ec9280351c1e27e1872ccd02455ec5f1ed
sarvesh shinde <sarvesh@futurewise.co.in>
2020-05-21 19:41:17 +0530

    please check the error and solve this

--- src/app/component/protect-component/AdviserComponent/transactions/transactions.module.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/transactions.module.ts
@@ -1,25 +1,25 @@
-import {EntryComponentsModule} from '../../../../entry.components.module';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TransactionsComponent} from './transactions/transactions.component';
-import {TransactionRoutingModule} from './transaction-routing.module';
-import {OverviewTransactionsComponent} from './overview-transactions/overview-transactions.component';
-import {TransactionsListComponent} from './transactions-list/transactions-list.component';
-import {InvestorsTransactionsComponent} from './investors-transactions/investors-transactions.component';
-import {MandatesTransactionsComponent} from './mandates-transactions/mandates-transactions.component';
-import {KycTransactionsComponent} from './kyc-transactions/kyc-transactions.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MaterialModule} from 'src/app/material/material';
-import {SettingsTransactionsModule} from './settings-transactions/settings-transactions.module';
-import {CustomDirectiveModule} from 'src/app/common/directives/common-directive.module';
-import {PhotoSigntureComponent} from './overview-transactions/know-your-customer/photo-signture/photo-signture.component';
-import {VideoKycComponent} from './overview-transactions/know-your-customer/video-kyc/video-kyc.component';
-import {CustomCommonModule} from 'src/app/common/custom.common.module';
-import {ScrollDispatchModule, ScrollingModule} from '@angular/cdk/scrolling';
-import {SatDatepickerModule, SatNativeDateModule} from 'saturn-datepicker';
-import {TransactionMobileViewComponent} from './transaction-mobile-view/transaction-mobile-view.component';
-import {TransactionBottomButtonComponent} from './transaction-mobile-view/transaction-bottom-button/transaction-bottom-button.component';
-import {IinCreationLoaderComponent} from './overview-transactions/IIN/UCC-Creation/submit-review-inn/iin-creation-loader/iin-creation-loader.component';
+import { EntryComponentsModule } from '../../../../entry.components.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { TransactionRoutingModule } from './transaction-routing.module';
+import { OverviewTransactionsComponent } from './overview-transactions/overview-transactions.component';
+import { TransactionsListComponent } from './transactions-list/transactions-list.component';
+import { InvestorsTransactionsComponent } from './investors-transactions/investors-transactions.component';
+import { MandatesTransactionsComponent } from './mandates-transactions/mandates-transactions.component';
+import { KycTransactionsComponent } from './kyc-transactions/kyc-transactions.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material';
+import { SettingsTransactionsModule } from './settings-transactions/settings-transactions.module';
+import { CustomDirectiveModule } from 'src/app/common/directives/common-directive.module';
+import { PhotoSigntureComponent } from './overview-transactions/know-your-customer/photo-signture/photo-signture.component';
+import { VideoKycComponent } from './overview-transactions/know-your-customer/video-kyc/video-kyc.component';
+import { CustomCommonModule } from 'src/app/common/custom.common.module';
+import { ScrollDispatchModule, ScrollingModule } from '@angular/cdk/scrolling';
+import { SatDatepickerModule, SatNativeDateModule } from 'saturn-datepicker';
+import { TransactionMobileViewComponent } from './transaction-mobile-view/transaction-mobile-view.component';
+import { TransactionBottomButtonComponent } from './transaction-mobile-view/transaction-bottom-button/transaction-bottom-button.component';
+// import {IinCreationLoaderComponent} from './overview-transactions/IIN/UCC-Creation/submit-review-inn/iin-creation-loader/iin-creation-loader.component';
 
 
 @NgModule({
@@ -55,7 +55,8 @@ import {IinCreationLoaderComponent} from './overview-transactions/IIN/UCC-Creati
     ScrollingModule
   ],
   entryComponents: [TransactionMobileViewComponent,
-    TransactionBottomButtonComponent, IinCreationLoaderComponent]
+    TransactionBottomButtonComponent,]
+  // IinCreationLoaderComponent
 })
 export class TransactionsModule {
 
