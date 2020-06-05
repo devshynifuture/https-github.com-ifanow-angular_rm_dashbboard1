a55a14e97974b5359bc8a635f75724c2ec7794a5
Gaurav <gaurav@futurewise.co.in>
2020-06-05 13:59:58 +0530

    prod issues
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-file-upload/backoffice-file-upload.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-file-upload/backoffice-file-upload.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ReconciliationService } from '../backoffice-aum-reconciliation/reconciliation/reconciliation.service';
-import { AuthService } from 'src/app/auth-service/authService';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpService } from 'src/app/http-service/http-service';
-import { BackofficeFileUploadService } from './backoffice-file-upload.service';
-import { EventService } from 'src/app/Data-service/event.service';
-import { SettingsService } from '../../setting/settings.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ReconciliationService} from '../backoffice-aum-reconciliation/reconciliation/reconciliation.service';
+import {AuthService} from 'src/app/auth-service/authService';
+import {HttpHeaders} from '@angular/common/http';
+import {HttpService} from 'src/app/http-service/http-service';
+import {BackofficeFileUploadService} from './backoffice-file-upload.service';
+import {EventService} from 'src/app/Data-service/event.service';
+import {SettingsService} from '../../setting/settings.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-backoffice-file-upload',
@@ -14,19 +14,25 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./backoffice-file-upload.component.scss']
 })
 export class BackofficeFileUploadComponent implements OnInit {
+
+  selectedFileType: any = '';
   filterRTA: any = 0;
   filterStatus: any = 2;
-  selectedFileType: any = "";
+  filter: any = {
+    rt: 0,
+    status: 2
+  };
   fileType: any;
   advisorId: any;
   filterList: any;
   arnRiaList = [];
   arnRiaId;
   showFilter = true;
-  filter: any = {
-    rt: 0,
-    status: 2
-  }
+  fileName: any;
+  fileSize: any;
+  targetFile: any;
+  uploadButton = false;
+
   constructor(
     private reconService: ReconciliationService,
     private eventService: EventService,
@@ -35,32 +41,30 @@ export class BackofficeFileUploadComponent implements OnInit {
     private settingService: SettingsService,
     private router: Router,
     private activatedRoute: ActivatedRoute
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.advisorId = AuthService.getAdvisorId();
     this.reconService.getBackOfficeFileUploadFileType({}).subscribe((data) => {
-      console.log("thisis filetype data", data);
+      console.log('thisis filetype data', data);
       this.fileType = data;
-    })
+    });
     this.reconService.getBackOfficeFilter({}).subscribe((data) => {
       this.filterList = data;
-      console.log("this is filter list :::", data);
+      console.log('this is filter list :::', data);
     });
-    this.settingService.getArnlist({ advisorId: this.advisorId })
+    this.settingService.getArnlist({advisorId: this.advisorId})
       .subscribe(res => {
         if (res) {
           this.arnRiaList = res;
         } else {
-          this.eventService.openSnackBar('No arn ria code found!!', "DISMISS");
+          this.eventService.openSnackBar('No arn ria code found!!', 'DISMISS');
         }
       });
     this.setFilter();
   }
-  fileName: any;
-  fileSize: any;
-  targetFile: any;
-  uploadButton: boolean = false;
+
   getFile(e) {
     this.fileName = e.currentTarget.files[0].name;
     this.fileSize = this.formatBytes(e.currentTarget.files[0].size, 2);
@@ -101,11 +105,11 @@ export class BackofficeFileUploadComponent implements OnInit {
   uploadTargetFile() {
     this.uploadButton = false;
 
-    let obj = {
+    const obj = {
       fileType: this.selectedFileType,
       advisorId: this.advisorId,
       arnRiaDetailId: this.arnRiaId,
-    }
+    };
     this.reconService.getBackOfficeFileToUpload(obj).subscribe((data) => {
       // this.fileType = data;
       if (data) {
@@ -115,7 +119,9 @@ export class BackofficeFileUploadComponent implements OnInit {
   }
 
   formatBytes(bytes, decimals) {
-    if (bytes === 0) return '0 Bytes';
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
 
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
@@ -128,8 +134,8 @@ export class BackofficeFileUploadComponent implements OnInit {
 
   setDefaultRtAndStatus() {
     this.showFilter = true;
-    this.filterStatus = "2";
-    this.filterRTA = "0";
+    this.filterStatus = '2';
+    this.filterRTA = '0';
   }
 
   uploadFileRes(data, file) {
@@ -138,22 +144,22 @@ export class BackofficeFileUploadComponent implements OnInit {
         .set('Content-Type', '')
     };
     this.http.putExternal(data.presignedUrl, file, httpOptions).subscribe((responseData) => {
-      this.successFileUpload(this.selectedFileType, data.fileName)
+      this.successFileUpload(this.selectedFileType, data.fileName);
     }, error => {
 
     });
   }
 
   successFileUpload(fileType, fileName) {
-    let obj = {
-      fileType: fileType,
-      fileName: fileName,
+    const obj = {
+      fileType,
+      fileName,
       advisorId: this.advisorId,
       arnRiaDetailId: this.arnRiaId
-    }
+    };
     this.reconService.successBackOfficeFileToUpload(obj).subscribe((data) => {
-      this.fileName = "";
-      this.fileSize = "";
+      this.fileName = '';
+      this.fileSize = '';
       this.eventService.openSnackBar('File uploaded successfully', 'Dismiss');
       // reload
       this.setFilter();
@@ -165,11 +171,12 @@ export class BackofficeFileUploadComponent implements OnInit {
     this.filter.rt = this.filterRTA;
     this.BackOffice.addFilterData(this.filter);
   }
+
   setDefaultFilter() {
-    this.BackOffice.addFilterData({ rt: 0, status: 2 });
+    this.BackOffice.addFilterData({rt: 0, status: 2});
   }
 
   refresh(flag) {
-    this.setFilter()
+    this.setFilter();
   }
 }
