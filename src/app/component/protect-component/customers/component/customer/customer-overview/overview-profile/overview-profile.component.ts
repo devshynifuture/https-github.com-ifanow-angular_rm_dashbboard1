5e1fcf1e322defe2a1d015d583d641dd3259d13c
sarvesh shinde <sarvesh@futurewise.co.in>
2020-06-08 15:01:52 +0530

    FM logic and bugs resolved

--- src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/overview-profile.component.ts
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-profile/overview-profile.component.ts
@@ -1,22 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {AddFamilyMemberComponent} from './add-family-member/add-family-member.component';
-import {SubscriptionInject} from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
-import {UtilService} from 'src/app/services/util.service';
-import {HistoryViewComponent} from './history-view/history-view.component';
-import {AddClientComponent} from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/add-client.component';
-import {CustomerService} from '../../customer.service';
-import {EventService} from 'src/app/Data-service/event.service';
-import {MatDialog} from '@angular/material';
-import {AuthService} from 'src/app/auth-service/authService';
-import {ClientAddressComponent} from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-address/client-address.component';
-import {ClientDematComponent} from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-demat/client-demat.component';
-import {ClientBankComponent} from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-bank/client-bank.component';
-import {PeopleService} from 'src/app/component/protect-component/PeopleComponent/people.service';
-import {EnumDataService} from 'src/app/services/enum-data.service';
-import {ActivatedRoute} from '@angular/router';
-import {CancelFlagService} from 'src/app/component/protect-component/PeopleComponent/people/Component/people-service/cancel-flag.service';
-import {UpdateClientProfileComponent} from './update-client-profile/update-client-profile.component';
-import {AgePopupComponent} from './age-popup/age-popup.component';
+import { Component, OnInit } from '@angular/core';
+import { AddFamilyMemberComponent } from './add-family-member/add-family-member.component';
+import { SubscriptionInject } from 'src/app/component/protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
+import { UtilService } from 'src/app/services/util.service';
+import { HistoryViewComponent } from './history-view/history-view.component';
+import { AddClientComponent } from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/add-client.component';
+import { CustomerService } from '../../customer.service';
+import { EventService } from 'src/app/Data-service/event.service';
+import { MatDialog } from '@angular/material';
+import { AuthService } from 'src/app/auth-service/authService';
+import { ClientAddressComponent } from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-address/client-address.component';
+import { ClientDematComponent } from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-demat/client-demat.component';
+import { ClientBankComponent } from 'src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-bank/client-bank.component';
+import { PeopleService } from 'src/app/component/protect-component/PeopleComponent/people.service';
+import { EnumDataService } from 'src/app/services/enum-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { CancelFlagService } from 'src/app/component/protect-component/PeopleComponent/people/Component/people-service/cancel-flag.service';
+import { UpdateClientProfileComponent } from './update-client-profile/update-client-profile.component';
+import { AgePopupComponent } from './age-popup/age-popup.component';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-overview-profile',
@@ -47,9 +48,10 @@ export class OverviewProfileComponent implements OnInit {
     dematList: false,
     bankList: false
   };
+  duplicateFlag: any;
 
   constructor(private peopleService: PeopleService, private authService: AuthService, public dialog: MatDialog, public subInjectService: SubscriptionInject,
-              private cusService: CustomerService, private eventService: EventService, private utils: UtilService, private enumDataService: EnumDataService, private route: ActivatedRoute, private cancelFlagService: CancelFlagService) {
+    private cusService: CustomerService, private eventService: EventService, private utils: UtilService, private enumDataService: EnumDataService, private route: ActivatedRoute, private cancelFlagService: CancelFlagService) {
   }
 
   ngOnInit() {
@@ -66,12 +68,12 @@ export class OverviewProfileComponent implements OnInit {
     // console.log(sessionStorage.getItem('clientData'));
     // this.clientOverviewData = JSON.parse(sessionStorage.getItem('clientData'));
     this.letsideBarLoader = true;
-    this.getFamilyMembersList(this.clientData);
+    this.getClientData(this.clientData);
     this.getAddressList(this.clientData);
     this.getDematList(this.clientData);
     this.getBankList(this.clientData);
-    this.getClientData(this.clientData);
     this.enumDataService.getDataForTaxMasterService();
+    this.getFamilyMembersList(this.clientData);
   }
 
   getClientData(data) {
@@ -86,9 +88,10 @@ export class OverviewProfileComponent implements OnInit {
         } else {
           this.letsideBarLoader = false;
           // this.authService.setClientData(data);
-          (data.martialStatusId == 1) ? data.martialStatus = 'Married' : (data.martialStatusId == 2) ? data.martialStatus = 'Unmarried' : (data.martialStatusId == 0) ? data.martialStatus = 'N/A' : data.martialStatus = 'Other';
+          (data.martialStatusId == 1 || data.martialStatusId == 0) ? data.martialStatus = 'Married' : (data.martialStatusId == 2) ? data.martialStatus = 'Unmarried' : (data.martialStatusId == 0) ? data.martialStatus = 'N/A' : data.martialStatus = 'Other';
           (data.genderId == 1) ? data.gender = 'Male' : (data.genderId == 2) ? data.gender = 'Female' : data.gender = 'Other';
           this.clientOverviewData = data;
+          this.authService.setClientData(data)
 
           this.calculateAge(this.clientOverviewData.dateOfBirth);
         }
@@ -123,6 +126,9 @@ export class OverviewProfileComponent implements OnInit {
     this.cusService.getFamilyMembers(obj).subscribe(
       data => {
         if (data && data.length > 0) {
+          this.duplicateFlag = data.map(element => {
+            return
+          })
           data.forEach(element => {
             if (element.name.length > 22) {
               element.shortName = element.name.substr(0, element.name.indexOf(' '));
@@ -250,8 +256,24 @@ export class OverviewProfileComponent implements OnInit {
   open(value, data) {
     let component;
     if (value == 'add') {
+      if (this.clientOverviewData.clientType == 1) {
+        if (this.familyMemberList) {
+          let relationType = (this.clientOverviewData.genderId == 1) ? 3 : 2
+          this.duplicateFlag = this.familyMemberList.some(element => {
+            if (element.relationshipId == relationType) {
+              return true
+            }
+            else {
+              return false
+            }
+          })
+        } else {
+          this.duplicateFlag = false;
+        }
+      }
+      this.clientOverviewData['duplicateFlag'] = this.duplicateFlag;
       component = AddFamilyMemberComponent;
-      data = {flag: 'Add Family Member', fieldFlag: 'familyMember', client: this.clientOverviewData};
+      data = { flag: 'Add Family Member', fieldFlag: 'familyMember', client: this.clientOverviewData };
     } else {
       data.flag = 'Edit Family Member';
       data.fieldFlag = 'familyMember';
