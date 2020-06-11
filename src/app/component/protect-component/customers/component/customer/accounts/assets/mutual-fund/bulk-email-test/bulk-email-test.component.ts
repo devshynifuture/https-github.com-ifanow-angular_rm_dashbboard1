c39a42a04b82d952179e031256f5539121ca43d6
devshyni <devshyni@futurewise.co.in>
2020-06-11 23:08:37 +0530

    pdf file name

--- src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/bulk-email-test/bulk-email-test.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/bulk-email-test/bulk-email-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MutualFundOverviewComponent } from '../mutual-fund/mutual-fund-overview/mutual-fund-overview.component';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-bulk-email-test',
@@ -12,8 +13,15 @@ export class BulkEmailTestComponent implements OnInit {
   sendData = [{
     clientId: 88317
   }]
-  getObj: { htmlInput: string; name: string; landscape: boolean; key: string; svg: string; };
-  constructor(public overview: MutualFundOverviewComponent ) { }
+  getObj: any;
+  dataSource3: any;
+  dataSource: any;
+  mfData: any;
+  dataSource4: any;
+  dataSource2: any;
+  chart: any;
+  
+  constructor(public overview: MutualFundOverviewComponent, private UtilService : UtilService) { }
 
   ngOnInit() {
     this.getUploadData()
@@ -21,5 +29,26 @@ export class BulkEmailTestComponent implements OnInit {
   getUploadData() {
     this.getObj = this.overview.uploadData(this.sendData)
     console.log('data ======', this.getObj)
+    this.dataSource3 = this.getObj.dataSource3;
+    this.dataSource = this.getObj.dataSource;
+    this.mfData = this.getObj.mfData;
+    this.dataSource4 = this.getObj.dataSource4;
+    this.dataSource2 = this.getObj.dataSource2;
+    this.chart = this.getObj.dataSource2;
+    this.generatePdf()
+
+  }
+  generatePdf() {
+    //this.svg = this.chart.getSVG()
+    let para = document.getElementById('template');
+    let obj = {
+      htmlInput: para.innerHTML,
+      name: 'Overview',
+      landscape: true,
+      key: 'showPieChart',
+      svg: ''
+    }
+ this.UtilService.htmlToPdf(para.innerHTML, 'Overview', false, true, 'showPieChart', '')
+    return obj
   }
 }
