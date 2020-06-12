5cef7061965ec6c42bc608755747fe941d702842
aman9893 <aman@futurewise.co.in>
2020-06-12 17:50:09 +0530

    add new pages

--- src/app/component/protect-component/AdviserComponent/backOffice/bulk-report-sending/bulk-report-sending.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/bulk-report-sending/bulk-report-sending.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bulk-report-sending.component.scss']
 })
 export class BulkReportSendingComponent implements OnInit {
+  displayedColumns: string[] = ['type', 'sendDate', 'recipients', 'emails', 'status'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
 }
+export interface PeriodicElement {
+  type: string;
+  sendDate: string;
+  recipients: string;
+  emails: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { type: 'MF Overview / MF Summary / MF Capital gains - Summary', sendDate: '02/05/2020', recipients: '115', emails: '2', status: 'Sent' },
+  { type: 'MF Overview / MF Summary', sendDate: '02/05/2020', recipients: '115', emails: '2', status: 'Sent' },
+
+];
