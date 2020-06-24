fd1afcdc388aaf5d8259006cb3e1d84382e786d6
syed khizaruddin <jarvis@syeds-MacBook-Air.local>
2020-06-24 20:15:42 +0530

    changes in google connect dialog

--- src/app/component/protect-component/AdviserComponent/Email/email-component/email-list/email-listing/google-connect/google-connect.component.ts
+++ src/app/component/protect-component/AdviserComponent/Email/email-component/email-list/email-listing/google-connect/google-connect.component.ts
@@ -30,13 +30,6 @@ export class GoogleConnectComponent implements OnInit {
   //  URLv2 = 'https://accounts.google.com/o/oauth2/v2/auth';
   //  SCOPE2 = 'https://mail.google.com+https://www.googleapis.com/auth/calendar';
 
-  URL = 'https://www.googleapis.com/oauth2/v4/token';
-  CLIENT_ID = '579886643607-h1m21go2dct12nva48pi5mr7meejv7nh.apps.googleusercontent.com';
-  CLIENT_SECRET = 'Zpw9vHRljgbRbgAOe4UQArld';
-  RESPONSE_TYPE = 'code';
-  SCOPE = 'htpps://mail.google.com';
-  GRANT_TYPE = 'authorization_code';
-  REDIRECT_URI = 'http://localhost:4200/admin/emails/inbox';
   advisorId;
   emailId;
   showEmailInput: boolean = false;
@@ -87,39 +80,25 @@ export class GoogleConnectComponent implements OnInit {
 
   }
 
-  toggleShowEmailInput() {
-    this.showEmailInput = !this.showEmailInput;
-  }
+  // gmailRedirectUrlCreation() {
+  //   const hitGmailUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=579886643607-h1m21go2dct12nva48pi5mr7meejv7nh.apps.googleusercontent.com&response_type=code&scope=https://mail.google.com+https://www.googleapis.com/auth/calendar&redirect_uri=http://localhost:4200/redirect&access_type=offline';
 
-  gmailRedirectUrlCreation() {
-    const hitGmailUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=579886643607-h1m21go2dct12nva48pi5mr7meejv7nh.apps.googleusercontent.com&response_type=code&scope=https://mail.google.com+https://www.googleapis.com/auth/calendar&redirect_uri=http://localhost:4200/redirect&access_type=offline';
-
-    localStorage.removeItem('associatedGoogleEmailId');
-    // compare it with authEmail id
-    if (AuthService.getUserInfo().userName == this.redirectForm.get('googleConnectEmail').value) {
-      localStorage.setItem('associatedGoogleEmailId', this.redirectForm.get('googleConnectEmail').value)
-      const redirectWindow = window.open(hitGmailUrl);
-      const lookForSuccessToken = setInterval(() => {
-        if (localStorage.getItem('successStoringToken') === 'true') {
-          clearInterval(lookForSuccessToken);
-          redirectWindow.close();
-          this.router.navigate(['../'], { relativeTo: this.activatedRoute });
-        }
-      }, 1000);
-    } else {
-      this.eventService.openSnackBar("Your email id is not same as your login credentials", "Dismiss")
-    }
-
-
-    // setTimeout(() => {
-    //   redirectWindow.close();
-    //   console.log("tis is something to work with");
-    //   if (localStorage.getItem('successStoringToken') === 'true') {
-    //     this.router.navigate(['../'], { relativeTo: this.activatedRoute });
-    //   }
-    // }, 25000);
-
-  }
+  //   localStorage.removeItem('associatedGoogleEmailId');
+  //   // compare it with authEmail id
+  //   if (AuthService.getUserInfo().userName == this.redirectForm.get('googleConnectEmail').value) {
+  //     localStorage.setItem('associatedGoogleEmailId', this.redirectForm.get('googleConnectEmail').value)
+  //     const redirectWindow = window.open(hitGmailUrl);
+  //     const lookForSuccessToken = setInterval(() => {
+  //       if (localStorage.getItem('successStoringToken') === 'true') {
+  //         clearInterval(lookForSuccessToken);
+  //         redirectWindow.close();
+  //         this.router.navigate(['../'], { relativeTo: this.activatedRoute });
+  //       }
+  //     }, 1000);
+  //   } else {
+  //     this.eventService.openSnackBar("Your email id is not same as your login credentials", "Dismiss")
+  //   }
+  // }
 
   openGoogleConnectDialog() {
 
@@ -130,7 +109,6 @@ export class GoogleConnectComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-
     });
 
   }
