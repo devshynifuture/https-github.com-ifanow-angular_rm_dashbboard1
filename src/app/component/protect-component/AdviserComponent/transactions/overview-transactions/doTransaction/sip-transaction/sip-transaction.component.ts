adc3443a26c39cb201bd9e379f94413ee7d56c6e
devshyni <devshyni@futurewise.co.in>
2020-07-04 16:08:27 +0530

    bulk-email

--- src/app/component/protect-component/AdviserComponent/transactions/overview-transactions/doTransaction/sip-transaction/sip-transaction.component.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/overview-transactions/doTransaction/sip-transaction/sip-transaction.component.ts
@@ -702,8 +702,8 @@ export class SipTransactionComponent implements OnInit {
       this.folioDetails = {}
       this.sipTransaction.controls.schemeSelection.setValue('1')
       this.sipTransaction.controls.folioSelection.setValue('1')
-      this.sipTransaction.controls.schemeSip.setValue({'schemeName': this.schemeName})
       this.filterSchemeList = of([{'schemeName': this.schemeName}])
+      this.sipTransaction.controls.schemeSip.setValue({'schemeName': this.schemeName})
       Object.assign(this.folioDetails, {folioNumber: this.folioNumber});
       this.scheme ={
         'schemeName': this.schemeName,
