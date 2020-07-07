04e515e648df0b66571872189b5bd093427ec3b6
Gaurav <gaurav@futurewise.co.in>
2020-07-07 14:53:51 +0530

    prod issue resolved GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/customers/component/customer/mobile/transactions/mobile-transactions/mobile-transactions.component.ts
+++ src/app/component/protect-component/customers/component/customer/mobile/transactions/mobile-transactions/mobile-transactions.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class MobileTransactionsComponent implements OnInit {
 
   constructor() { }
-
+  myControl;
+  option;
   ngOnInit() {
   }
 
