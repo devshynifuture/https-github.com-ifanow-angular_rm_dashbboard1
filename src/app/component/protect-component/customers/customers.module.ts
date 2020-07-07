7000c3b9c05a729b0e9bb90c12925b9e19d582db
devshyni <devshyni@futurewise.co.in>
2020-07-07 18:19:17 +0530

    bulk-email

--- src/app/component/protect-component/customers/customers.module.ts
+++ src/app/component/protect-component/customers/customers.module.ts
@@ -38,7 +38,7 @@ import { MobileLeftSidenavComponent } from './component/customer/mobile/left-sid
 
 @NgModule({
   declarations: [CustomerComponent, TransactionSuccessfulComponent, MobileMyfeedComponent, MobileDocumentComponent, MobilePortfoiloComponent, MobileProfileComponent, MobileTransactionsComponent, MobileLeftSidenavComponent],
-  imports: [
+  imports: [ 
     // BrowserModule,
     CommonModule,
     MaterialModule,
