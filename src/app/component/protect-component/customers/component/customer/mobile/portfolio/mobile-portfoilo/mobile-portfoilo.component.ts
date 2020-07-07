510cb594c4803c890222be215896f8946994980b
devshyni <devshyni@futurewise.co.in>
2020-07-07 15:49:11 +0530

    bulk-email

--- src/app/component/protect-component/customers/component/customer/mobile/portfolio/mobile-portfoilo/mobile-portfoilo.component.ts
+++ src/app/component/protect-component/customers/component/customer/mobile/portfolio/mobile-portfoilo/mobile-portfoilo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-portfoilo',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mobile-portfoilo.component.scss']
 })
 export class MobilePortfoiloComponent implements OnInit {
+  openMenue: boolean = false;
+  inputData: any;
 
   constructor() { }
-
+  @Input()
+  set data(data) {
+    this.inputData = data;
+    console.log('This is Input data of proceed ', data);
+  }
+  get data() {
+    return this.inputData;
+  }
   ngOnInit() {
   }
-
+  openMenu(flag) {
+    if (flag == false) {
+      this.openMenue = true
+    } else {
+      this.openMenue = false
+    }
+  }
 }
