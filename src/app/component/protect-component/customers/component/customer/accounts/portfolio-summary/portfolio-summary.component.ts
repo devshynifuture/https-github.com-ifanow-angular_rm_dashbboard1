2a2960c14d1aaaf43524f1c1a81dd342edf94948
sarvesh <sarvesh@futurewise.co.in>
2020-07-09 17:18:54 +0530

    add

--- src/app/component/protect-component/customers/component/customer/accounts/portfolio-summary/portfolio-summary.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/portfolio-summary/portfolio-summary.component.ts
@@ -160,13 +160,13 @@ export class PortfolioSummaryComponent implements OnInit, OnDestroy {
             this.liabilityTotal = 0;
           });
           this.totalOfLiabilitiesAndTotalAssset(this.summaryTotalValue);
-          this.letsideBarLoader = true;
+          this.letsideBarLoader = false;
           this.summaryMap = tempSummaryTotalValue;
           this.pieChart('piechartMutualFund', this.summaryTotalValue);
         }
       },
       err => {
-        this.letsideBarLoader = true;
+        this.letsideBarLoader = false;
         this.finalTotal = 0;
         this.liabilityTotal = 0;
         this.totalAssetsWithoutLiability = 0;
