396079110c3b8b266ca66369dfcff4be91b8e730
sarvesh shinde <sarvesh@futurewise.co.in>
2020-07-10 18:06:18 +0530

    new move copy module component

---
+++ src/app/component/protect-component/customers/component/customer/mobile/document/mobile-document/move-copy-mobile-view/move-copy-mobile-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-move-copy-mobile-view',
+  templateUrl: './move-copy-mobile-view.component.html',
+  styleUrls: ['./move-copy-mobile-view.component.scss']
+})
+export class MoveCopyMobileViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
