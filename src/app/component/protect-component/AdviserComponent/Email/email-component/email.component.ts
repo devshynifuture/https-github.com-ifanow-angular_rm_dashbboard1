426beef26540bfed48933d7cc018a8ee2e7fce26
sunny <sunny@futurewise.co.in>
2020-07-14 21:07:46 +0530

    crm-email ui fixed

--- src/app/component/protect-component/AdviserComponent/Email/email-component/email.component.ts
+++ src/app/component/protect-component/AdviserComponent/Email/email-component/email.component.ts
@@ -1,5 +1,6 @@
 import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-email-component',
@@ -9,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class EmailComponent implements OnInit {
   displayedColumns = ['checkbox', 'position', 'name', 'img', 'symbol'];
   dataSource = ELEMENT_DATA;
-  constructor() { }
+  isCustomerEmail: boolean;
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let location;
+    location = this.router.url.split('/')[this.router.url.split('/').length - 1];
+    if (this.router.url.split('/').includes('customer')) {
+      this.isCustomerEmail = true;
+    } else {
+      this.isCustomerEmail = false;
+    }
   }
 
   // tabs = ['Primary', 'Social', 'Promotions', 'Forum'];
