079fa12a3b9fa3ea7fb1d2a4360572bb724e6c77
Gaurav <gaurav@futurewise.co.in>
2020-07-14 12:33:50 +0530

    prod issue resolved GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/customers/customers.module.ts
+++ src/app/component/protect-component/customers/customers.module.ts
@@ -1,38 +1,39 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CustomersRoutingModule } from './customers-routing.module';
-import { MaterialModule } from '../../../material/material';
-import { ChartModule } from 'angular-highcharts';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CustomersRoutingModule} from './customers-routing.module';
+import {MaterialModule} from '../../../material/material';
+import {ChartModule} from 'angular-highcharts';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 // import {BrowserModule} from '@angular/platform-browser';
-import { CustomerComponent } from './component/customer/customer.component';
+import {CustomerComponent} from './component/customer/customer.component';
 
-import { CustomCommonModule } from '../../../common/custom.common.module';
-import { EntryComponentsModule } from '../../../entry.components.module';
-import { AccountEntryModule } from './component/customer/accounts/account.entry.module';
-import { PlanEntryModule } from './component/customer/plan/plan.entry.module';
-import { DynamicComponentService } from '../../../services/dynamic-component.service';
-import { AdviceEntryModule } from './component/customer/customer-activity/advice-entry.module';
-import { CustomDirectiveModule } from 'src/app/common/directives/common-directive.module';
-import { CustomerOverviewEntryModule } from './component/customer/customer-overview/customer-overview-entry-module';
-import { TransactionSuccessfulComponent } from './component/customer/transaction-successful/transaction-successful.component';
-import { MatStepperModule } from '@angular/material/stepper';
-import { AdvisorAndOrganizationInfoService } from './resolvers/advisor-and-organization-info.service';
-import { TransactionEntryModule } from '../AdviserComponent/transactions/transaction.entry.module';
-import { SubscriptionEntry } from "../AdviserComponent/Subscriptions/subscription.entry.module";
-import { SubscriptionUpperEntry } from "../AdviserComponent/Subscriptions/subscription-upper-entry-module";
-import { MobileMyfeedComponent } from './component/customer/mobile/myfeed/mobile-myfeed/mobile-myfeed.component';
-import { MobileDocumentComponent } from './component/customer/mobile/document/mobile-document/mobile-document.component';
-import { MobilePortfoiloComponent } from './component/customer/mobile/portfolio/mobile-portfoilo/mobile-portfoilo.component';
-import { MobileProfileComponent } from './component/customer/mobile/profile/mobile-profile/mobile-profile.component';
-import { MobileTransactionsComponent } from './component/customer/mobile/transactions/mobile-transactions/mobile-transactions.component';
-import { MobileLeftSidenavComponent } from './component/customer/mobile/left-side/mobile-left-sidenav/mobile-left-sidenav.component';
-import { MobileRoutingModule } from './mobile-routing.module';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { EditDocumentPopupComponent } from './component/customer/mobile/document/mobile-document/edit-document-popup/edit-document-popup.component';
-import { UploadDocumentComponent } from './component/customer/mobile/document/mobile-document/upload-document/upload-document.component';
-import { MutualFundsComponent } from './component/customer/mobile/mutual-funds/mutual-funds/mutual-funds.component';
+import {CustomCommonModule} from '../../../common/custom.common.module';
+import {EntryComponentsModule} from '../../../entry.components.module';
+import {AccountEntryModule} from './component/customer/accounts/account.entry.module';
+import {PlanEntryModule} from './component/customer/plan/plan.entry.module';
+import {DynamicComponentService} from '../../../services/dynamic-component.service';
+import {AdviceEntryModule} from './component/customer/customer-activity/advice-entry.module';
+import {CustomDirectiveModule} from 'src/app/common/directives/common-directive.module';
+import {CustomerOverviewEntryModule} from './component/customer/customer-overview/customer-overview-entry-module';
+import {TransactionSuccessfulComponent} from './component/customer/transaction-successful/transaction-successful.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import {AdvisorAndOrganizationInfoService} from './resolvers/advisor-and-organization-info.service';
+import {TransactionEntryModule} from '../AdviserComponent/transactions/transaction.entry.module';
+import {SubscriptionEntry} from '../AdviserComponent/Subscriptions/subscription.entry.module';
+import {SubscriptionUpperEntry} from '../AdviserComponent/Subscriptions/subscription-upper-entry-module';
+import {MobileMyfeedComponent} from './component/customer/mobile/myfeed/mobile-myfeed/mobile-myfeed.component';
+import {MobileDocumentComponent} from './component/customer/mobile/document/mobile-document/mobile-document.component';
+import {MobilePortfoiloComponent} from './component/customer/mobile/portfolio/mobile-portfoilo/mobile-portfoilo.component';
+import {MobileProfileComponent} from './component/customer/mobile/profile/mobile-profile/mobile-profile.component';
+import {MobileTransactionsComponent} from './component/customer/mobile/transactions/mobile-transactions/mobile-transactions.component';
+import {MobileLeftSidenavComponent} from './component/customer/mobile/left-side/mobile-left-sidenav/mobile-left-sidenav.component';
+import {MobileRoutingModule} from './mobile-routing.module';
+import {SlickCarouselModule} from 'ngx-slick-carousel';
+import {EditDocumentPopupComponent} from './component/customer/mobile/document/mobile-document/edit-document-popup/edit-document-popup.component';
+import {UploadDocumentComponent} from './component/customer/mobile/document/mobile-document/upload-document/upload-document.component';
+import {MutualFundsComponent} from './component/customer/mobile/mutual-funds/mutual-funds/mutual-funds.component';
+import { MoveCopyMobileViewComponent } from './component/customer/mobile/document/mobile-document/move-copy-mobile-view/move-copy-mobile-view.component';
 
 
 // import { RightFilterComponent } from './component/common-component/right-filter/right-filter.component';
@@ -41,8 +42,11 @@ import { MutualFundsComponent } from './component/customer/mobile/mutual-funds/m
 
 
 @NgModule({
-  declarations: [CustomerComponent, TransactionSuccessfulComponent, MobileMyfeedComponent, MobileDocumentComponent, MobilePortfoiloComponent, MobileProfileComponent, MobileTransactionsComponent, MobileLeftSidenavComponent,UploadDocumentComponent,EditDocumentPopupComponent, MutualFundsComponent],
-  imports: [ 
+  declarations: [CustomerComponent, TransactionSuccessfulComponent, MobileMyfeedComponent,
+    MobileDocumentComponent, MobilePortfoiloComponent, MobileProfileComponent,
+    MobileTransactionsComponent, MobileLeftSidenavComponent, UploadDocumentComponent,
+    EditDocumentPopupComponent, MutualFundsComponent, MoveCopyMobileViewComponent],
+  imports: [
     // BrowserModule,
     CommonModule,
     MaterialModule,
@@ -56,8 +60,8 @@ import { MutualFundsComponent } from './component/customer/mobile/mutual-funds/m
     EntryComponentsModule,
     AccountEntryModule,
     PlanEntryModule,
-  CustomersRoutingModule,
-  // MobileRoutingModule,
+    CustomersRoutingModule,
+    // MobileRoutingModule,
     SlickCarouselModule,
     AdviceEntryModule,
     CustomerOverviewEntryModule,
@@ -72,7 +76,7 @@ import { MutualFundsComponent } from './component/customer/mobile/mutual-funds/m
   exports: [],
   providers: [DynamicComponentService, AdvisorAndOrganizationInfoService],
   entryComponents: [EntryComponentsModule.getComponentList(), AccountEntryModule.getComponentList(), AdviceEntryModule.getComponentList()
-    , PlanEntryModule.getComponentList(), CustomerOverviewEntryModule.getComponentList(),UploadDocumentComponent,EditDocumentPopupComponent]
+    , PlanEntryModule.getComponentList(), CustomerOverviewEntryModule.getComponentList(), UploadDocumentComponent, EditDocumentPopupComponent]
 })
 export class CustomersModule {
 }
