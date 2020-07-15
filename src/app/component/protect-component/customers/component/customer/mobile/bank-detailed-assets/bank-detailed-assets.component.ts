fc2216d2796a17370962464a44d5c73a78252a39
devshyni <devshyni@futurewise.co.in>
2020-07-15 18:15:29 +0530

    bulk-email

--- src/app/component/protect-component/customers/component/customer/mobile/bank-detailed-assets/bank-detailed-assets.component.ts
+++ src/app/component/protect-component/customers/component/customer/mobile/bank-detailed-assets/bank-detailed-assets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from 'src/app/auth-service/authService';
 import { EventService } from 'src/app/Data-service/event.service';
 import { UtilService } from 'src/app/services/util.service';
@@ -17,10 +17,18 @@ import { upperSliderAnimation, rightSliderAnimation } from 'src/app/animation/an
 export class BankDetailedAssetsComponent implements OnInit {
   fixdeposit
   backToMf
+  inputData: any;
   constructor(
     public eventService : EventService,
   ) { }
-
+  @Input()
+  set data(data) {
+    this.inputData = data;
+    console.log('This is Input data of proceed ', data);
+  }
+  get data() {
+    return this.inputData;
+  }
   ngOnInit() {
   }
   addFixedDeposit(){     
