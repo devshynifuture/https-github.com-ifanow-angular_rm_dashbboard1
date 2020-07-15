a7181b71cea8b835385a3a564cbd5fc31ede2019
devshyni <devshyni@futurewise.co.in>
2020-07-15 13:47:27 +0530

    bulk-email

---
+++ src/app/component/protect-component/customers/component/customer/mobile/fixed-income-mob/fixed-deposit-mob/fixed-deposit-mob.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-deposit-mob',
+  templateUrl: './fixed-deposit-mob.component.html',
+  styleUrls: ['./fixed-deposit-mob.component.scss'],
+  
+})
+export class FixedDepositMobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
