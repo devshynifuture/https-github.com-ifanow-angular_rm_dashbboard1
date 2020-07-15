89339e797a0082da4121e47a69fc14ed081b134b
Gayatri Pawar <gayatri@futurewise.co.in>
2020-07-15 21:30:20 +0530

    recurring deposite add done

--- src/app/component/protect-component/customers/customers.module.ts
+++ src/app/component/protect-component/customers/customers.module.ts
@@ -59,6 +59,7 @@ import { AddGoldMobComponent } from './component/customer/mobile/commodities-mob
 import { BankDetailedAssetsComponent } from './component/customer/mobile/bank-detailed-assets/bank-detailed-assets.component';
 import { FixedDepositMobComponent } from './component/customer/mobile/fixed-income-mob/fixed-deposit-mob/fixed-deposit-mob.component';
 import { BondMobComponent } from './component/customer/mobile/fixed-income-mob/bond-mob/bond-mob.component';
+import { RecurringDepositeMobComponent } from './component/customer/mobile/fixed-income-mob/recurring-deposite-mob/recurring-deposite-mob.component';
 
 
 // import { RightFilterComponent } from './component/common-component/right-filter/right-filter.component';
@@ -77,7 +78,7 @@ import { BondMobComponent } from './component/customer/mobile/fixed-income-mob/b
     AddEditDocumentMobileViewComponent, IndividualMemberFormComponent,
     MobileViewMoreInfoComponent,
     MinorMemberFormComponent, RetirementAccMobComponent, CashAndBankMobComponent, StocksMobComponent, RealEstateMobComponent, SmallSavingMobComponent, CommoditiesMobComponent, LiabilitiesMobComponent, LifeInsuranceMobComponent, GeneralInsuranceMobComponent,
-    AddGoldMobComponent, BankDetailedAssetsComponent, FixedDepositMobComponent, BondMobComponent],
+    AddGoldMobComponent, BankDetailedAssetsComponent, FixedDepositMobComponent, BondMobComponent, RecurringDepositeMobComponent],
   imports: [
     // BrowserModule,
     CommonModule,
