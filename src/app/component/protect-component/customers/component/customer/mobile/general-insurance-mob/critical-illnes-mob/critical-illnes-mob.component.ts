c7e37d6240cbc21869223077a7ab3fad01b1fc36
Gayatri Pawar <gayatri@futurewise.co.in>
2020-07-17 12:45:01 +0530

    add general insurance component

---
+++ src/app/component/protect-component/customers/component/customer/mobile/general-insurance-mob/critical-illnes-mob/critical-illnes-mob.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-critical-illnes-mob',
+  templateUrl: './critical-illnes-mob.component.html',
+  styleUrls: ['./critical-illnes-mob.component.scss']
+})
+export class CriticalIllnesMobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
