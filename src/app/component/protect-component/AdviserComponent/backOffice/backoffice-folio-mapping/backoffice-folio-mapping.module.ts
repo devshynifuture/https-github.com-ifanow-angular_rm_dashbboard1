46f9de2fbb675d9c51ba73654cf05b87d34b4504
ani <aniruddha@futurewise.co.in>
2020-08-03 20:14:09 +0530

    ui changes

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-folio-mapping/backoffice-folio-mapping.module.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-folio-mapping/backoffice-folio-mapping.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackofficeFolioMappingComponent } from './backoffice-folio-mapping.component';
 import { MaterialModule } from 'src/app/material/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [BackofficeFolioMappingComponent],
   imports: [
-    CommonModule,
-    BackofficeFolioMappingRoutingModule,MaterialModule
+    CommonModule,MaterialModule,FormsModule, ReactiveFormsModule,
+    BackofficeFolioMappingRoutingModule
   ]
 })
 export class BackofficeFolioMappingModule { }
