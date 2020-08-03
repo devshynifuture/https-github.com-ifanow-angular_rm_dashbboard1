cea746ac2b43a7c291487b1cfc6ffe74c22624fc
ani <aniruddha@futurewise.co.in>
2020-08-03 18:25:17 +0530

    ui

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-folio-mapping/backoffice-folio-mapping.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-folio-mapping/backoffice-folio-mapping.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backoffice-folio-mapping.component.scss']
 })
 export class BackofficeFolioMappingComponent implements OnInit {
+  displayedColumns: string[] = ['checkBoxIcon', 'schemeName', 'number', 'investName'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+
 }
+
+
+export interface PeriodicElement {
+  checkBoxIcon: string;
+  schemeName: string;
+  number: string;
+  investName: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { checkBoxIcon: '', schemeName: 'ICICI Prudential Long Term Equity Fund (Tax Saving) - Growth', number: 'K126860', investName: 'Rahul jain'  },
+  {checkBoxIcon: '', schemeName: 'Tata Hybrid Equity Fund Regular Plan- Monthly Dividend Option ', number: 'K126860', investName: 'Devshyani Jadhav'  },
+  { checkBoxIcon: '', schemeName: 'ICICI Prudential Long Term Equity Fund (Tax Saving) - Growth', number: 'K126860', investName: 'Rahul jain'  },
+  {checkBoxIcon: '', schemeName: 'Tata Hybrid Equity Fund Regular Plan- Monthly Dividend Option ', number: 'K126860', investName: 'Devshyani Jadhav'  },
+  { checkBoxIcon: '', schemeName: 'ICICI Prudential Long Term Equity Fund (Tax Saving) - Growth', number: 'K126860', investName: 'Rahul jain'  },
+  {checkBoxIcon: '', schemeName: 'Tata Hybrid Equity Fund Regular Plan- Monthly Dividend Option ', number: 'K126860', investName: 'Devshyani Jadhav'  },
+  { checkBoxIcon: '', schemeName: 'ICICI Prudential Long Term Equity Fund (Tax Saving) - Growth', number: 'K126860', investName: 'Rahul jain'  },
+  {checkBoxIcon: '', schemeName: 'Tata Hybrid Equity Fund Regular Plan- Monthly Dividend Option ', number: 'K126860', investName: 'Devshyani Jadhav'  },
+   
+];
