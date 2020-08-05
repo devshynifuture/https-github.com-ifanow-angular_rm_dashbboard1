534f668afa0663215272b05c20f4a86d87314dfd
devshyni <devshyni@futurewise.co.in>
2020-08-05 19:57:10 +0530

    upload in mobile app

--- src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
@@ -897,10 +897,10 @@ export class MutualFundSummaryComponent implements OnInit {
           'text- align': 'right',
           'font- size': '13px',
           'padding': '8px',
-          'border - bottom': '1px solid #dee5e7',
-          'border - top': '1px solid #dee5e7',
-          'border - right': '1px solid #dee5e7',
-          'border - left': '1px solid #dee5e7'
+          'border-bottom': '1px solid #dee5e7',
+          'border-top': '1px solid #dee5e7',
+          'border-right': '1px solid #dee5e7',
+          'border-left': '1px solid #dee5e7'
         }
       });
       this.customDataSource.data.array1.push({
