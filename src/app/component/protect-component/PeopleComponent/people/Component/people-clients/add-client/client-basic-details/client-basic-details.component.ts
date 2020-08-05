b00076c251c066e111e7ed583bfc11954616b873
sarvesh <sarvesh@futurewise.co.in>
2020-08-05 21:08:55 +0530

    Family member changes

--- src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-basic-details/client-basic-details.component.ts
+++ src/app/component/protect-component/PeopleComponent/people/Component/people-clients/add-client/client-basic-details/client-basic-details.component.ts
@@ -130,7 +130,7 @@ export class ClientBasicDetailsComponent implements OnInit {
       }
       else {
         this.relationList = relationListFilterOnID(AuthService.getClientData().clientType);
-        if (this.basicDetailsData.age > 18) {
+        if (this.basicDetailsData.age > 18 || this.basicDetailsData.familyMemberType == 1) {
           this.familyMemberType = { name: 'Individual', value: '1' };
           this.invTypeCategory = '1';
           this.hideDematTab.emit(true);
