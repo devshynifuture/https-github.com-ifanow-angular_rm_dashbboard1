85c660b6a7109af9c44fa42421be0140dce7a3d0
Gayatri Pawar <gayatri@futurewise.co.in>
2020-08-06 17:08:24 +0530

    bal unit

--- src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
@@ -1179,7 +1179,7 @@ export class MutualFundSummaryComponent implements OnInit {
         this.summary.data = [{}, {}, {}];
         this.summary.data = data.customDataSourceData;
         this.mfData.withdrawals = this.grandTotal.withdrawals
-        this.mfData.totalBalanceUnit = this.grandTotal.totalBalanceUnit
+        this.mfData.totalBalanceUnit = this.mfService.mutualFundRoundAndFormat(this.grandTotal.totalBalanceUnit,3)
         this.mfData.sip = this.grandTotal.sip
         this.mfData.total_absolute_return=this.mfService.mutualFundRoundAndFormat(this.mfData.total_absolute_return,2);
         this.mfData.total_xirr=this.mfService.mutualFundRoundAndFormat(this.mfData.total_xirr,2)
