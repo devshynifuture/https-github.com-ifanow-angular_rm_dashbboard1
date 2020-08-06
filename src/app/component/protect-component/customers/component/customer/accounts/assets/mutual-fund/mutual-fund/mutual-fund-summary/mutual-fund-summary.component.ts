f0e20258743f603495bac802cdeea350ec383ccc
devshyni <devshyni@futurewise.co.in>
2020-08-06 19:24:15 +0530

    upload in mobile app

--- src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mutual-fund-summary/mutual-fund-summary.component.ts
@@ -849,7 +849,7 @@ export class MutualFundSummaryComponent implements OnInit {
       this.customDataSource.data.array.push({
         'name': 'NAV date', 'index': ind, isCheked: true,
         style: {
-          'width': '9%',
+          'width': '7%',
           'text-align': 'right',
           'font-size': ' 13px',
           'padding': '8px',
@@ -861,7 +861,7 @@ export class MutualFundSummaryComponent implements OnInit {
       this.customDataSource.data.array1.push({
         'index': ind,
         style: {
-          'width': '9%',
+          'width': '7%',
           'font-size': '13px',
           'padding': '8px',
           'border-right': '1px solid #dee5e7',
@@ -875,7 +875,7 @@ export class MutualFundSummaryComponent implements OnInit {
         'index': ind,
         style: {
           'text-align': 'right',
-          'width': '9%',
+          'width': '7%',
           'font-size': '13px',
           'padding': '8px',
           'font-weight': '600',
@@ -888,7 +888,7 @@ export class MutualFundSummaryComponent implements OnInit {
         'index': ind,
         style: {
           'text-align': 'right',
-          'width': '9%',
+          'width': '7%',
           'font-size': '13px',
           'padding': '8px',
           'font-weight': '600',
@@ -1546,7 +1546,7 @@ export class MutualFundSummaryComponent implements OnInit {
         var type = typeof element.navDate == "boolean" ? element.navDate : false;
         console.log('type', type)
         if(type == false){
-          element.navDate = element.nav + ' | '+element.navDate
+          element.navDate = element.nav +  +'\xa0\xa0\xa0\xa0\xa0\xa0\xa0'+' | '+''+element.navDate
         }
       }
     });
@@ -1849,14 +1849,13 @@ export class MutualFundSummaryComponent implements OnInit {
         var type = typeof element.navDate == "boolean" ? element.navDate : false;
         console.log('type', type)
         if(type == false){
-          element.navDate = element.nav + ' | '+element.navDate
+          element.navDate = element.nav +  +'\xa0\xa0\xa0\xa0\xa0\xa0\xa0'+' | '+''+element.navDate
         }
       }
     });
     this.displayedColumns.forEach((element, ind) => {
       this.styleObject(element, ind)
     });
-
     this.showDownload = true
     this.fragmentData.isSpinner = true;
     const date = this.datePipe.transform(new Date(), 'dd-MMM-yyyy');
