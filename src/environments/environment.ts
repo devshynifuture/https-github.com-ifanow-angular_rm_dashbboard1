9f99663d820be465bd672026092506dfa4b0dffd
Gaurav <gaurav@futurewise.co.in>
2020-08-10 18:24:26 +0530

    Hard coded base url removed GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,13 +11,13 @@
 
 export const environment = {
   production: false,
-  APIEndpoint: "https://dev.ifanow.in:8080/futurewise/api/v1/web/",
-  GmailAPIEndpoint: "https://dev.ifanow.in:8080/email-server/api/v1/web/",
+  APIEndpoint: 'https://dev.ifanow.in:8080/futurewise/api/v1/web/',
+  GmailAPIEndpoint: 'https://dev.ifanow.in:8080/email-server/api/v1/web/',
   // TransactionPlatform: 'http://localhost:8080/transaction-platform/api/v1/web/',
   TransactionPlatform:
-    "https://dev.ifanow.in:8080/transaction-platform/api/v1/web/",
-  PostalUrl: "https://api.postalpincode.in/pincode/",
-  UserUrl: "https://dev.ifanow.in:8080/userserver/api/v1/web/",
-  MarketPlaceUrl: "https://dev.ifanow.in:8080/marketplace-server/api/v1/web/",
+    'https://dev.ifanow.in:8080/transaction-platform/api/v1/web/',
+  PostalUrl: 'https://api.postalpincode.in/pincode/',
+  UserUrl: 'https://dev.ifanow.in:8080/userserver/api/v1/web/',
+  MarketPlaceUrl: 'https://dev.ifanow.in:8080/marketplace-server/api/v1/web/',
   hmr: false,
 };
