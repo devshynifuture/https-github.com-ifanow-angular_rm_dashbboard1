869884b5aca6f5e8e847de99538974ae2a01ff0b
Gayatri Pawar <gayatri@futurewise.co.in>
2020-08-11 15:40:32 +0530

    backoffice issue

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/folio-query/folio-query.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/folio-query/folio-query.component.ts
@@ -202,7 +202,7 @@ export class FolioQueryComponent implements OnInit {
           let arrValue = [];
           res.forEach(element => {
             arrValue.push({
-              arnRiaCode: element.broker_id ? element.broker_id : '-',
+              arnRiaCode: element.arnRiaCode ? element.arnRiaCode : '-',
               schemeName: element.shemeName,
               investorName: element.investorName,
               folioNumber: element.folioNumber,
