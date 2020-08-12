29353f97ea5f41b746665918f36432f6d42dead7
Gayatri Pawar <gayatri@futurewise.co.in>
2020-08-12 11:40:09 +0530

    orting in asset Allocation

--- src/app/component/protect-component/customers/component/customer/customer-overview/overview-myfeed/overview-myfeed.component.ts
+++ src/app/component/protect-component/customers/component/customer/customer-overview/overview-myfeed/overview-myfeed.component.ts
@@ -567,7 +567,19 @@ export class OverviewMyfeedComponent implements OnInit, AfterViewInit, OnDestroy
           }
         });
         if(chartData){
-          chartData = this.sorting(chartData,'name')
+          let index;
+          let obj={};
+          chartData = this.sorting(chartData,'name');
+          chartData.forEach((element,ind) => {
+              if(element.name == 'Others'){
+                index = ind
+              }
+          });
+          if(index){
+             obj = chartData.splice(index, 1);
+            let outputObj=obj[0]
+            chartData.push(outputObj)
+          }
         }
         chartTotal -= 1;
         if (chartTotal === 0) {
