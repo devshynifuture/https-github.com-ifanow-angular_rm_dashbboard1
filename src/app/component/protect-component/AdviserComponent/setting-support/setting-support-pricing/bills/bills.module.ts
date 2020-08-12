7ebb0bf743a8f16eaa1cb6b321bcaf3e9e145324
sunny <sunny@futurewise.co.in>
2020-08-12 21:09:18 +0530

    billing new screen settings

--- src/app/component/protect-component/AdviserComponent/setting-support/setting-support-pricing/bills/bills.module.ts
+++ src/app/component/protect-component/AdviserComponent/setting-support/setting-support-pricing/bills/bills.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { EstimateComponent } from "./estimate/estimate.component";
 import { BillsPaymentsComponent } from "./bills-payments/bills-payments.component";
+import { MaterialModule } from 'src/app/material/material';
 
 @NgModule({
   declarations: [BillsComponent, EstimateComponent, BillsPaymentsComponent],
-  imports: [CommonModule, BillsRoutingModule],
+  imports: [CommonModule, BillsRoutingModule, MaterialModule],
 })
-export class BillsModule {}
+export class BillsModule { }
--- src/app/component/protect-component/AdviserComponent/setting-support/setting-support-pricing/refer-earn/refer-earn.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting-support/setting-support-pricing/refer-earn/refer-earn.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./refer-earn.component.scss']
 })
 export class ReferEarnComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 'Amitesh Anand', name: 'Trial user', weight: '₹0' },
+  { position: 'Rajan Prakash', name: 'Successfully signed up', weight: '₹2,0000' },
+  { position: 'Sunil Kumar', name: 'Successfully signed up', weight: '₹2,000' },
+  { position: 'Arnav Pandit', name: 'Successfully signed up', weight: '₹2,000' },
+
+];
+
