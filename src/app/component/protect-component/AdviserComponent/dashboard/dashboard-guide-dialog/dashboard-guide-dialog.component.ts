be59f9afac664280dd5dae15107169155d1fbf2c
ani <aniruddha@futurewise.co.in>
2020-08-14 12:13:49 +0530

    add tab view

--- src/app/component/protect-component/AdviserComponent/dashboard/dashboard-guide-dialog/dashboard-guide-dialog.component.ts
+++ src/app/component/protect-component/AdviserComponent/dashboard/dashboard-guide-dialog/dashboard-guide-dialog.component.ts
@@ -146,7 +146,7 @@ export class DashboardGuideDialogComponent implements OnInit {
   ngOnInit() {
     this.validatorType = ValidatorType
     this.advisorId = AuthService.getAdvisorId();
-    this.step = 1;
+    this.step = 14;
     this.ArnRiaForm = this.fb.group({
       ArnRiaFormList: new FormArray([])
     })
