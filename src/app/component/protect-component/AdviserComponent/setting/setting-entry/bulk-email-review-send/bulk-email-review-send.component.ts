23739a3d4357a89f2ea68c52b59315eeccbc9146
sarvesh <sarvesh@futurewise.co.in>
2020-08-25 13:13:54 +0530

    bulk email changes

--- src/app/component/protect-component/AdviserComponent/setting/setting-entry/bulk-email-review-send/bulk-email-review-send.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-entry/bulk-email-review-send/bulk-email-review-send.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/auth-service/authService';
 import { EventService } from 'src/app/Data-service/event.service';
 import { EnumDataService } from 'src/app/services/enum-data.service';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-bulk-email-review-send',
@@ -13,6 +14,9 @@ export class BulkEmailReviewSendComponent implements OnInit {
   clientList: any;
   dataSource = new MatTableDataSource();
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  displayedColumns: string[] = ['checkbox', 'name', 'email'];
+  isLoading = false
+  dataCount: number;
 
   constructor(
     public authService: AuthService,
@@ -27,13 +31,39 @@ export class BulkEmailReviewSendComponent implements OnInit {
   }
 
   @Input() set data(data) {
-    this.dataSource.data = this.enumDataService.getEmptySearchStateData()
+    if (data && data.length > 0) {
+      data.forEach(element => {
+        element.selected = false
+      });
+      this.dataSource.data = data
+    }
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
     this.dataSource.sort = this.sort;
+  }
+
+  selectAll(event) {
+    this.dataCount = 0;
+    if (this.dataSource != undefined) {
+      this.dataSource.filteredData.forEach(element => {
+        element['selected'] = event.checked;
+        if (element['selected']) {
+          this.dataCount++;
+        }
+      });
+    }
+  }
+
+  changeSelect() {
+    this.dataCount = 0;
+    this.dataSource.filteredData.forEach(item => {
+      if (item['selected']) {
+        this.dataCount++;
+      }
+    });
   }
 
   close() {
