641ff4b172cd34e326b403f62e114e27acace28f
devshyni <devshyni@futurewise.co.in>
2020-09-10 12:49:16 +0530

    multi year goal

--- src/app/component/protect-component/customers/component/customer/plan/goals-plan/goals-plan.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/goals-plan/goals-plan.component.ts
@@ -32,7 +32,7 @@ import { element } from 'protractor';
   providers: [LoaderFunction]
 })
 export class GoalsPlanComponent implements OnInit, OnDestroy {
-  displayedColumns = ['goalYear', 'goalFv', 'goalFvAllocated', 'status'];
+  displayedColumns = ['goalYear', 'goalFv', 'goalFvAllocated'];
   clientFamily: any[];
   data: Array<any> = [{}, {}, {}];
   dataSource = new MatTableDataSource(this.data);
