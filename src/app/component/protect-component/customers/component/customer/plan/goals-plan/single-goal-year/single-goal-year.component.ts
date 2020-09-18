4bf765d431aff4e4db6a100d446030b236ce5e72
devshyni <devshyni@futurewise.co.in>
2020-09-18 17:54:53 +0530

    multi year goal

--- src/app/component/protect-component/customers/component/customer/plan/goals-plan/single-goal-year/single-goal-year.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/goals-plan/single-goal-year/single-goal-year.component.ts
@@ -145,7 +145,9 @@ export class SingleGoalYearComponent implements OnInit {
         obj['monthlyExpense'] = this.singleYearGoalForm.get('cost').value;
         obj['goalAdditionDate'] = this.datePipe.transform(new Date, 'yyyy-MM-dd')
          this.singleYearGoalForm.value.getMilestoneName.forEach(element => {
-          obj['milestoneModels'].push(element)
+           if(element.onRetirementOrDemise !=""){
+            obj['milestoneModels'].push(element)
+           }
         });
         break;
       case AppConstants.CAR_GOAL: // Car
