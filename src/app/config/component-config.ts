247145d49eec8eb79f0427492975db56c05eceb2
Gayatri Pawar <gayatri@futurewise.co.in>
2020-10-04 19:42:00 +0530

    issues in insurance planning

--- src/app/config/component-config.ts
+++ src/app/config/component-config.ts
@@ -324,7 +324,7 @@ export const appConfig = {
   GET_FAMILYMEMBER_DETAILS: 'insurance/planning/family-member/info/get',
   GET_LIFE_NEED_ANALYSIS: 'insurance/planning/life/insurance/need/analysis/get',
   ADD_LIFE_NEED_ANALYSIS_MAP: 'insurance/planning/life/insurance/need/analysis/map/to/plan',
-  REMOVE_LIFE_NEED_ANALYSIS_MAP: 'insurance/planning/insurance/delete',
+  REMOVE_LIFE_NEED_ANALYSIS_MAP: 'insurance/planning/insurance/delete?',
   LIFE_ANALYSIS_ADD: 'insurance/planning/life/insurance/need/analysis/save',
   GET_DETAILED_INSURANCE: 'insurance/planning/life/insurance/need/analysis/mapped/to/plan/get',
   DELETE_INSURANCE_INPLANNING: 'insurance/planning/insurance/delete?',
