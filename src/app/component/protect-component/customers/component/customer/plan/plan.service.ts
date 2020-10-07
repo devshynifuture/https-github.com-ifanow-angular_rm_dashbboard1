6bf726553bf40a08729161b15148b06b8b96cb4e
Gayatri Pawar <gayatri@futurewise.co.in>
2020-10-07 14:02:56 +0530

    plan service changes

--- src/app/component/protect-component/customers/component/customer/plan/plan.service.ts
+++ src/app/component/protect-component/customers/component/customer/plan/plan.service.ts
@@ -253,7 +253,7 @@ export class PlanService {
     return this.http.get(apiConfig.MAIN_URL + appConfig.EXPENSE_ALL_GET, data)
   }
   deleteInsurancePlanning(data) {
-    return this.http.put(apiConfig.MAIN_URL + appConfig.DELETE_INSURANCE_INPLANNING,data)
+    return this.http.put(apiConfig.MAIN_URL + appConfig.DELETE_INSURANCE_INPLANNING+ '?clientId=' + data.clientId + '&familyMemberId=' + data.familyMemberId + '&id=' + data.id + '&insuranceSubTypeId=' + data.insuranceSubTypeId+ '&insuranceTypeId=' + data.insuranceTypeId,'')
   }
   getInsuranceRecommendation(data) {
     return this.http.get(apiConfig.MAIN_URL + appConfig.RECOMMENDATION_LIST_GET+ 'id=' + data, '')
