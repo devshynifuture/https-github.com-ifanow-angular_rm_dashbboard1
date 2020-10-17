ff46dec101678eb94e85941cbae0ffa78db1e7ab
devshyni <devshyni@futurewise.co.in>
2020-10-17 12:57:19 +0530

    bulk button

--- src/app/component/protect-component/customers/component/customer/plan/goals-plan/goals-plan.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/goals-plan/goals-plan.component.ts
@@ -126,6 +126,7 @@ export class GoalsPlanComponent implements OnInit, OnDestroy {
   assetError = false;
   selectedGoalId: any = null;
   subscriber = new Subscriber();
+  highlight: boolean = false;
 
   constructor(
     private subInjectService: SubscriptionInject,
@@ -265,6 +266,7 @@ export class GoalsPlanComponent implements OnInit, OnDestroy {
   }
 
   afterDataLoadMethod() {
+    this.highlight = false
     this.allGoals = this.allGoals.reverse().map(goal => this.mapGoalDashboardData(goal));
     console.log('allGoals',this.allGoals)
     this.allGoals.map(element => {
@@ -604,6 +606,7 @@ export class GoalsPlanComponent implements OnInit, OnDestroy {
   }
   // drag drop for assets brought from allocations tab
   drop(event: CdkDragDrop<string[]>) {
+    this.highlight = false
     if (event.previousContainer === event.container || !event.isPointerOverContainer) {
       return;
     }
@@ -710,6 +713,7 @@ export class GoalsPlanComponent implements OnInit, OnDestroy {
   }
 
   inDropZoneMethod(e) {
+    this.highlight = true
     console.log(e, 'dz method')
   }
 
