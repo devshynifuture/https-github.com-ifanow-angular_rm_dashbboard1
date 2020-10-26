ded2c3de99bcd3036cfb8d23e3bab803dd532c60
Gaurav <gaurav@futurewise.co.in>
2020-10-26 11:19:50 +0530

    prod issues gd commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,7 +1,12 @@
 const webpack = require('webpack');
 
 module.exports = {
-    plugins: [
-        // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    ]
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ]
 }
