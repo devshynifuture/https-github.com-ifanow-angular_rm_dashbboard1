c5e752ffd8cb65eee761c3417e95149659f12a05
Gayatri Pawar <gayatri@futurewise.co.in>
2020-10-27 23:35:04 +0530

    sumary plan dynamic

--- src/app/component/protect-component/customers/component/customer/plan/summary-plan/summary-plan.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/summary-plan/summary-plan.component.ts
@@ -6,6 +6,8 @@ import { PlanService } from './../plan.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PeopleService } from 'src/app/component/protect-component/PeopleComponent/people.service';
 import { DatePipe } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { ConstantsService } from 'src/app/constants/constants.service';
 
 @Component({
   selector: 'app-summary-plan',
@@ -27,11 +29,13 @@ export class SummaryPlanComponent implements OnInit {
   endDate: any;
   cashFlowData: any;
   yearToShow: string;
+  budgetData = [];
   constructor(
     private planService: PlanService,
     private eventService: EventService,
     private peopleService:PeopleService,
-    private datePipe: DatePipe
+    private datePipe: DatePipe,
+    private constantService: ConstantsService
   ) { }
 
   @ViewChild(MatPaginator, { static: false }) paginator;
@@ -45,6 +49,128 @@ export class SummaryPlanComponent implements OnInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+  getBudgetApis() {
+    const obj2 = {
+      advisorId: this.advisorId,
+      clientId: this.clientId,
+      allOrSingle: 1,
+      endDate: this.endDate,
+      startDate: this.startDate,
+      limit: 10,
+      offset: 1,
+      familyMemberId: 0,
+      clientDob: this.clientDob,
+      fmDobList: JSON.stringify(this.familyList)
+    };
+    const obj3 = {
+      advisorId: this.advisorId,
+      clientId: this.clientId,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      clientDob: this.clientDob,
+      fmDobList: JSON.stringify(this.familyList)
+    };
+    const BudgetRecurring = this.planService.otherCommitmentsGet(obj2);
+    const recurringAssets = this.planService.getAllExpense(obj2);
+    forkJoin(BudgetRecurring,recurringAssets).subscribe(result => {
+      let budgetRecurring = this.filterData(result[0]);
+      let BudgetCommitments = this.getAssetData(result[1]);
+      let mergeArray = [...BudgetCommitments,...budgetRecurring];
+      mergeArray = this.sorting(mergeArray,'expenseType');
+      console.log(mergeArray);
+      this.budgetData = mergeArray;
+    }, err => {
+      this.eventService.openSnackBar(err, 'Dismiss');
+     
+    })
+  }
+  sorting(data, filterId) {
+    if (data) {
+      data.sort((a, b) =>
+        a[filterId] > b[filterId] ? 1 : (a[filterId] === b[filterId] ? 0 : -1)
+      );
+    }
+    return data
+  }
+  getAssetData(data) {
+    let finalArray = [];
+    if (data) {
+      let committedInvestmentExpense = this.filterAssetData(data.committedInvestmentExpense);
+      let committedExpenditureExpense = this.filterAssetData(data.committedExpenditureExpense);
+      let pord = this.filterAssetData(data.pordAsset);
+      let lifeInsuranceList = this.filterAssetData(data.lifeInsuranceList);
+      let generalInsuranceExpense = this.filterAssetData(data.generalInsurancePremium);
+      let ssy = this.filterAssetData(data.ssyAsset);
+      let loanExpense = this.filterAssetData(data.loanEmi);
+      let recurringDeposit = this.filterAssetData(data.rdAsset);
+      let sipExpense = this.filterAssetData(data.mutualFundSipList);
+      finalArray = [...committedInvestmentExpense, ...committedExpenditureExpense, ...pord, ...lifeInsuranceList, ...generalInsuranceExpense, ...ssy, ...loanExpense, ...recurringDeposit, ...sipExpense];
+
+      console.log(finalArray)
+
+
+
+    }else{
+      finalArray =[];
+    }
+    return finalArray
+  }
+  filterAssetData(data) {
+    let obj;
+    let filterArray = []
+    if (data) {
+      if(data[1].name == 'Pord'){
+        data[1].name = 'Post office recurring deposits'
+        data[0].assetList = data[0].pordList;
+      }else if(data[1].name == 'Recurring deposits'){
+        data[1].name = 'Bank recurring deposits'
+      }else if(data[1].name == 'Sukanya samriddhi yojna'){
+        data[0].assetList = data[0].ssyList;
+      }
+      obj = {
+        expenseType: data[1].name, total: data[2].total,amount: data[2].total,spent: data[2].total, assetList: data[0].assetList, progressPercent: 0, spentPerOther: 0, budgetPerOther: 0
+      }
+      obj.progressPercent = 0;
+      obj.progressPercent += (data[2].total / data[2].total) * 100;
+      obj.progressPercent = Math.round(obj.progressPercent);
+      if (obj.progressPercent > 100) {
+        obj.spentPerOther = 100;
+        obj.budgetPerOther = obj.progressPercent - 100;
+      } else {
+        obj.spentPerOther = obj.progressPercent;
+      }
+    }
+    if (obj) {
+      // filterArray.push({name:data[1].name},{total:data[2].total},{});
+      filterArray.push(obj);
+
+    }
+    return filterArray
+  }
+  filterData(array) {
+    if (array) {
+      array = array.filter(item => item.totalAmount > 0);
+      array.forEach(singleExpense => {
+        singleExpense.progressPercent = 0;
+        singleExpense.progressPercent += (singleExpense.spent / singleExpense.amount) * 100;
+        singleExpense.progressPercent = Math.round(singleExpense.progressPercent);
+        if (singleExpense.progressPercent > 100) {
+          singleExpense.spentPer = 100;
+          singleExpense.budgetPer = singleExpense.progressPercent - 100;
+        } else {
+          singleExpense.spentPer = singleExpense.progressPercent;
+        }
+        const singleExpenseCategory = this.constantService.expenseJsonMap[singleExpense.budgetCategoryId];
+        if (singleExpenseCategory) {
+          singleExpense.expenseType = singleExpenseCategory.expenseType;
+        }
+      });
+    } else {
+      array = [];
+    }
+
+    return array;
   }
   getStartAndEndDate(){
     var date = new Date();
@@ -217,12 +343,13 @@ export class SummaryPlanComponent implements OnInit {
             delete obj['id'];
             return obj;
           });
+         
+        } 
           this.getCashflowData();
-        } else {
-          this.getCashflowData();
-        }
+          this.getBudgetApis();
       },err=>{
         this.getCashflowData();
+        this.getBudgetApis();
       }
     );
   }
