d282ae2be7fb13871da8d9e22e55c84155d150ae
devshyni <devshyni@futurewise.co.in>
2020-10-30 17:40:37 +0530

    edit doc

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'https://futurewise.my-planner.in/futurewise/api/v1/web/',
+  APIEndpoint: 'https://dev.ifanow.in/futurewise/api/v1/web/',
   GmailAPIEndpoint: 'https://emailserver.my-planner.in/email-server/api/v1/web/',
   TransactionPlatform: 'https://transactplatform.my-planner.in/transaction-platform/api/v1/web/',
   PostalUrl: 'https://api.postalpincode.in/pincode/',
