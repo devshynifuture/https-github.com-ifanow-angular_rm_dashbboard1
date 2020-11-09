e8cd44f24f432d1419629284a4082b80637f5480
ng-khizar <khizaruddin@futurewise.co.in>
2020-11-09 18:55:46 +0530

    changes

---
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/mutual-fund/mutual-fund/mf-import-cas-file/mf-import-cas-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mf-import-cas-file',
+  templateUrl: './mf-import-cas-file.component.html',
+  styleUrls: ['./mf-import-cas-file.component.scss']
+})
+export class MfImportCasFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
