5d11d83cfd37f949a99a7eb2c7f37274e0346c84
devshyni <devshyni@futuerwise.co.in>
2020-11-18 14:45:58 +0530

    pdf genration

--- src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth-service/authService';
 
 @Component({
   selector: 'app-finacial-plan-section',
@@ -11,5 +12,20 @@ export class FinacialPlanSectionComponent implements OnInit {
 
   ngOnInit() {
   }
+  download() {
+    let list = [{ url: 'pdf/summary', id: 1 }, { url: 'pdf/allTransactions', id: 2 }, { url: 'pdf/unrealisedTransactions', id: 3 },]
+    list.forEach(element => {
+      if (element.id == 1) {
+        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
+        window.open(element.url)
+      } else if (element.id == 2) {
+        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18' + '&' + 'fromDate=2019%2F11%2F18'
+        window.open(element.url)
+      } else if (element.id == 3) {
+        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
+        window.open(element.url)
+      }
+    });
 
+  }
 }
