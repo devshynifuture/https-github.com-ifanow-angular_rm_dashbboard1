cd14c588ebd42b54f6ba3ce8d0863d723c7464ad
devshyni <devshyni@futuerwise.co.in>
2020-11-19 12:15:15 +0530

    drag remove module add fin plan

--- src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
@@ -59,19 +59,19 @@ export class FinacialPlanSectionComponent implements OnInit {
     this.moduleAdded.splice(i, 1);
   }
   download() {
-    let list = [{ url: 'pdf/summary', id: 1 }, { url: 'pdf/allTransactions', id: 2 }, { url: 'pdf/unrealisedTransactions', id: 3 },]
-    list.forEach(element => {
-      if (element.id == 1) {
-        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
-        window.open(element.url)
-      } else if (element.id == 2) {
-        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18' + '&' + 'fromDate=2019%2F11%2F18'
-        window.open(element.url)
-      } else if (element.id == 3) {
-        element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
-        window.open(element.url)
-      }
-    });
+    // let list = [{ url: 'pdf/summary', id: 1 }, { url: 'pdf/allTransactions', id: 2 }, { url: 'pdf/unrealisedTransactions', id: 3 },]
+    // list.forEach(element => {
+    //   if (element.id == 1) {
+    //     element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
+    //     window.open(element.url)
+    //   } else if (element.id == 2) {
+    //     element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18' + '&' + 'fromDate=2019%2F11%2F18'
+    //     window.open(element.url)
+    //   } else if (element.id == 3) {
+    //     element.url = 'http://localhost:4200/' + element.url + '?' + 'advisorId=' + AuthService.getAdvisorId() + '&' + 'clientId=' + AuthService.getClientId() + '&' + 'parentId=0' + '&' + 'toDate=2020%2F11%2F18'
+    //     window.open(element.url)
+    //   }
+    // });
 
   }
   checkAndLoadPdf(value: any, sectionName: any) {
