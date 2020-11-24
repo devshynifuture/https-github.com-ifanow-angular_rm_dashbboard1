404029e6f435e21bf934c06f36729dd8b2f82051
devshyni <devshyni@futuerwise.co.in>
2020-11-24 15:10:57 +0530

    switch and redemption

--- src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
+++ src/app/component/protect-component/customers/component/customer/plan/profile-plan/finacial-plan-section/finacial-plan-section.component.ts
@@ -148,16 +148,19 @@ export class FinacialPlanSectionComponent implements OnInit {
     this.planService.mergeCall(this.moduleAdded).subscribe(
       data => this.mergeCallRes(data)
     );
-    setTimeout(() => {
-      this.getPDFCall()
-    }, 500);
+    this.getPDFCall()
   }
   mergeCallRes(data) {
 
   }
+  pdfRes(data) {
+    setTimeout(() => {
+      this.getPDFCall()
+    }, 500);
+  }
   getPDFCall() {
     this.planService.getPDFCall().subscribe(
-      data => this.mergeCallRes(data)
+      data => this.pdfRes(data)
     );
   }
   checkAndLoadPdf(value: any, sectionName: any, obj: any, displayName: any) {
