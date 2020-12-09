7692197d4bf3865397464858ee481ad7f146a92f
ng-khizar <khizaruddin@futurewise.co.in>
2020-12-09 21:09:41 +0530

    changes

--- src/app/component/protect-component/SupportComponent/support-dashboard/support-upper/support-upper-nj/support-upper-nj.component.ts
+++ src/app/component/protect-component/SupportComponent/support-dashboard/support-upper/support-upper-nj/support-upper-nj.component.ts
@@ -1,3 +1,4 @@
+import { Subscription } from 'rxjs';
 import { EventService } from './../../../../../../Data-service/event.service';
 import { UtilService } from './../../../../../../services/util.service';
 import { SupportUpperService } from './../support-upper.service';
@@ -51,13 +52,20 @@ export class SupportUpperNjComponent implements OnInit {
   selectedElement: any;
   apiCallingStack: any[] = [];
   searchSchemes: any[];
+  mapUnmappedOptionFC = new FormControl(1);
   njOptionsDropdown = [
     { id: 1, title: 'Unmapped' },
     { id: 2, title: 'Mapped' }
   ];
 
+  startLimit = 0;
+
+  isMapped = false;
   pageEvent: PageEvent;
   totalNjCount: number = 0;
+  selectedSchemeRes;
+  schemeControlSubs: Subscription;
+  searchSchemeControlSubs: Subscription;
 
   constructor(
     private supportUpperService: SupportUpperService,
@@ -74,8 +82,61 @@ export class SupportUpperNjComponent implements OnInit {
     // console.log(this.schemeFormControl);
     this.dataSource = new MatTableDataSource(ELEMENT_DATA);
     this.isLoading = true;
-    this.getUnmappedNjSchemes();
-    this.schemeControl.valueChanges
+    this.getMappedUnmappedNjSchemes();
+    this.searchSchemeControlSubs = this.searchSchemeControl.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredSchemes = [];
+          this.isLoadingForDropDown = true;
+          this.schemeControl.patchValue(null, { emitEvent: false });
+        }),
+        switchMap(value => this.searchSchemeNjPrudent(value)
+          .pipe(
+            finalize(() => {
+              this.isLoadingForDropDown = false
+            }),
+          )
+        )
+      ).subscribe(data => {
+        this.isLoading = false;
+        this.isLoadingForDropDown = false;
+        if (data) {
+          let dataTable: any[] = [];
+          this.apiCallingStack = [];
+          data.forEach(item => {
+            dataTable.push({
+              name: item.schemeName,
+              nav: item.nav,
+              schemeName: '',
+              schemeCode: '',
+              amficode: '',
+              navTwo: '',
+              navDate: '',
+              njCount: '',
+              map: '',
+              id: item.id,
+              transactionDate: item.transactionDate
+            });
+          });
+          console.log("this is some data::::::", dataTable);
+          this.dataTable = dataTable;
+          this.dataSource.data = dataTable;
+          console.log(data);
+          this.apiCallingStack = [];
+          this.filteredSchemes = data;
+          console.log("this is what i need::::::::", data);
+          this.checkIfDataNotPresentAndShowError(data);
+          console.log(this.filteredSchemes);
+        }
+      }, err => {
+        this.filteredSchemeError = true;
+        this.errorMsg = 'No data Found';
+        console.error(err);
+      });
+
+    this.schemeControlSubs = this.schemeControl.valueChanges
       .pipe(
         debounceTime(500),
         tap(() => {
@@ -98,6 +159,21 @@ export class SupportUpperNjComponent implements OnInit {
         this.checkIfDataNotPresentAndShowError(data);
         console.log(this.filteredSchemes);
       });
+  }
+
+  searchSchemeNjPrudent(value) {
+    if (value === '') {
+      if (this.selectedElement) {
+        let threeWords = this.supportUpperService.getThreeWordsOfSchemeName(this.selectedElement);
+        if (this.apiCallingStack[1] !== threeWords) {
+          return this.supportUpperService.getSearchSchemeList({ rtMasterId: 5, schemeName: threeWords, startLimit: 0, endLimit: 50, isMapped: this.isMapped });
+        }
+      } else {
+        return this.supportUpperService.getSearchSchemeList({ rtMasterId: 5, schschemeNameeme: value, startLimit: 0, endLimit: 50, isMapped: this.isMapped });
+      }
+    } else {
+      return this.supportUpperService.getSearchSchemeList({ rtMasterId: 5, schemeName: value, startLimit: 0, endLimit: 50, isMapped: this.isMapped });
+    }
   }
   close() {
     const fragmentData = {
@@ -128,10 +204,10 @@ export class SupportUpperNjComponent implements OnInit {
       let threeWords = this.supportUpperService.getThreeWordsOfSchemeName(this.selectedElement);
       this.apiCallingStack.push(threeWords);
       if (this.apiCallingStack[1] !== threeWords) {
-        return this.supportUpperService.getFilteredSchemes({ scheme: threeWords });
+        return this.supportUpperService.getFilteredSchemes({ scheme: threeWords, startLimit: 0, endLimit: 50 });
       }
     } else {
-      return this.supportUpperService.getFilteredSchemes({ scheme: value });
+      return this.supportUpperService.getFilteredSchemes({ scheme: value, startLimit: 0, endLimit: 50 });
     }
   }
 
@@ -139,6 +215,8 @@ export class SupportUpperNjComponent implements OnInit {
     this.logValue('again clicked');
     this.supportUpperService.getSchemesDetails({ id: scheme.id })
       .subscribe(res => {
+        element.isSchemeSelected = true;
+        this.selectedSchemeRes = res;
         console.log('scheme details', res)
         element.navDate = res.navDate
         element.nav = res.nav
@@ -158,7 +236,7 @@ export class SupportUpperNjComponent implements OnInit {
     let threeWords = this.supportUpperService.getThreeWordsOfSchemeName(element);
     this.apiCallingStack.push(threeWords);
     if (this.apiCallingStack[1] !== threeWords) {
-      this.supportUpperService.getFilteredSchemes({ scheme: threeWords })
+      this.supportUpperService.getFilteredSchemes({ scheme: threeWords, startLimit: 0, endLimit: 0 })
         .subscribe(res => {
           this.apiCallingStack = [];
           this.isLoadingForDropDown = false;
@@ -176,31 +254,34 @@ export class SupportUpperNjComponent implements OnInit {
     //let threeWords = this.supportUpperService.getThreeWordsOfSchemeName(element);
     //this.apiCallingStack.push(threeWords);
     if (this.apiCallingStack[1] !== threeWords && element.length >= 3) {
-      this.supportUpperService.getFilteredSchemes({ scheme: threeWords })
+      this.supportUpperService.getFilteredSchemes({ scheme: threeWords, startLimit: 0, endLimit: 50 })
         .subscribe(res => {
-          console.log('serach scheme', res)
-          let dataTable: elementI[] = [];
-          this.apiCallingStack = [];
-          this.isLoadingForDropDown = false;
-          res.forEach(item => {
-            console.log(item);
-            dataTable.push({
-              name: item.schemeName,
-              nav: '',
-              schemeName: '',
-              schemeCode: '',
-              amficode: '',
-              navTwo: '',
-              navDate: '',
-              njCount: '',
-              map: ''
+          if (res) {
+            console.log('serach scheme', res)
+            let dataTable: any[] = [];
+            this.apiCallingStack = [];
+            this.isLoadingForDropDown = false;
+            res.forEach(item => {
+              dataTable.push({
+                name: item.schemeName,
+                nav: '',
+                schemeName: '',
+                schemeCode: '',
+                amficode: '',
+                navTwo: '',
+                navDate: '',
+                njCount: '',
+                map: '',
+                id: item.id,
+                transactionDate: item.transactionDate
+              });
             });
-          });
-          console.log("this is some data::::::", dataTable);
-          this.dataTable = dataTable;
-          this.dataSource.data = dataTable;
-          console.log(res);
-          this.checkIfDataNotPresentAndShowError(res);
+            console.log("this is some data::::::", dataTable);
+            this.dataTable = dataTable;
+            this.dataSource.data = dataTable;
+          } else {
+            this.checkIfDataNotPresentAndShowError(res);
+          }
         });
     }
   }
@@ -208,22 +289,58 @@ export class SupportUpperNjComponent implements OnInit {
     return scheme ? scheme.schemeName : undefined;
   }
 
+  unMapMappedNjScheme(element) {
+    let obj = {
+      id: this.selectedSchemeRes.id,
+      mutualFundSchemeMasterId: element.id,
+      schemeCode: element.schemeCode
+    }
+    console.log(obj);
+    this.supportUpperService.postUnmapUnmappedNjPrudentScheme(obj)
+      .subscribe(res => {
+        if (res) {
+          console.log(res);
+          element.isMapped = false;
+        }
+      })
+  }
+
+  mapUnmappedNJScheme(element) {
+    let obj = {
+      id: this.selectedSchemeRes.id,
+      mutualFundSchemeMasterId: element.id,
+      rt_id: 5,
+      schemeCode: element.schemeCode
+    }
+    console.log(obj);
+
+    this.supportUpperService.postMapUnmappedNjPrudentScheme(obj)
+      .subscribe(res => {
+        if (res) {
+          console.log(res);
+          element.isMapped = true;
+        }
+      })
+  }
+
   mapNjScheme(element) {
     console.log("this is some mapping value:::::;", element);
   }
 
-  getUnmappedNjSchemes() {
+  getMappedUnmappedNjSchemes() {
     this.isLoading = true;
     let data = {
       rtMasterId: 5,
-      startLimit: 0,
-      endLimit: 50
+      startLimit: this.startLimit,
+      endLimit: this.startLimit + 50,
+      isMapped: this.isMapped
     };
     this.supportUpperService.getUnmappedSchemesNj(data).subscribe(res => {
       console.log("this is unmapped Nj schemes::::::::::", res);
       this.isLoading = false;
-      let dataTable: elementI[] = [];
-      res.forEach(item => {
+      let dataTable: any[] = [];
+      this.totalNjCount = res.count;
+      res.njSchemeMasterList.forEach(item => {
         console.log(item);
         dataTable.push({
           name: item.schemeName,
@@ -234,7 +351,10 @@ export class SupportUpperNjComponent implements OnInit {
           navTwo: '',
           navDate: '',
           njCount: '',
-          map: ''
+          map: '',
+          id: item.id,
+          transactionDate: item.transactionDate,
+          isSchemeSelected: false
         });
       });
       console.log("this is some data::::::", dataTable);
@@ -250,13 +370,31 @@ export class SupportUpperNjComponent implements OnInit {
     this.eventService.changeUpperSliderState({ state: 'close' });
   }
 
-  onDropdownChange(event): void {
-    console.log('dropdown,changed', event);
+  setMapUnmapOption(event): void {
+    console.log(event);
+    this.isMapped = event.value == 1 ? false : true;
+    this.isLoading = true;
+    this.dataSource.data = [{}, {}, {}];
+    this.getMappedUnmappedNjSchemes();
   }
 
   onPaginationChange(event) {
     console.log(event);
+    if (event.value >= 0) {
+      this.startLimit = event.value * 50;
+    } else {
+      this.startLimit = 0;
+    }
     return event;
+  }
+  ngOnDestroy(): void {
+    if (this.searchSchemeControlSubs) {
+      this.searchSchemeControlSubs.unsubscribe();
+    }
+
+    if (this.schemeControlSubs) {
+      this.schemeControlSubs.unsubscribe();
+    }
   }
 }
 
