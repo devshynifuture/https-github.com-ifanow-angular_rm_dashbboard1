4779b0595f289693d57b53f5813792e07a85d3ce
Gaurav <gaurav@futurewise.co.in>
2020-12-15 11:39:30 +0530

    Transaction role integration partial GD commit
    Signed-off-by: Gaurav <gaurav@futurewise.co.in>

--- src/app/component/protect-component/AdviserComponent/transactions/transaction-role.service.ts
+++ src/app/component/protect-component/AdviserComponent/transactions/transaction-role.service.ts
@@ -53,6 +53,11 @@ export class TransactionRoleService {
     capabilityList: [],
     capabilityMap: {view: true, add: true, edit: true, delete: true}
   };
+  doSwpTransaction = {
+    showModule: true,
+    capabilityList: [],
+    capabilityMap: {view: true, add: true, edit: true, delete: true}
+  };
 
   constructor(
     private router: Router,
@@ -70,6 +75,13 @@ export class TransactionRoleService {
         this.transactionPermissionData.next(transactModule.subModule);
         this.setAddArnRiaDetailPermission(transactModule.subModule.addArnRiaCredentials);
         this.setDoRedemptionTransactionPermission(transactModule.subModule.doRedemptionTransaction);
+        this.setAddSubbrokersPermission(transactModule.subModule.addSubbrokers);
+        this.setClientMappingPermission(transactModule.subModule.clientMapping);
+        this.setDoPurchaseTransactionPermission(transactModule.subModule.doPurchaseTransaction);
+        this.setDoStpTransactionPermission(transactModule.subModule.doStpTransaction);
+        this.setDoSipTransactionPermission(transactModule.subModule.doSipTransaction);
+        this.setDoSwitchTransactionPermission(transactModule.subModule.doSwitchTransaction);
+        this.setDoSwpTransactionPermission(transactModule.subModule.doSwpTransaction);
         console.log('TransactionRoleService allPermissionData transaction : ', transactModule.subModule);
       }
     });
@@ -92,5 +104,69 @@ export class TransactionRoleService {
       console.log('TransactionRoleService doRedemptionTransaction : ', this.doRedemptionTransaction);
     }
   }
+
+  setAddSubbrokersPermission(addSubbrokers) {
+    if (addSubbrokers) {
+      this.addSubbrokers.showModule = addSubbrokers.showModule;
+      this.addSubbrokers.capabilityList = addSubbrokers.subModule.addSubbrokers.capabilityList;
+      this.addSubbrokers.capabilityMap = UtilService.getCapabilityMap(this.addSubbrokers.capabilityList);
+      console.log('TransactionRoleService addSubbrokers : ', this.addSubbrokers);
+    }
+  }
+
+  setClientMappingPermission(clientMapping) {
+    if (clientMapping) {
+      this.clientMapping.showModule = clientMapping.showModule;
+      this.clientMapping.capabilityList = clientMapping.subModule.clientMapping.capabilityList;
+      this.clientMapping.capabilityMap = UtilService.getCapabilityMap(this.clientMapping.capabilityList);
+      console.log('TransactionRoleService clientMapping : ', this.clientMapping);
+    }
+  }
+
+  setDoPurchaseTransactionPermission(doPurchaseTransaction) {
+    if (doPurchaseTransaction) {
+      this.doPurchaseTransaction.showModule = doPurchaseTransaction.showModule;
+      this.doPurchaseTransaction.capabilityList = doPurchaseTransaction.subModule.doPurchaseTransaction.capabilityList;
+      this.doPurchaseTransaction.capabilityMap = UtilService.getCapabilityMap(this.doPurchaseTransaction.capabilityList);
+      console.log('TransactionRoleService doPurchaseTransaction : ', this.doPurchaseTransaction);
+    }
+  }
+
+  setDoStpTransactionPermission(doStpTransaction) {
+    if (doStpTransaction) {
+      this.doStpTransaction.showModule = doStpTransaction.showModule;
+      this.doStpTransaction.capabilityList = doStpTransaction.subModule.doStpTransaction.capabilityList;
+      this.doStpTransaction.capabilityMap = UtilService.getCapabilityMap(this.doStpTransaction.capabilityList);
+      console.log('TransactionRoleService doStpTransaction : ', this.doStpTransaction);
+    }
+  }
+
+  setDoSipTransactionPermission(doSipTransaction) {
+    if (doSipTransaction) {
+      this.doSipTransaction.showModule = doSipTransaction.showModule;
+      this.doSipTransaction.capabilityList = doSipTransaction.subModule.doSipTransaction.capabilityList;
+      this.doSipTransaction.capabilityMap = UtilService.getCapabilityMap(this.doSipTransaction.capabilityList);
+      console.log('TransactionRoleService doSipTransaction : ', this.doSipTransaction);
+    }
+  }
+
+  setDoSwitchTransactionPermission(doSwitchTransaction) {
+    if (doSwitchTransaction) {
+      this.doSwitchTransaction.showModule = doSwitchTransaction.showModule;
+      this.doSwitchTransaction.capabilityList = doSwitchTransaction.subModule.doSwitchTransaction.capabilityList;
+      this.doSwitchTransaction.capabilityMap = UtilService.getCapabilityMap(this.doSwitchTransaction.capabilityList);
+      console.log('TransactionRoleService doSwitchTransaction : ', this.doSwitchTransaction);
+    }
+  }
+
+  setDoSwpTransactionPermission(doSwpTransaction) {
+    if (doSwpTransaction) {
+      this.doSwpTransaction.showModule = doSwpTransaction.showModule;
+      this.doSwpTransaction.capabilityList = doSwpTransaction.subModule.doSwpTransaction.capabilityList;
+      this.doSwpTransaction.capabilityMap = UtilService.getCapabilityMap(this.doSwpTransaction.capabilityList);
+      console.log('TransactionRoleService doSwpTransaction : ', this.doSwpTransaction);
+    }
+  }
+
 
 }
