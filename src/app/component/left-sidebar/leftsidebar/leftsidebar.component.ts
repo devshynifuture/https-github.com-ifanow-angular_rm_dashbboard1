de2eb10240cdf4e85c30e01e6421dcd930489b59
ani <aniruddha@futurewise.co.in>
2021-01-06 16:21:31 +0530

    add

--- src/app/component/left-sidebar/leftsidebar/leftsidebar.component.ts
+++ src/app/component/left-sidebar/leftsidebar/leftsidebar.component.ts
@@ -1,21 +1,21 @@
-import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
-import {AuthService} from 'src/app/auth-service/authService';
-import {EventService} from '../../../Data-service/event.service';
-import {SubscriptionInject} from '../../protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
-import {FormControl} from '@angular/forms';
-import {SubscriptionService} from '../../protect-component/AdviserComponent/Subscriptions/subscription.service';
-import {Router} from '@angular/router';
-import {DialogContainerComponent} from '../../../common/dialog-container/dialog-container.component';
-import {DynamicComponentService} from '../../../services/dynamic-component.service';
-import {dialogContainerOpacity, rightSliderAnimation, upperSliderAnimation} from '../../../animation/animation';
-import {EnumDataService} from '../../../services/enum-data.service';
-import {SettingsService} from '../../protect-component/AdviserComponent/setting/settings.service';
-import {UtilService} from 'src/app/services/util.service';
-import {PeopleService} from '../../protect-component/PeopleComponent/people.service';
-import {debounceTime, startWith} from 'rxjs/operators';
-import {Observable, Subscription} from 'rxjs';
-import {RoleService} from '../../../auth-service/role.service';
-import {MfServiceService} from '../../protect-component/customers/component/customer/accounts/assets/mutual-fund/mf-service.service';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/auth-service/authService';
+import { EventService } from '../../../Data-service/event.service';
+import { SubscriptionInject } from '../../protect-component/AdviserComponent/Subscriptions/subscription-inject.service';
+import { FormControl } from '@angular/forms';
+import { SubscriptionService } from '../../protect-component/AdviserComponent/Subscriptions/subscription.service';
+import { Router } from '@angular/router';
+import { DialogContainerComponent } from '../../../common/dialog-container/dialog-container.component';
+import { DynamicComponentService } from '../../../services/dynamic-component.service';
+import { dialogContainerOpacity, rightSliderAnimation, upperSliderAnimation } from '../../../animation/animation';
+import { EnumDataService } from '../../../services/enum-data.service';
+import { SettingsService } from '../../protect-component/AdviserComponent/setting/settings.service';
+import { UtilService } from 'src/app/services/util.service';
+import { PeopleService } from '../../protect-component/PeopleComponent/people.service';
+import { debounceTime, startWith } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { RoleService } from '../../../auth-service/role.service';
+import { MfServiceService } from '../../protect-component/customers/component/customer/accounts/assets/mutual-fund/mf-service.service';
 
 @Component({
   selector: 'app-leftsidebar',
@@ -45,7 +45,7 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
   showDefaultDropDownOnSearch: boolean;
   isOpen: boolean;
   roleObj: any = {};
-  @ViewChild('inputSearch', {static: true}) inputRef: ElementRef;
+  @ViewChild('inputSearch', { static: true }) inputRef: ElementRef;
   logoText = 'Your Logo here';
   role: any;
   isLoding: boolean;
@@ -53,15 +53,15 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
   familyOutputObservable: Observable<any> = new Observable<any>();
 
   constructor(public authService: AuthService, private _eref: ElementRef,
-              protected eventService: EventService, protected subinject: SubscriptionInject,
-              private subService: SubscriptionService, private router: Router, private ngZone: NgZone,
-              protected dynamicComponentService: DynamicComponentService,
-              public enumDataService: EnumDataService,
-              private settingsService: SettingsService,
-              private auth: AuthService,
-              private utilService: UtilService, private peopleService: PeopleService,
-              public roleService: RoleService,
-              public MfServiceService: MfServiceService) {
+    protected eventService: EventService, protected subinject: SubscriptionInject,
+    private subService: SubscriptionService, private router: Router, private ngZone: NgZone,
+    protected dynamicComponentService: DynamicComponentService,
+    public enumDataService: EnumDataService,
+    private settingsService: SettingsService,
+    private auth: AuthService,
+    private utilService: UtilService, private peopleService: PeopleService,
+    public roleService: RoleService,
+    public MfServiceService: MfServiceService) {
     /*constructor(private router: Router, protected eventService: EventService, protected subinject: SubscriptionInject,
       protected dynamicComponentService: DynamicComponentService, private route: ActivatedRoute,
       private authService: AuthService) {*/
@@ -133,9 +133,7 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
      );
    }*/
 
-  uuidRes(data) {
 
-  }
 
 
   getActiveLink(value) {
@@ -204,7 +202,7 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
             this.auth.setClientData(data);
             this.myControl.setValue(singleClientData.displayName);
             this.ngZone.run(() => {
-              this.router.navigate(['customer', 'detail', 'overview', 'myfeed'], {state: {...data}});
+              this.router.navigate(['customer', 'detail', 'overview', 'myfeed'], { state: { ...data } });
             });
           }
         },
@@ -216,7 +214,7 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
       this.auth.setClientData(singleClientData);
       this.myControl.setValue(singleClientData.displayName);
       this.ngZone.run(() => {
-        this.router.navigate(['customer', 'detail', 'overview', 'myfeed'], {state: {...singleClientData}});
+        this.router.navigate(['customer', 'detail', 'overview', 'myfeed'], { state: { ...singleClientData } });
       });
     }
   }
@@ -282,29 +280,29 @@ export class LeftsidebarComponent extends DialogContainerComponent implements On
     }
     this.familyOutputSubscription = this.familyOutputObservable.pipe(startWith(''),
       debounceTime(700)).subscribe(
-      data => {
-        this.peopleService.getClientFamilyMemberList(obj).subscribe(responseArray => {
-          if (responseArray) {
-            if (value.length >= 0) {
-              this.clientList = responseArray;
-              this.showDefaultDropDownOnSearch = false;
-              this.isLoding = false;
+        data => {
+          this.peopleService.getClientFamilyMemberList(obj).subscribe(responseArray => {
+            if (responseArray) {
+              if (value.length >= 0) {
+                this.clientList = responseArray;
+                this.showDefaultDropDownOnSearch = false;
+                this.isLoding = false;
+              } else {
+                this.showDefaultDropDownOnSearch = undefined;
+                this.isLoding = undefined;
+                this.clientList = undefined;
+              }
             } else {
-              this.showDefaultDropDownOnSearch = undefined;
-              this.isLoding = undefined;
+              this.showDefaultDropDownOnSearch = true;
+              this.isLoding = false;
               this.clientList = undefined;
             }
-          } else {
-            this.showDefaultDropDownOnSearch = true;
-            this.isLoding = false;
+          }, error => {
             this.clientList = undefined;
-          }
-        }, error => {
-          this.clientList = undefined;
-          console.log('getFamilyMemberListRes error : ', error);
-        });
-      }
-    );
+            console.log('getFamilyMemberListRes error : ', error);
+          });
+        }
+      );
   }
 
   getPersonalProfiles() {
