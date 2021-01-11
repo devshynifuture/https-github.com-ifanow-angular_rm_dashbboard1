fa572cfc1c205ffbb5b1c91e708d94bb8cc87517
ng-khizar <khizaruddin@futurewise.co.in>
2021-01-11 12:19:45 +0530

    aum recon routing tab issue resolved

--- src/app/component/protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/reconciliation/reconciliation.component.ts
+++ src/app/component/protect-component/AdviserComponent/backOffice/backoffice-aum-reconciliation/reconciliation/reconciliation.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ReconciliationService } from './reconciliation.service';
 
@@ -8,7 +9,25 @@ import { ReconciliationService } from './reconciliation.service';
 })
 export class ReconciliationComponent implements OnInit {
 
-  constructor( ) { }
-  ngOnInit() {}
+  constructor(
+    private reconService: ReconciliationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.reconService.getRoutedOn().subscribe(value => {
+      if (value) {
+        this.router.navigate([value], { relativeTo: this.activatedRoute });
+      } else {
+        this.router.navigate(['cams'], { relativeTo: this.activatedRoute });
+      }
+    })
+  }
+  ngOnInit() {
+
+  }
+
+  setRoutedOnValue(value) {
+    this.reconService.setRoutedOn(value);
+  }
 
 }
