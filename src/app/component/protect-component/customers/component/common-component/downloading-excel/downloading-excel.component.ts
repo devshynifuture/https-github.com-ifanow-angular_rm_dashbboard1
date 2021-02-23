ad061aa07c2832e053acff2241511a8c03ffbfbe
ani <aniruddha@futurewise.co.in>
2021-02-23 11:32:00 +0530

    downloading Excel

---
+++ src/app/component/protect-component/customers/component/common-component/downloading-excel/downloading-excel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-downloading-excel',
+  templateUrl: './downloading-excel.component.html',
+  styleUrls: ['./downloading-excel.component.scss']
+})
+export class DownloadingExcelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
