083e7e4012e65637ed451703e6824234dff9e853
sunny <sunny@futurewise.co.in>
2021-03-03 13:42:23 +0530

    new add templates ui

--- src/app/component/protect-component/AdviserComponent/setting/setting-plan/setting-plan/plan-templates/plan-templates.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-plan/setting-plan/plan-templates/plan-templates.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { EventService } from 'src/app/Data-service/event.service';
+import { UtilService } from 'src/app/services/util.service';
+import { SubscriptionInject } from '../../../../Subscriptions/subscription-inject.service';
+import { SettingsService } from '../../../settings.service';
+import { AddNewTemplateComponent } from './add-new-template/add-new-template.component';
 
 @Component({
   selector: 'app-plan-templates',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanTemplatesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private subInjectService: SubscriptionInject,
+    private settingsService: SettingsService,
+    private eventService: EventService,
+    private dialog: MatDialog,
+    protected subinject: SubscriptionInject) { }
 
   ngOnInit() {
   }
+  openAddtemlates(value, data) {
 
+    const fragmentData = {
+      flag: value,
+      data,
+      id: 1,
+      state: 'open',
+      componentName: AddNewTemplateComponent,
+
+    };
+    const rightSideDataSub = this.subinject.changeNewRightSliderState(fragmentData).subscribe(
+      sideBarData => {
+        console.log('this is sidebardata in subs subs : ', sideBarData);
+        if (UtilService.isDialogClose(sideBarData)) {
+          if (UtilService.isRefreshRequired(sideBarData)) {
+
+          }
+          rightSideDataSub.unsubscribe();
+        }
+
+      }
+    );
+  }
 }
