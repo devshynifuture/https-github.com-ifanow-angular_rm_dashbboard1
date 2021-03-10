a657e3119d9eb52e9b78aecc6055de03ddcdf6b7
devshyni <devshyni@futurewise.co.in>
2021-03-10 12:30:56 +0530

    capital gain year

--- src/app/component/protect-component/AdviserComponent/setting/setting-plan/setting-plan.component.ts
+++ src/app/component/protect-component/AdviserComponent/setting/setting-plan/setting-plan.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingPlanComponent implements OnInit {
 
-  constructor() { }
+  constructor(public enumDataService: EnumDataService,
+  ) { }
 
   ngOnInit() {
   }
