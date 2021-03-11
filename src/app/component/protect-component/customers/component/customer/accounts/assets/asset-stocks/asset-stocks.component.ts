78c91f009013a58861936b0feaa41dda965bb26d
devshyni <devshyni@futurewise.co.in>
2021-03-11 19:15:05 +0530

    totalAbsulateReturn in stock change in stock footer value

--- src/app/component/protect-component/customers/component/customer/accounts/assets/asset-stocks/asset-stocks.component.ts
+++ src/app/component/protect-component/customers/component/customer/accounts/assets/asset-stocks/asset-stocks.component.ts
@@ -37,8 +37,8 @@ export class AssetStocksComponent implements OnInit {
   displayedColumns25 = ['scrip', 'amt', 'cvalue', 'gain', 'bal', 'price', 'mprice', 'ret',
     'dividend', 'icons'];
 
-  footerColumns = ['scrip', /*'owner', 'bal', 'price', 'mprice',*/ 'amt', 'cvalue', 'gain', 'bal',
-    'dividend', 'icons'];
+  // footerColumns = ['scrip', /*'owner', 'bal', 'price', 'mprice',*/ 'amt', 'cvalue', 'gain', 'bal',
+  //   'dividend', 'icons'];
   dataSource25 = ELEMENT_DATA25;
   advisorId: any;
   clientId: any;
