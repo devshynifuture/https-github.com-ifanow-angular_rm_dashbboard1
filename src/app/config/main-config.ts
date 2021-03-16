750d35a09f8b4dda69e3844b5d1627a2b8e8ed28
devshyni <devshyni@futurewise.co.in>
2021-03-16 12:09:44 +0530

    goal optimization removed

--- src/app/config/main-config.ts
+++ src/app/config/main-config.ts
@@ -4,6 +4,7 @@ export const apiConfig = {
   MAIN_URL: environment.APIEndpoint,
   GMAIL_URL: environment.GmailAPIEndpoint,
   TRANSACT: environment.TransactionPlatform,
+  GOAL: environment.GoalPlatform,
   USER: environment.UserUrl,
   MARKET_PLACE: environment.MarketPlaceUrl,
   PRODUCTION: environment.production,
