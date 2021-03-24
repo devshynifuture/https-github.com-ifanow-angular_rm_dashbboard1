bf3b7760d155776d89877e1ba85c9b8bc3fd3416
ani <aniruddha@futurewise.co.in>
2021-03-24 10:18:02 +0530

    chnage in environmemnt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   APIEndpoint: 'https://api.my-planner.in/futurewise/api/v1/web/',
-  mfProject: 'https://dev.ifanow.in/mutualfund/api/v1/web/',
+  mfProject: 'https://mutual-fund.my-planner.in/mutualfund/api/v1/web/',
   GmailAPIEndpoint: 'https://emailserver.my-planner.in/email-server/api/v1/web/',
   TransactionPlatform: 'https://transactplatform.my-planner.in/transaction-platform/api/v1/web/',
   GoalPlatform: 'https://api-goal.my-planner.in/goalplanningfuturewise/api/v1/web/',
