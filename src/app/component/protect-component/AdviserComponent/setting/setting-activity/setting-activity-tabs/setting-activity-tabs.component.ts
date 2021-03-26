ad6d4ea3216d9e5008aa795236a65efb2e4258d9
sunny <sunny@futurewise.co.in>
2021-03-26 16:34:21 +0530

    new ui setting

---
+++ src/app/component/protect-component/AdviserComponent/setting/setting-activity/setting-activity-tabs/setting-activity-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-activity-tabs',
+  templateUrl: './setting-activity-tabs.component.html',
+  styleUrls: ['./setting-activity-tabs.component.scss']
+})
+export class SettingActivityTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
